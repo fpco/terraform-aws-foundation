3216d25e6db93883dfec5c88c0edc9e73d8029a1
ketzacoatl <ketzacoatl@users.noreply.github.com>
2015-11-02 23:27:19 -0500

    Update consul-leaders module
    * use the new ASG, ssh-sg and cluster-network modules
    * add missing AMI
    * whitespace / ssh_pubkey variable cleanup
    * other updates to consul-leaders module
    * Drop consul-leader-sg from consul-leaders module
    
    Rather than create this security group in the module, we will
    provide the list of security groups to the consul leaders module.
    This means the module is not responsible for the security group,
    but the "caller" is. Thus, we simplify variables and swap for a
    pass-through (leader_security_group_ids accepts string-lists).

--- tf-modules/consul-leaders/outputs.tf
+++ tf-modules/consul-leaders/outputs.tf
@@ -1,18 +1,15 @@
-output "subnet-a-cidr_block" {
-    value = "${aws_subnet.a.cidr_block}"
+output "cidr_a" {
+    value = "${module.cluster-net.cidr_a}"
 }
-output "subnet-c-cidr_block" {
-    value = "${aws_subnet.c.cidr_block}"
+output "cidr_c" {
+    value = "${module.cluster-net.cidr_c}"
 }
 output "leader_dns" {
     value = "${aws_route53_record.leaders.name}"
 }
 output "asg_name" {
-    value = "${aws_autoscaling_group.leaders.name}"
+    value = "${module.leader-asg.name}"
 }
 output "asg_id" {
-    value = "${aws_autoscaling_group.leaders.id}"
-}
-output "lc_name" {
-    value = "${aws_launch_configuration.leaders.name}"
+    value = "${module.leader-asg.id}"
 }
--- tf-modules/consul-leaders/variables.tf
+++ tf-modules/consul-leaders/variables.tf
@@ -1,81 +1,54 @@
 variable "name" {
-    default = "test"
+    default = ""
     description = "The name of this cluster of consul leaders, this should be unique"
 }
-# this is not yet in use
-variable "key_file" {
-    default = "~/.ssh/consul-leader-asg"
-    description = "The full path to the private key for use with SSH"
-}
-
 variable "key_name" {
-    default = "consul-leader-asg"
+    default = ""
     description = "The name of the (AWS) SSH key to associate with the instance"
 }
-
 variable "ami" {
     description = "The base AMI for each AWS instance created"
 }
-
 variable "instance_type" {
     default = "t2.micro"
     description = "The type of AWS instance (size)"
 }
-
 variable "user_data" {
     default = ""
     description = "The user_data string to pass to cloud-init"
 }
-
-variable "inbound_security_group" {
+variable "leader_security_group_ids" {
     default = ""
-    description = "ID of a security group, whitelist of client CIDR to consul leader"
+    description = "String-list of ids, the security groups to assign to the leaders"
 }
-
-variable "ssh_cidr_block" {
-    default = "0.0.0.0/0"
-    description = "The CIDR IP block to allow SSH connections from"
-}
-
 variable "max_nodes" {
     default = 9
     description = "The maximum number of nodes in the auto-scaling group"
 }
-
 variable "min_nodes" {
     default = 3
     description = "The minimum number of nodes in the auto-scaling group"
 }
-
 variable "desired_capacity" {
     default = 7
     description = "The desired number of nodes in the auto-scaling group"
 }
-
-variable "ssh_pubkey" {
-    description = "The contents of the public key to be dropped into the AWS instances"
-}
-
 variable "access_key" {
     description = "AWS key id"
 }
 variable "secret_key" {
     description = "AWS key secret"
 }
-
 variable "consul_secret_key" {
     description = "Secret key to secure cluster communication, generate this with `consul keygen`"
 }
-
 variable "consul_master_token" {
     description = "Master token for the ACL system in Consul"
 }
-
 variable "leader_count" {
     default = "'3'"
     description = "The number of leaders to form majority consensus, better to leave this as is"
 }
-
 variable "cidr_prefix_a" {
     default = "10.100.3"
     description = "The prefix to use for the CIDR block and IP details in the formula, subnet a"
@@ -88,7 +61,6 @@ variable "cidr_mask" {
     default = "28"
     description = "The CIDR mask to use for CIDR block definitions, better to leave this as is"
 }
-
 variable "region" {
     description = "AWS region to deploy to"
 }
