139f31811ed0f27bd550872fb13a5e6936be71fa
ketzacoatl <ketzacoatl@users.noreply.github.com>
2015-11-09 04:18:56 -0500

    Update consul leader security group
    * Drop WAN port, that is now in its own security group to more
      accurately enable support for multi-dc consul clusters (when you want
      it, rather than enabled-by-default)
    * use replace() with split(), remove extra spaces in string-lists

--- tf-modules/consul-leader-sg/main.tf
+++ tf-modules/consul-leader-sg/main.tf
@@ -10,65 +10,52 @@ resource "aws_security_group" "main" {
         from_port = 8300
         to_port = 8300
         protocol = "tcp"
-        cidr_blocks = ["${split(",", var.cidr_blocks)}"]
+        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
     }
     ingress {
         from_port = 8300
         to_port = 8300
         protocol = "udp"
-        cidr_blocks = ["${split(",", var.cidr_blocks)}"]
+        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
     }
     # Serf LAN, used to handle gossip in the LAN. Required by all agents. TCP and UDP.
     ingress {
         from_port = 8301
         to_port = 8301
         protocol = "tcp"
-        cidr_blocks = ["${split(",", var.cidr_blocks)}"]
+        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
     }
     ingress {
         from_port = 8301
         to_port = 8301
         protocol = "udp"
-        cidr_blocks = ["${split(",", var.cidr_blocks)}"]
-    }
-    # Serf WAN, used by servers to gossip over the WAN to other servers. TCP and UDP.
-    ingress {
-        from_port = 8302
-        to_port = 8302
-        protocol = "tcp"
-        cidr_blocks = ["${split(",", var.cidr_blocks)}"]
-    }
-    ingress {
-        from_port = 8302
-        to_port = 8302
-        protocol = "udp"
-        cidr_blocks = ["${split(",", var.cidr_blocks)}"]
+        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
     }
     # consul CLI RPC, used by all agents to handle RPC from the CLI. TCP only.
     ingress {
         from_port = 8400
         to_port = 8400
         protocol = "tcp"
-        cidr_blocks = ["${split(",", var.cidr_blocks)}"]
+        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
     }
     # HTTP API, used by clients to talk to the HTTP API. TCP only.
     ingress {
         from_port = 8500
         to_port = 8500
         protocol = "tcp"
-        cidr_blocks = ["${split(",", var.cidr_blocks)}"]
+        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
     }
     # DNS Interface, used to resolve DNS queries. TCP and UDP.
     ingress {
         from_port = 8600
         to_port = 8600
         protocol = "tcp"
-        cidr_blocks = ["${split(",", var.cidr_blocks)}"]
+        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
     }
     ingress {
         from_port = 8600
         to_port = 8600
         protocol = "udp"
-        cidr_blocks = ["${split(",", var.cidr_blocks)}"]
+        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
     }
 }
