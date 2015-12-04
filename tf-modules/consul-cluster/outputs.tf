043e23c660dbc9198dcc5e7f56e591af0dd1165d
ketzacoatl <ketzacoatl@users.noreply.github.com>
2015-12-04 16:35:23 -0500

    add subnet_*_cidr output to consul-cluster module
    other modules will want to use this

--- tf-modules/consul-cluster/outputs.tf
+++ tf-modules/consul-cluster/outputs.tf
@@ -1,8 +1,14 @@
 output "subnet_a_id" {
     value = "${module.cluster-net.id_a}"
 }
+output "subnet_a_cidr" {
+    value = "${module.cluster-net.cidr_a}"
+}
 output "subnet_c_id" {
     value = "${module.cluster-net.id_c}"
+}
+output "subnet_c_cidr" {
+    value = "${module.cluster-net.cidr_c}"
 }
 output "asg_name" {
     value = "${module.agent-asg.name}"
