8127d263ad3586b6f3b0b25a4e17b1becd50d01a
ketzacoatl <ketzacoatl@users.noreply.github.com>
2015-12-04 16:30:10 -0500

    Add support for availability_zone output in cluster-network
    az_a and az_c for subnets a and c, respectively.

--- tf-modules/cluster-network/outputs.tf
+++ tf-modules/cluster-network/outputs.tf
@@ -1,8 +1,15 @@
+output "az_a" {
+    value = "${aws_subnet.a.availability_zone}"
+}
 output "id_a" {
     value = "${aws_subnet.a.id}"
 }
 output "cidr_a" {
     value = "${aws_subnet.a.cidr_block}"
+}
+#
+output "az_c" {
+    value = "${aws_subnet.c.availability_zone}"
 }
 output "id_c" {
     value = "${aws_subnet.c.id}"
