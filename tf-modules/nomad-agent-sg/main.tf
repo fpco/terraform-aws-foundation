220bd2c092ad7b518f0f816e5e2ddd5224a3c9d0
ketzacoatl <ketzacoatl@users.noreply.github.com>
2015-12-26 02:36:22 -0500

    Correct ports needed in nomad agent security group

--- tf-modules/nomad-agent-sg/main.tf
+++ tf-modules/nomad-agent-sg/main.tf
@@ -2,13 +2,13 @@
 resource "aws_security_group" "main" {
     vpc_id = "${var.vpc_id}"
     tags {
-        Name = "${var.name}-${var.region}-nomad-server"
-        Description = "Allow ports 4646/4647 (TCP) to nomad agents in ${var.name}"
+        Name = "${var.name}-${var.region}-nomad-agent"
+        Description = "Allow port 4646 (TCP/HTTP) to nomad agents in ${var.name}"
     }
-    # open port 4646/7 (nomad) tcp
+    # open port 4646 (nomad http) tcp
     ingress {
         from_port = 4646
-        to_port = 4647
+        to_port = 4646
         protocol = "tcp"
         cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
     }
