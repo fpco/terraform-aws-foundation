3ab660bd344ec1ba92bf4e6afad71bdd4fd0d894
ketzacoatl <ketzacoatl@users.noreply.github.com>
2016-02-27 02:18:25 -0500

    simplify (shorten) name of elasticache subnet group

--- tf-modules/elasticache-redis-cluster/main.tf
+++ tf-modules/elasticache-redis-cluster/main.tf
@@ -42,7 +42,7 @@ resource "aws_elasticache_cluster" "redis" {
 }
 
 resource "aws_elasticache_subnet_group" "redis" {
-    name = "${var.name}-redis-ec-${var.region}"
+    name = "${var.name}-${var.region}"
     description = "Redis on Elasticache for ${var.name}"
     subnet_ids = ["${aws_subnet.a.id}", "${aws_subnet.c.id}"]
 }
