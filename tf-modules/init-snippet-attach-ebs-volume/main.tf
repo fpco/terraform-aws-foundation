6c980a7884e0dc91fba3a1cea3ad30d9061c1ce4
ketzacoatl <ketzacoatl@users.noreply.github.com>
2016-05-28 23:17:31 -0400

    Add init snippet to block while making attemptps to attach an EBS volume

---
+++ tf-modules/init-snippet-attach-ebs-volume/main.tf
@@ -0,0 +1,48 @@
+# variables used by this snippet of init shellcode
+variable "device_path" {
+    default = "/dev/xvdf"
+    description = "path, to the device's path in /dev/"
+}
+variable "init_prefix" {
+    default = ""
+    description = "initial init (shellcode) to prefix this snippet with"
+}
+variable "init_suffix" {
+    default = ""
+    description = "init (shellcode) to append to the end of this snippet"
+}
+variable "log_level" {
+    default = "info"
+    description = "default log level verbosity for apps that support it"
+}
+variable "log_prefix" {
+    default = "OPS: "
+    description = "string to prefix log messages with"
+}
+variable "region" {
+    description = "AWS region the volume is in"
+}
+variable "wait_interval" {
+    default = "5"
+    description = "time (in seconds) to wait when looping to find the device"
+}
+variable "volume_id" {
+    description = "ID of the EBS volume to attach"
+}
+# render init script for a cluster using our generic template
+resource "template_file" "init-snippet" {
+    template = "${path.module}/snippet.tpl"
+    vars {
+        device_path = "${var.device_path}"
+        init_prefix = "${var.init_prefix}"
+        init_suffix = "${var.init_suffix}"
+        log_prefix = "${var.log_prefix}"
+        log_level = "${var.log_level}"
+        region = "${var.region}"
+        volume_id = "${var.volume_id}"
+        wait_interval = "${var.wait_interval}"
+    }
+}
+output "init_snippet" {
+    value = "${template_file.init-snippet.rendered}"
+}
---
+++ tf-modules/init-snippet-attach-ebs-volume/snippet.tpl
@@ -0,0 +1,16 @@
+# start snippet - attach EBS volume
+${init_prefix}
+export AWS_DEFAULT_REGION=${region}
+VOLUME_ID=${volume_id}
+INSTANCE_ID=$$(ec2metadata --instance-id)
+echo "${log_prefix} will attach $$VOLUME_ID via the AWS API in ${region}"
+aws ec2 attach-volume                \
+          --volume-id $$VOLUME_ID     \
+          --instance-id $$INSTANCE_ID \
+          --device ${device_path}
+
+while [ $$? -ne 0 ]; do
+  sleep ${wait_interval}
+  ls ${device_path}
+done
+${init_suffix}
