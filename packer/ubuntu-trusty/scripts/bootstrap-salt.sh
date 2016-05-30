2d733181d534cdf2b3406f8425d60b9249a197f4
ketzacoatl <ketzacoatl@users.noreply.github.com>
2016-05-29 23:05:08 -0400

    update how we install saltstack on the base AMI
    This is based on http://repo.saltstack.com/#ubuntu, which is the
    "official" install - the ppa used previously eventually became the
    "commumnity supported ppa". Now we have access to the latest releases,
    2016.3 and similar.
    
    With that said, this update..
    a) aims to install 2015.5.10
    b) grabs the pip state and module from 2015.8.x
    c) installs salt.utils.locales from 2015.8.x as well
    
    This aims to address the issue where the pip state fails to install
    packages, but is a temporary work around until this is resolved in our
    favor: https://github.com/saltstack/salt/issues/31541
    
    (ext_pillar now fails hard, as of 2015.8.x, which brings down a state
    run when consul is not yet available, but has bee configured as an
    external pillar provider)
    
    include config for extension modules
    sync modules/source imported from upstream
    
    TIL, if you have source in _states or _modules, and use
    saltutil.sync_all, salt will overwrite what you put in your
    extension_modules path, go figure.

--- packer/ubuntu-trusty/scripts/bootstrap-salt.sh
+++ packer/ubuntu-trusty/scripts/bootstrap-salt.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# this will give us latest saltstack
-add-apt-repository -y ppa:saltstack/salt
-# ensure we get zmq v4
-add-apt-repository -y ppa:chris-lea/zeromq
+# see http://repo.saltstack.com/#ubuntu for more info
+# import the SaltStack repository key
+wget -O - https://repo.saltstack.com/apt/ubuntu/14.04/amd64/2016.3/SALTSTACK-GPG-KEY.pub | apt-key add -
+# point apt at the official saltstack repo
+echo "deb http://repo.saltstack.com/apt/ubuntu/14.04/amd64/2015.5 trusty main" > /etc/apt/sources.list.d/saltstack.list
 # ensure PPAs are active and install the salt minion!
 apt-get update
 apt-get install -y salt-minion
@@ -43,6 +44,19 @@ END_ALIAS
 # setup pillar for running state.highstate
 # the user gave us pillar .sls as uploads, move them into place for salt
 mv /tmp/pillar /srv/
+
+# the following is a hack to a) allow us to continue running 2015.5 for it's ext_pillar module
+# and b) get the pip state/module from 2015.8, which doesn't break pip
+echo "extension_modules: /srv/salt-ext" > /etc/salt/minion.d/extension_modules.conf
+wget --output-document=/usr/lib/python2.7/dist-packages/salt/utils/locales.py https://raw.githubusercontent.com/saltstack/salt/2015.8/salt/utils/locales.py
+# there are files in _states and _modules we want to sync and make available,
+# that is where we have our pip state/module source we've imported from upstream
+mv /tmp/top.sls /srv/salt-git/fpco-salt-formula/
+mkdir -p /srv/salt-ext/states /srv/salt-ext/modules
+salt-call --local saltutil.sync_all --log-level=debug
+echo "formula:"
+ls -alh /srv/salt-deb/*/_*
+ls -alh /srv/salt-git/*/_*
 
 # all of /srv/* is root only, and not world readable
 chown -R root:root /srv
