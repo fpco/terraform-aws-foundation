d0844243fdfd9ee69bd4ec30ca34110f49ae844b
ketzacoatl <ketzacoatl@users.noreply.github.com>
2016-07-25 10:21:18 -0400

    add scale-asg-iam-policy, creates reusable IAM policy to scale ASGs in an account

---
+++ tf-modules/scale-asg-iam-policy/main.tf
@@ -0,0 +1,29 @@
+variable "name" { }
+
+resource "aws_iam_policy" "main" {
+    name = "${var.name}-scale-all-asgs"
+    description = "allows users to scale up/down any ASGs in the account"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "autoscaling:SetDesiredCapacity"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+output "id" {
+    value = "${aws_iam_policy.main.id}"
+}
+output "arn" {
+    value = "${aws_iam_policy.main.arn}"
+}
+output "name" {
+    value = "${aws_iam_policy.main.name}"
+}
