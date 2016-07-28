42b2a9678f5d137fccca0c0d531a21a9916ba2a3
ketzacoatl <ketzacoatl@users.noreply.github.com>
2016-07-28 01:18:14 -0400

    add packer build script for Jenkins/CI

---
+++ packer/scripts/jenkins_build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+PACKER_DIR=$WORKSPACE/fpco-terraform-aws/packer/$BUILD
+cd $PACKER_DIR
+
+cat <<EOT > variables.json
+{
+    "ami_name": "$NAME",
+    "region": "$REGION",
+    "vpc_id": "$VPC_ID",
+    "subnet_id": "$SUBNET_ID",
+    "source_ami": "$AMI",
+    "instance_type": "$INSTANCE_TYPE",
+    "description": "$DESCRIPTION",
+    "os_version": "$OS_VERSION",
+    "os_release": "$OS_RELEASE",
+    "build_id": "$BUILD_TAG",
+    "copy_to_a": "$COPY_AMI_TO_A",
+    "copy_to_b": "$COPY_AMI_TO_B"
+}
+EOT
+packer build -var-file=variables.json base-host.json
