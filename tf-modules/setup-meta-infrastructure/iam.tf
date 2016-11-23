78744aeb6766487bea17969b33878df513e79241
Emanuel Borsboom <manny@fpcomplete.com>
2016-11-23 04:59:37 -0800

    tf-modules/setup-meta-infrastructure/iam: use aws_iam_policy_document instead of template_file

--- tf-modules/setup-meta-infrastructure/iam.tf
+++ tf-modules/setup-meta-infrastructure/iam.tf
@@ -46,53 +46,125 @@ resource "aws_iam_group" "setup-mfa" {
 # Policies and attachments
 #----------------------------------------------------------------------
 
-data "template_file" "admin-group-policy" {
-  template = "${file("${path.module}/admin-group-policy-doc.json.tpl")}"
-  vars {
-    account_id = "${data.aws_caller_identity.current.account_id}"
+# Has full access to everything, including IAM management.  Requires MFA.
+data "aws_iam_policy_document" "admin" {
+  statement {
+    effect = "Allow"
+    actions = ["*"]
+    resources = ["*"]
+    condition {
+      test = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values = ["true"]
+    }
+  }
+  statement {
+    effect = "Allow",
+    actions = ["iam:ListMFADevices"]
+    resources = ["*"]
   }
 }
 
 resource "aws_iam_group_policy" "admin" {
   name = "admin"
   group = "${aws_iam_group.admin.name}"
-
-  # Has full access to everything, including IAM management.  Requires MFA.
-  policy = "${data.template_file.admin-group-policy.rendered}"
+  policy = "${data.aws_iam_policy_document.admin.json}"
 }
 
-data "template_file" "power-user-group-policy" {
-  template = "${file("${path.module}/power-user-group-policy-doc.json.tpl")}"
-  vars {
-    account_id = "${data.aws_caller_identity.current.account_id}"
+# Has full access to AWS, _except_ for IAM management (besides their own user,
+# e.g. to change their own password). Requires MFA.
+data "aws_iam_policy_document" "power-user" {
+  statement {
+    effect = "Allow"
+    not_actions = ["iam:*"]
+    resources = ["*"]
+    condition {
+      test = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values = ["true"]
+    }
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:*LoginProfile",
+      "iam:*AccessKey*"
+    ]
+    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/&{aws:username}"]
+    condition {
+      test = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values = ["true"]
+    }
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:ListAccount*",
+      "iam:GetAccount*",
+      "iam:ListUsers",
+      "iam:ListRoles"
+    ],
+    resources = ["*"]
+    condition {
+      test = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values = ["true"]
+    }
+  }
+  statement {
+    effect = "Allow"
+    actions = ["iam:ListMFADevices"]
+    resources = ["*"]
   }
 }
 
 resource "aws_iam_group_policy" "power-user" {
   name = "power-user"
   group = "${aws_iam_group.power-user.name}"
-
-  # Has full access to AWS, _except_ for IAM management (besides their own user,
-  # e.g. to change their own password). Requires MFA.
-  policy = "${data.template_file.power-user-group-policy.rendered}"
+  policy = "${data.aws_iam_policy_document.power-user.json}"
 }
 
-data "template_file" "setup-mfa-group-policy" {
-  template = "${file("${path.module}/setup-mfa-group-policy-doc.json.tpl")}"
-  vars {
-    account_id = "${data.aws_caller_identity.current.account_id}"
+# Only allows a user to set up their MFA device (and does not require
+# MFA to log in). If a user belongs to this group, they should not belong to any
+# other groups (otherwise, MFA is pointless since anyone with their credentials
+# could remove their MFA device).
+data "aws_iam_policy_document" "setup-mfa" {
+  statement {
+    sid = "AllowUsersToCreateDeleteTheirOwnVirtualMFADevices"
+    effect = "Allow"
+    actions = ["iam:*VirtualMFADevice"]
+    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:mfa/&{aws:username}"]
+  }
+  statement {
+    sid = "AllowUsersToEnableSyncDisableTheirOwnMFADevices"
+    effect = "Allow"
+    actions = [
+      "iam:DeactivateMFADevice",
+      "iam:EnableMFADevice",
+      "iam:ListMFADevices",
+      "iam:ResyncMFADevice"
+    ]
+    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/&{aws:username}"]
+  }
+  statement {
+    sid = "AllowUsersToListVirtualMFADevices"
+    effect = "Allow"
+    actions = ["iam:ListVirtualMFADevices"]
+    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:mfa/*"]
+  }
+  statement {
+    sid = "AllowUsersToListUsersInConsole"
+    effect = "Allow"
+    actions = ["iam:ListUsers"]
+    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/*"]
   }
 }
 
 resource "aws_iam_group_policy" "setup-mfa" {
   name = "setup-mfa"
   group = "${aws_iam_group.setup-mfa.id}"
-
-  # This group _only_ allows a user to set up their MFA device (and does not require
-  # MFA to log in). If a user belongs to this group, they should not belong to any
-  # other groups (otherwise, MFA is pointless since anyone with their credentials
-  # could remove their MFA device).
-  policy = "${data.template_file.setup-mfa-group-policy.rendered}"
+  policy = "${data.aws_iam_policy_document.setup-mfa.json}"
 }
 
 #----------------------------------------------------------------------
