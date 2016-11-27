3bb9186bb920c55e0f2e13938d35e82f31f50db6
ketzacoatl <ketzacoatl@users.noreply.github.com>
2016-11-26 23:09:23 -0500

    r53-subdomain: add new module

---
+++ tf-modules/r53-subdomain/main.tf
@@ -0,0 +1,27 @@
+variable "name" {
+    description = "name (FQDN) of the subdomain"
+}
+variable "parent_zone_id" {
+    description = "Zone ID of the parent domain"
+}
+variable "ttl" {
+    default = 172800
+    description = "TTL for the NS records created"
+}
+
+resource "aws_route53_zone" "subdomain" {
+    name = "${var.name}"
+}
+
+resource "aws_route53_record" "subdomain-NS" {
+    zone_id = "${var.parent_zone_id}"
+    name    = "${var.name}"
+    type    = "NS"
+    ttl     = "${var.ttl}"
+    records = [
+        "${aws_route53_zone.subdomain.name_servers.0}",
+        "${aws_route53_zone.subdomain.name_servers.1}",
+        "${aws_route53_zone.subdomain.name_servers.2}",
+        "${aws_route53_zone.subdomain.name_servers.3}",
+    ]
+}
