de8384f8b80a6301e4d3479245dce34310cba5d6
Alexey Kuleshevich <alexey@fpcomplete.com>
2017-01-28 00:39:56 +0300

    renamed grant script and added future cleanup action

--- tf-modules/logstash/iam.tf
+++ tf-modules/logstash/iam.tf
@@ -17,8 +17,16 @@ resource "aws_iam_role_policy_attachment" "credstash-reader-policy-attachment" {
 
 resource "aws_iam_role" "logstash-role" {
   provisioner "local-exec" {
-    command = "../credstash/create-grant.sh create reader ${var.credstash_kms_key_arn} ${aws_iam_role.logstash-role.arn}"
+    command = "../credstash/grant.sh create reader ${var.credstash_kms_key_arn} ${aws_iam_role.logstash-role.arn}"
   }
+  ## Cleanup action that requires terraform 0.9.0
+  ## See RFC: https://docs.google.com/document/d/15nEcV7fxskDgYrXoNMl6RYIo10PCiZGle7TP8xitrFE/edit#
+  ## and: https://docs.google.com/document/d/15nEcV7fxskDgYrXoNMl6RYIo10PCiZGle7TP8xitrFE/edit#
+  # provisioner "local-exec" {
+  #   when       = "destroy"
+  #   on_failure = "continue"
+  #   command    = "../credstash/grant.sh revoke ${var.credstash_kms_key_arn} ${aws_iam_role.logstash-role.arn}"
+  # }
   name_prefix  = "${var.name_prefix}-logstash-role-"
   assume_role_policy = <<END_POLICY
 {
