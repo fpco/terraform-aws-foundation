3de3ac6b97e6bee0bb9ad14c4237cf65fd351a08
Alexey Kuleshevich <alexey@fpcomplete.com>
2017-02-01 22:43:25 +0300

    Added docs for credstash module

--- tf-modules/credstash/main.tf
+++ tf-modules/credstash/main.tf
@@ -1,3 +1,31 @@
+/**
+ *## This module contains helper resources and scripts for using credstash.
+ *
+ * You can use this module to create reader/writer policies, which together
+ * with use `grant.sh` script will let a IAM Role be able read/write credentials
+ * by using credstash. It also includes helper commands for getting/putting
+ * credentials up into DynamoDB
+ */
+
+
+data "template_file" "credstash-get-cmd" {
+  template = "/usr/local/bin/credstash -r ${var.region == "" ? data.aws_region.current.name : var.region} -t ${var.table_name} get"
+}
+
+data "template_file" "credstash-put-cmd" {
+  template = "/usr/local/bin/credstash -r ${var.region == "" ? data.aws_region.current.name : var.region} -t ${var.table_name} put -k ${var.kms_key_arn}"
+}
+
+data "template_file" "credstash-install-snippet" {
+  template = <<END_TEMPLATE
+apt-get update
+apt-get install -y build-essential libssl-dev libffi-dev python-dev python-pip
+pip install --upgrade pip
+pip install credstash
+END_TEMPLATE
+}
+
+
 // Ubuntu bash script snippet for installing credstash
 output "install_snippet" {
   value = "${data.template_file.credstash-install-snippet.rendered}"
@@ -19,19 +47,3 @@ output "writer_policy_arn" {
   value = "${aws_iam_policy.writer-policy.arn}"
 }
 
-data "template_file" "credstash-get-cmd" {
-  template = "/usr/local/bin/credstash -r ${var.region == "" ? data.aws_region.current.name : var.region} -t ${var.table_name} get"
-}
-
-data "template_file" "credstash-put-cmd" {
-  template = "/usr/local/bin/credstash -r ${var.region == "" ? data.aws_region.current.name : var.region} -t ${var.table_name} put -k ${var.kms_key_arn}"
-}
-
-data "template_file" "credstash-install-snippet" {
-  template = <<END_TEMPLATE
-apt-get update
-apt-get install -y build-essential libssl-dev libffi-dev python-dev python-pip
-pip install --upgrade pip
-pip install credstash
-END_TEMPLATE
-}
