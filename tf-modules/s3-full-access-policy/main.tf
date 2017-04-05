53c6a1d31290e8bf42ccdf8320fd3549cc73d542
ketzacoatl <ketzacoatl@users.noreply.github.com>
2017-04-05 12:50:15 -0400

    s3-full-access-policy: convert JSON to use aws_iam_policy_document

--- tf-modules/s3-full-access-policy/main.tf
+++ tf-modules/s3-full-access-policy/main.tf
@@ -20,35 +20,36 @@ output "arn" {
 output "name" {
     value = "${aws_iam_policy.s3-full-access.name}"
 }
+
+data "aws_iam_policy_document" "s3-full-access" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+      "s3:ListBucketMultipartUploads",
+    ]
+
+    resources = ["${join("\",\"",formatlist("arn:aws:s3:::%s",split(",", var.bucket_names)))}"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListObjects",
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:CreateMultipartUpload",
+      "s3:ListMultipartUploadParts",
+      "s3:AbortMultipartUpload"
+    ]
+
+    resources = ["${join("\",\"",formatlist("arn:aws:s3:::%s/*",split(",", var.bucket_names)))}"]
+  }
+}
+
 resource "aws_iam_policy" "s3-full-access" {
     name = "${var.name}_s3_bucket_access"
-    policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:ListBucket",
-                "s3:GetBucketLocation",
-                "s3:ListBucketMultipartUploads"
-            ],
-            "Resource": ["${join("\",\"",formatlist("arn:aws:s3:::%s",split(",", var.bucket_names)))}"]
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:ListObjects",
-                "s3:PutObject",
-                "s3:GetObject",
-                "s3:DeleteObject",
-                "s3:CreateMultipartUpload",
-                "s3:ListMultipartUploadParts",
-                "s3:AbortMultipartUpload"
-            ],
-            "Resource": ["${join("\",\"",formatlist("arn:aws:s3:::%s/*",split(",", var.bucket_names)))}"]
-        }
-    ]
-}
-EOF
+    policy = "${data.aws_iam_policy_document.s3-full-access.json}"
 }
