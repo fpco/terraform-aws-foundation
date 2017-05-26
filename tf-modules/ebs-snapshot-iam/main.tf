3d28c850ef2d1088c2e857a42a8049e54bc639ac
J Boyer <jason@fpcomplete.com>
2017-05-26 13:34:26 -0400

    add module to create IAM for EBS Snapshot Manager

---
+++ tf-modules/ebs-snapshot-iam/main.tf
@@ -0,0 +1,40 @@
+variable "name" {
+  default     = "snap-ebs"
+  description = "name IAM resources, should match service name"
+}
+
+resource "aws_iam_user" "snap-ebs" {
+  name = "${var.name}"
+}
+
+resource "aws_iam_user_policy" "snap-ebs" {
+  name = "${var.name}"
+  user = "${aws_iam_user.snap-ebs.name}"
+  policy = <<EOF
+{
+  "Statement": [
+    {
+      "Action": [
+        "ec2:CreateSnapshot",
+        "ec2:CreateTags",
+        "ec2:DeleteSnapshot",
+        "ec2:DescribeAvailabilityZones",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumeAttribute",
+        "ec2:DescribeVolumeStatus",
+        "ec2:DescribeVolumes"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+// ARN of the IAM user created for the EBS snapshots service
+output "user_arn" {
+  value = "${aws_iam_user.snap-ebs.arn}"
+}
