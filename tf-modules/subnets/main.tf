ca90a57d7412669eadc064436dbfb44376ff5e6e
J Boyer <jason@fpcomplete.com>
2017-06-18 13:22:50 -0400

    subnets: refactor the module
    instead of creating private/public subnets, create only one type at a
    time.

--- tf-modules/subnets/main.tf
+++ tf-modules/subnets/main.tf
@@ -1,27 +1,16 @@
 /**
  *## Subnets creation
  *
- * This module can create public and private subnets while interleaving them
- * throughout supplied availiability zones.
+ * This module creates one or more subnets, interleaving them across a list of
+ * availiability zones.
  *
  */
 
-
-resource "aws_subnet" "public" {
-  count                   = "${length(var.public_subnet_cidrs)}"
-  vpc_id                  = "${var.vpc_id}"
-  cidr_block              = "${var.public_subnet_cidrs[count.index]}"
-  availability_zone       = "${element(var.azs, count.index)}"
-  map_public_ip_on_launch = true
-  tags                    = "${merge(map("Name", "${var.name_prefix}-public-${format("%02d", count.index)}-${element(var.azs, count.index)}"), "${var.extra_tags}")}"
+resource "aws_subnet" "main" {
+  count             = "${length(var.cidr_blocks)}"
+  vpc_id            = "${var.vpc_id}"
+  cidr_block        = "${var.cidr_blocks[count.index]}"
+  availability_zone = "${element(var.azs, count.index)}"
+  tags              = "${merge(map("Name", "${var.name_prefix}-${format("%02d", count.index)}-${element(var.azs, count.index)}"), "${var.extra_tags}")}"
+  map_public_ip_on_launch = "${var.public}"
 }
-
-resource "aws_subnet" "private" {
-  count                   = "${length(var.private_subnet_cidrs)}"
-  vpc_id                  = "${var.vpc_id}"
-  cidr_block              = "${var.private_subnet_cidrs[count.index]}"
-  availability_zone       = "${element(var.azs, count.index)}"
-  map_public_ip_on_launch = false
-  tags                    = "${merge(map("Name", "${var.name_prefix}-private-${format("%02d", count.index)}-${element(var.azs, count.index)}"), "${var.extra_tags}")}"
-}
-
