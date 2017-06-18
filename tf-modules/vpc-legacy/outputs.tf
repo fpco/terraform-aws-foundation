09d1a3c32f51c0baf46feb36007dd1e6da8fd54c
J Boyer <jason@fpcomplete.com>
2017-06-18 14:46:52 -0400

    vpc-legacy: copy existing vpc module, in prep for refactor

---
+++ tf-modules/vpc-legacy/outputs.tf
@@ -0,0 +1,24 @@
+//VPC id
+output "vpc_id" {
+  value = "${aws_vpc.main.id}"
+}
+
+//List of private subnet ids. None created if list is empty.
+output "private_subnet_ids" {
+  value = ["${module.subnets.private_ids}"]
+}
+
+//List of public subnet ids
+output "public_subnet_ids" {
+  value = ["${module.subnets.public_ids}"]
+}
+
+// Route table id associated with public subnets
+output "public_route_table_id" {
+  value = "${aws_route_table.public.id}"
+}
+
+//Internet gateway id
+output "igw_id" {
+  value = "${aws_internet_gateway.main.id}"
+}
