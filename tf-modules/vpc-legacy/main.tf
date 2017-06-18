771d49400a72816252abe0483ad93933548e055f
J Boyer <jason@fpcomplete.com>
2017-06-18 14:46:52 -0400

    vpc-legacy: copy existing vpc module, in prep for refactor

---
+++ tf-modules/vpc-legacy/main.tf
@@ -0,0 +1,74 @@
+/**
+ *## Virtual Private Cloud (VPC)
+ *
+ * This module takes care of VPC deployment. Scenarios 1 and 2 are possible with
+ * this module: http://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_Scenarios.html
+ *
+ */
+resource "aws_vpc" "main" {
+  cidr_block           = "${var.cidr}"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = "${merge(map("Name", "${var.name_prefix}-vpc"), "${var.extra_tags}")}"
+}
+
+resource "aws_vpc_dhcp_options" "main" {
+  domain_name         = "${var.region}.compute.internal"
+  domain_name_servers = ["${var.dns_server_list}"]
+
+  tags = "${merge(map("Name", "${var.name_prefix}-dhcp-options"), "${var.extra_tags}")}"
+}
+
+resource "aws_vpc_dhcp_options_association" "main" {
+  vpc_id          = "${aws_vpc.main.id}"
+  dhcp_options_id = "${aws_vpc_dhcp_options.main.id}"
+}
+
+module "subnets" {
+  source               = "../subnets"
+  azs                  = "${var.azs}"
+  vpc_id               = "${aws_vpc.main.id}"
+  name_prefix          = "${var.name_prefix}"
+  public_subnet_cidrs  = "${var.public_subnet_cidrs}"
+  private_subnet_cidrs = "${var.private_subnet_cidrs}"
+  extra_tags           = "${var.extra_tags}"
+}
+
+
+## Internet Gateway - provide internet access to public subnets.
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = "${merge(map("Name", "${var.name_prefix}-igw"), "${var.extra_tags}")}"
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main.id}"
+  }
+
+  tags = "${merge(map("Name", "${var.name_prefix}-public"), "${var.extra_tags}")}"
+}
+
+resource "aws_route_table_association" "public-rta" {
+  count          = "${length(var.public_subnet_cidrs)}"
+  subnet_id      = "${element(module.subnets.public_ids, count.index)}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+
+## NAT Gateways - provide internet access to private subnets.
+
+module "nat-gateways" {
+  source             = "../nat-gateways"
+  vpc_id             = "${aws_vpc.main.id}"
+  name_prefix        = "${var.name_prefix}"
+  nat_count          = "${var.nat_count}"
+  public_subnet_ids  = ["${module.subnets.public_ids}"]
+  private_subnet_ids = ["${module.subnets.private_ids}"]
+  extra_tags         = "${var.extra_tags}"
+}
---
+++ tf-modules/vpc-legacy/variables.tf
@@ -0,0 +1,39 @@
+variable "name_prefix" {
+  description = "Project name. It will be prepended to all resources."
+}
+
+variable "region" {
+  description = "Region were VPC will be created"
+}
+
+variable "cidr" {
+  description = "CIDR range of VPC. eg: 172.16.0.0/16"
+}
+variable "dns_server_list" {
+  description = "list of IPs to use as DNS servers (in DHCP options for the VPC)"
+  default     = ["AmazonProvidedDNS"]
+}
+variable "public_subnet_cidrs" {
+  type = "list"
+  description = "A list of public subnet CIDRs to deploy inside the VPC."
+}
+
+variable "private_subnet_cidrs" {
+  description = "A list of private subnet CIDRs to deploy inside the VPC. Should not be higher than public subnets count"
+  default = []
+}
+
+variable "azs" {
+  type = "list"
+  description = "A list of Availaiblity Zones in the region"
+}
+
+variable "extra_tags" {
+  description = "Extra tags that will be added to VPC, DHCP Options, Internet Gateway, Subnets and Routing Table."
+  default = {}
+}
+
+variable "nat_count" {
+  description = "Number of NAT gateways to deploy. Should not be higher than the number of private subnets count"
+  default = 0
+}
