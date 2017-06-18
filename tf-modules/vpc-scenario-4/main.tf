7b5851ec5f2c4499abfb8dff9541aa76ca587830
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2017-06-18 17:23:42 -0400

    vpc-scenario-4: add new module with VPC for AWS Network Scenario 4
    * VPC w/ DHCP options
    * Only private subnets (no public)
    * Hardware VPN, no Internet Gateway
    
    This module creates a VPC that has no direct access to the public
    internet, and instead uses a VPN to a corporate network for linking
    to external networks.
    
    See the AWS docs for more info about this scenario:
    http://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_Scenario4.html

---
+++ tf-modules/vpc-scenario-4/main.tf
@@ -0,0 +1,55 @@
+/**
+ *## AWS VPC, Scenario 4: VPC w/ Private Subnets, and VPN only
+ *
+ * This module creates a VPC with private subnets across one or more availability
+ * zones, a hardware VPN gateway, and a route table for those subnets to pass
+ * traffic through the gateway. No NAT or public subnets/gateway.
+ *
+ * Scenario 4 from AWS docs:
+ * http://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_Scenarios.html
+ *
+ */
+
+module "vpc" {
+  source               = "../vpc"
+  region               = "${var.region}"
+  cidr                 = "${var.cidr}"
+  name_prefix          = "${var.name_prefix}"
+  enable_dns_hostnames = "${var.enable_dns_hostnames}"
+  enable_dns_support   = "${var.enable_dns_support}"
+  dns_servers          = ["${var.dns_servers}"]
+  extra_tags           = "${var.extra_tags}"
+}
+
+module "private-subnets" {
+  source      = "../subnets"
+  azs         = "${var.azs}"
+  vpc_id      = "${module.vpc.id}"
+  name_prefix = "${var.name_prefix}-private"
+  cidr_blocks = "${var.private_subnet_cidrs}"
+  extra_tags  = "${var.extra_tags}"
+}
+
+## TODO: Move these next two resources into the IPSEC/VPN module..?
+# Route private subnets through the VPN gateway
+resource "aws_route_table" "private-vpn" {
+  vpc_id           = "${module.vpc.id}"
+  propagating_vgws = ["${module.vpn.vpn_gw_id}"]
+
+  tags = {
+    Name = "${var.name_prefix}-private-vpn"
+  }
+}
+resource "aws_route_table_association" "private-vpn" {
+  count          = "${length(module.private-subnets.ids)}"
+  subnet_id      = "${element(module.private-subnet.ids, count.index)}"
+  route_table_id = "${aws_route_table.private-vpn.id}"
+}
+
+module "vpn" {
+  source           = "../aws-ipsec-vpn"
+  vpc_id           = "${module.vpc.id}"
+  extra_tags       = "${var.extra_tags}"
+  remote_device_ip = "${var.vpn_remote_ip}"
+  static_routes    = ["${var.vpn_static_routes}"]
+}
---
+++ tf-modules/vpc-scenario-4/variables.tf
@@ -0,0 +1,54 @@
+variable "name_prefix" {
+  description = "Name to prefix various resources with"
+}
+
+variable "region" {
+  description = "Region were VPC will be created"
+}
+
+variable "cidr" {
+  description = "CIDR range of VPC. eg: 172.16.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type = "list"
+  description = "A list of public subnet CIDRs to deploy inside the VPC."
+}
+
+variable "private_subnet_cidrs" {
+  description = "A list of private subnet CIDRs to deploy inside the VPC. Should not be higher than public subnets count"
+  default = []
+}
+
+variable "azs" {
+  type = "list"
+  description = "A list of Availaiblity Zones in the region"
+}
+
+variable "extra_tags" {
+  description = "Extra tags that will be added to VPC, DHCP Options, Internet Gateway, Subnets and Routing Table."
+  default = {}
+}
+
+variable "enable_dns_hostnames" {
+  default     = true
+  description = "boolean, enable/disable VPC attribute, enable_dns_hostnames"
+}
+
+variable "enable_dns_support" {
+  default     = true
+  description = "boolean, enable/disable VPC attribute, enable_dns_support"
+}
+
+variable "dns_servers" {
+  default     = ["AmazonProvidedDNS"]
+  description = "list of DNS servers"
+}
+
+variable "vpn_static_routes" {
+  description = "list of static routes to use with AWS customer gateway (VPN)"
+}
+
+variable "vpn_remote_ip" {
+  description = "IP address of the remote VPN for AWS to associate with"
+}
