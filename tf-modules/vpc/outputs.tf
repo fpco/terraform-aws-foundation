b51cdee480c46a366c76966ccd3d6f5563c5d9b5
J Boyer <jason@fpcomplete.com>
2017-06-18 15:15:04 -0400

    vpc: refactor the module
    let vpc-legacy live on as this module used to be, make this module more
    focused on a) the VPC, and b) DHCP Options Set for the VPC.
    
    Leave all the subnet, gateway, routing table, and related networking
    resources to other modules that combine these resources in specific
    ways (rather than a single VPC module which attempts to do it all).

--- tf-modules/vpc/outputs.tf
+++ tf-modules/vpc/outputs.tf
@@ -1,24 +1,12 @@
-//VPC id
-output "vpc_id" {
+// VPC ID
+output "id" {
   value = "${aws_vpc.main.id}"
 }
-
-//List of private subnet ids. None created if list is empty.
-output "private_subnet_ids" {
-  value = ["${module.subnets.private_ids}"]
+// VPC CIDR block
+output "cidr_block" {
+  value = "${aws_vpc.main.cidr_block}"
 }
-
-//List of public subnet ids
-output "public_subnet_ids" {
-  value = ["${module.subnets.public_ids}"]
-}
-
-// Route table id associated with public subnets
-output "public_route_table_id" {
-  value = "${aws_route_table.public.id}"
-}
-
-//Internet gateway id
-output "igw_id" {
-  value = "${aws_internet_gateway.main.id}"
+// ID of the DHCP options resource
+output "dhcp_options_id" {
+  value = "${aws_vpc_dhcp_options.main.id}"
 }
