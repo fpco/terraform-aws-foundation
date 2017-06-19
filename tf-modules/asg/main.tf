8f42cb345513f3d83ee14f9f510ed9ff724b434e
J Boyer <jason@fpcomplete.com>
2017-06-19 13:41:36 -0400

    asg: whitespace fixup for main.tf

--- tf-modules/asg/main.tf
+++ tf-modules/asg/main.tf
@@ -12,36 +12,36 @@
  */
 # Auto-Scaling Group
 resource "aws_autoscaling_group" "cluster" {
-    desired_capacity = "${var.desired_capacity}"
-    force_delete = true
-    health_check_grace_period = 300
-    health_check_type = "EC2"
-    launch_configuration = "${aws_launch_configuration.cluster.name}"
-    load_balancers =  ["${var.elb_names}"]
-    max_size = "${var.max_nodes}"
-    min_size = "${var.min_nodes}"
-    name = "${var.name}-${var.suffix}"
-    lifecycle {
-      create_before_destroy = true
-    }
-    vpc_zone_identifier = ["${var.subnet_ids}"]
   availability_zones        = ["${var.azs}"]
+  desired_capacity          = "${var.desired_capacity}"
+  force_delete              = true
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+  launch_configuration      = "${aws_launch_configuration.cluster.name}"
+  load_balancers            = ["${var.elb_names}"]
+  max_size                  = "${var.max_nodes}"
+  min_size                  = "${var.min_nodes}"
+  name                      = "${var.name}-${var.suffix}"
+  lifecycle {
+    create_before_destroy = true
+  }
+  vpc_zone_identifier = ["${var.subnet_ids}"]
 }
 # Launch Config for the ASG
 resource "aws_launch_configuration" "cluster" {
-    # omit name so it's generated as a unique value
-    associate_public_ip_address = "${var.public_ip}"
-    iam_instance_profile = "${var.iam_profile}"
-    image_id = "${var.ami}"
-    instance_type = "${var.instance_type}"
-    key_name = "${var.key_name}"
-    security_groups = ["${var.security_group_ids}"]
-    user_data = "${var.user_data}"
-    root_block_device {
-        volume_type = "${var.root_volume_type}"
-        volume_size = "${var.root_volume_size}"
-    }
-    lifecycle {
-      create_before_destroy = true
-    }
+  # omit name so it's generated as a unique value
+  associate_public_ip_address = "${var.public_ip}"
+  iam_instance_profile        = "${var.iam_profile}"
+  image_id                    = "${var.ami}"
+  instance_type               = "${var.instance_type}"
+  key_name                    = "${var.key_name}"
+  security_groups             = ["${var.security_group_ids}"]
+  user_data                   = "${var.user_data}"
+  root_block_device {
+    volume_type = "${var.root_volume_type}"
+    volume_size = "${var.root_volume_size}"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
