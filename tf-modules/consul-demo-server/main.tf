6ab0ff0682969c1450de2c5ab7d586c8fb32fc35
J Boyer <jason@fpcomplete.com>
2017-07-15 12:16:11 -0400

    consul-demo-server: add new module
    basic module to run a "demo server" - a single consul node w/ data
    persistence on EBS, and semi-HA provided by ASG. wrap the new single-
    node-asg module and fit the details to consul. For now, leave "install
    and run" details to the module user.

---
+++ tf-modules/consul-demo-server/main.tf
@@ -0,0 +1,33 @@
+/**
+ *## Consul Demo Server
+ *
+ * Run an instance of consul in server mode, on a single-node auto-scaling group.
+ * Use an EBS volume for data persistence.
+ *
+ */
+module "consul-server" {
+  source                  = "../single-node-asg"
+  name                    = "${var.name}"
+  name_suffix             = "consul-server"
+  region                  = "${var.region}"
+  az                      = "${var.az}"
+  root_volume_type        = "gp2"
+  root_volume_size        = "8"
+  data_volume_type        = "gp2"
+  data_volume_size        = "${var.data_volume_size}"
+  data_volume_encrypted   = "${var.data_volume_encrypted}"
+  data_volume_kms_key_id  = "${var.data_volume_kms_key_id}"
+  data_volume_snapshot_id = "${var.data_volume_snapshot_id}"
+  key_name                = "${var.key_name}"
+  key_file                = "${var.key_file}"
+  ami                     = "${var.ami}"
+  instance_type           = "${var.instance_type}"
+  load_balancers          = "${var.load_balancers}"
+  public_ip               = "${var.public_ip}"
+  subnet_id               = "${var.subnet_id}"
+  load_balancers          = "${var.load_balancers}"
+  security_group_ids      = "${var.security_group_ids}"
+  init_suffix = <<END_INIT
+${var.init_suffix}
+END_INIT
+}
---
+++ tf-modules/consul-demo-server/variables.tf
@@ -0,0 +1,56 @@
+variable "name" {
+  description = "The name of the environment to deploy to (beta/prod/etc)"
+}
+variable "key_name" {
+  description = "The name of the (AWS) SSH key to associate with the instance"
+}
+variable "key_file" {
+  description = "Path to the SSH private key to provide connection info as output"
+}
+variable "ami" {
+  description = "The base AMI for each AWS instance created"
+}
+variable "instance_type" {
+  default = "t2.small"
+  description = "The type of AWS instance (size)"
+}
+variable "data_volume_size" {
+  default = "2"
+  description = "Size (in GB) of EBS volume to use for the EBS volume"
+}
+variable "data_volume_encrypted" {
+  default = "false"
+  description = "Boolean, whether or not to encrypt the EBS block device"
+}
+variable "data_volume_kms_key_id" {
+  default = ""
+  description = "ID of the KMS key to use when encyprting the EBS block device"
+}
+variable "data_volume_snapshot_id" {
+  default = ""
+  description = "The ID of the snapshot to base the EBS block device on"
+}
+variable "init_suffix" {
+  default = ""
+  description = "init shell to run after setting VOLUME_ID and REGION exports"
+}
+variable "public_ip" {
+  default = "false"
+  description = "Boolean flag to enable/disable `map_public_ip_on_launch` in the launch configuration"
+}
+variable "subnet_id" {
+  description = "The ID of the subnet to use, depends on the availability zone"
+}
+variable "security_group_ids" {
+  description = "string list of security groups (by ID) to associate with the ASG"
+}
+variable "region" {
+  description = "The AWS region to deploy to"
+}
+variable "az" {
+  description = "The AWS Availability Zone (AZ) to create the instance in"
+}
+variable "load_balancers" {
+  default = ""
+  description = "The string list of load balancers names to pass to the ASG module"
+}
