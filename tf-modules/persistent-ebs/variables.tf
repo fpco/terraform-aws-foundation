85e4f0f0cb9902b789d271011570ef587ab58345
J Boyer <jason@fpcomplete.com>
2017-07-14 23:13:42 -0400

    persistent-ebs: add support for extra_tags
    on the EBS volume in particular

--- tf-modules/persistent-ebs/variables.tf
+++ tf-modules/persistent-ebs/variables.tf
@@ -32,6 +32,9 @@ variable "iops" {
     default = ""
     description = "The amount of IOPS to provision for the EBS block device"
 }
+variable "extra_tags" {
+  default = {}
+  description = "Map with extra tags to be applied to all ebs volumes"
 }
 variable "extra_tags" {
   default = {}
