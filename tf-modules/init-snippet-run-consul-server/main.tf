b88bb30acf3e95acb2c796dafaafc1e6b6931d0c
J Boyer <jason@fpcomplete.com>
2017-07-15 12:14:10 -0400

    init-snippet/run-consul-server: add new module
    basic single-node server config for now - basic, basic, basic.
    run the web ui and include support for gossip encryption.

---
+++ tf-modules/init-snippet-run-consul-server/main.tf
@@ -0,0 +1,43 @@
+# variables used by this snippet of init shellcode
+variable "encrypt" {
+  description = "base64 encoded key for consul gossip encryption"
+}
+variable "data_dir" {
+  default = "/opt/consul"
+  description = "path on consul server for -data-dir"
+}
+variable "datacenter" {
+  description = "name of consul datacenter, maps to -datacenter config"
+}
+variable "config_file" {
+  default     = "/etc/consul.json"
+  description = "path to consul config file"
+}
+variable "init_prefix" {
+  default = ""
+  description = "initial init (shellcode) to prefix this snippet with"
+}
+variable "init_suffix" {
+  default = ""
+  description = "init (shellcode) to append to the end of this snippet"
+}
+variable "log_prefix" {
+  default = "OPS: "
+  description = "string to prefix log messages with"
+}
+# render init script snippet from the template
+data "template_file" "init_snippet" {
+  template = "${file("${path.module}/snippet.tpl")}"
+  vars {
+    encrypt     = "${var.encrypt}"
+    data_dir    = "${var.data_dir}"
+    datacenter  = "${var.datacenter}"
+    config_file = "${var.config_file}"
+    init_prefix = "${var.init_prefix}"
+    init_suffix = "${var.init_suffix}"
+    log_prefix  = "${var.log_prefix}"
+  }
+}
+output "init_snippet" {
+  value = "${data.template_file.init_snippet.rendered}"
+}
---
+++ tf-modules/init-snippet-run-consul-server/snippet.tpl
@@ -0,0 +1,23 @@
+# start snippet - configure and run consul
+${init_prefix}
+mkdir /etc/consul.d
+cat <<EOT >> ${config_file}
+{
+  "datacenter": "${datacenter}",
+  "data_dir": "${data_dir}",
+  "bind_addr": "$(ec2metadata --local-ipv4)",
+  "addresses": {
+    "http": "$(ec2metadata --local-ipv4)",
+    "dns": "127.0.0.1"
+  },
+  "server": true,
+  "bootstrap_expect": 1,
+  "ui_dir": "${data_dir}/ui",
+  "disable_remote_exec": true,
+  "disable_update_check": true,
+  "disable_anonymous_signature": true,
+  "encrypt": "${encrypt}"
+}
+EOT
+consul agent -config-file ${config_file} -config-dir /etc/consul.d
+${init_suffix}
