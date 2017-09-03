3bd22c5dea2452e2904b88504667279a4b0ce262
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2017-09-03 17:06:41 -0400

    snapshot-bucket: add `aws_cloud` variable

--- tf-modules/snapshot-bucket/variables.tf
+++ tf-modules/snapshot-bucket/variables.tf
@@ -1,3 +1,7 @@
 variable "name" {
     description = "Name to give bucket"
 }
+variable "aws_cloud" {
+  description = "set to 'aws-us-gov' if using GovCloud, otherwise leave the default"
+  default     = "aws"
+}
