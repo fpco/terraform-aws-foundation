7d2d3fd2dc1633b7d2fc49c99daea71a94434cdc
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2017-09-05 13:25:53 -0400

    vpc-scenario-1: add docs to vpc-scenario-1 module to demo order of operations

--- tf-modules/vpc-scenario-1/main.tf
+++ tf-modules/vpc-scenario-1/main.tf
@@ -8,6 +8,17 @@
  * Scenario 1 from AWS docs:
  * http://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_Scenarios.html
  *
+ * Note that, when using this module and deploying the VPC for the first time,
+ * Terraform needs the user to add the VPC, Subnets, and then Route Tables. For
+ * example, use targets to apply these updates sequentially:
+ *
+ *     ᐅ tfp -out=tf.out -target=module.vpc.module.vpc
+ *     ᐅ tfa tf.out
+ *     ᐅ tfp -out=tf.out -target=module.vpc.module.public-subnets
+ *     ᐅ tfa tf.out
+ *     ᐅ tfp -out=tf.out -target=module.vpc.module.public-gateway
+ *     ᐅ tfa tf.out
+ *
  */
 
 module "vpc" {
