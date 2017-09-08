0a07e85c68f1643c06647a4862c51f4562f50642
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2017-09-08 03:04:55 -0400

    add initial test env for kube-stack module
    (include 2, one each for public/private topology)

---
+++ tests/kube-stack-private/output.tf
@@ -0,0 +1,24 @@
+// IP address of the bastion jump box
+output "bastion_ip" {
+    value = "${aws_instance.bastion.public_ip}"
+}
+// AWS region of deployment
+output "region" {
+  value = "${var.region}"
+}
+// Id of the created VPC
+output "vpc_id" {
+  value = "${module.vpc.vpc_id}"
+}
+// Ids of created public subnets
+output "public_subnet_ids" {
+  value = ["${module.vpc.public_subnet_ids}"]
+}
+// Ids of created private subnets
+output "private_subnet_ids" {
+  value = ["${module.vpc.private_subnet_ids}"]
+}
+// SSH Key pair name
+output "ssh_key_name" {
+  value = "${aws_key_pair.main.key_name}"
+}
---
+++ tests/kube-stack-public/data.tf
@@ -0,0 +1,38 @@
+data "aws_availability_zones" "available" {}
+
+data "aws_ami" "coreos-stable" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["CoreOS-stable-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  owners = ["${var.coreos_account_id}"]
+}
+
+data "aws_ami" "ubuntu-xenial" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["${var.canonical_account_id}"]
+}
