3f2acd0f54a05fab42a0355f7506d1fa41308ab3
Yghor Kerscher <kerscher@acm.org>
2017-09-26 17:38:48 +0200

    elk-stack: use `ami-ubuntu` module.

--- tf-modules/elk-stack/main.tf
+++ tf-modules/elk-stack/main.tf
@@ -27,6 +27,10 @@ data "aws_ami" "ubuntu" {
     name   = "root-device-type"
     values = ["ebs"]
   }
+module "ubuntu-ami" {
+  source      = "../ami-ubuntu"
+  release     = "16.04"
+  is_govcloud = "${var.is_govcloud}"
 }
 
 data "aws_vpc" "current" {
@@ -56,6 +60,7 @@ resource "aws_security_group" "ssh" {
 module "elasticsearch" {
   source = "../elasticsearch"
 
+  is_govcloud                 = "${var.is_govcloud}"
   name_prefix                 = "${var.name_prefix}"
   vpc_id                      = "${var.vpc_id}"
   key_name                    = "${var.ssh_key_name}"
@@ -63,7 +68,7 @@ module "elasticsearch" {
   private_subnet_ids          = ["${var.private_subnet_ids}"]
   extra_sg_ids                = ["${aws_security_group.ssh.id}"]
   auth_elb_ingress_cidrs      = ["${var.elasticsearch_auth_elb_ingress_cidrs}"]
-  node_ami                    = "${data.aws_ami.ubuntu.id}"
+  node_ami                    = "${module.ubuntu-ami.id}"
   elasticsearch_dns_name      = "${var.elasticsearch_dns_name}"
   elasticsearch_dns_ssl_name  = "${var.elasticsearch_dns_ssl_name}"
   data_node_count             = "${var.elasticsearch_data_node_count}"
@@ -116,7 +121,7 @@ module "logstash-kibana" {
   public_subnet_ids           = ["${var.public_subnet_ids}"]
   private_subnet_ids          = ["${var.private_subnet_ids}"]
   logstash_dns_name           = "${var.logstash_dns_name}"
-  ami                         = "${data.aws_ami.ubuntu.id}"
+  ami                         = "${module.ubuntu-ami.id}"
   instance_type               = "${var.logstash_kibana_instance_type}"
   key_name                    = "${var.ssh_key_name}"
   elasticsearch_url           = "http://${var.elasticsearch_dns_name}:9200"
