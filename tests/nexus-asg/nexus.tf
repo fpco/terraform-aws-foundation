15abab5ebf87d08c1a12958066c8a270f2372d75
Tom Sydney Kerckhove <syd@fpcomplete.com>
2017-10-06 18:07:15 +0200

    A single-node autoscaling group running Nexus

--- tests/nexus-asg/nexus.tf
+++ tests/nexus-asg/nexus.tf
@@ -1,13 +1,15 @@
 variable "region" {
-  default = "us-east-1"
+  description = "The region to put resources in"
+  default     = "us-east-1"
 }
 
 variable "az" {
-  default = "us-east-1a"
+  description = "The availability zone to put resources in"
+  default     = "us-east-1a"
 }
 
 variable "key_name" {
-  description = "Keypair name to log into the ec2 instance"
+  description = "The keypair used to ssh into the asg intances"
 }
 
 variable "key_file" {
@@ -16,28 +18,27 @@ variable "key_file" {
 }
 
 module "setup" {
-  name               = "test"
-  source             = "../../tf-modules/gitlab-single-node-asg"
+  source             = "../../tf-modules/nexus-single-node-asg"
   region             = "${var.region}"
   az                 = "${var.az}"
   key_file           = "${var.key_file}"
   key_name           = "${var.key_name}"
   subnet_id          = "${module.vpc.public_subnet_ids[0]}"
+  security_group_ids = ["${aws_security_group.nexus-asg.id}"]
   vpc_id             = "${module.vpc.vpc_id}"
-  security_group_ids = ["${aws_security_group.gitlab-asg.id}"]
 }
 
 module "vpc" {
   source              = "../../tf-modules/vpc-scenario-1"
   azs                 = ["${var.az}"]
-  name_prefix         = "gitlab-single-node-asg"
+  name_prefix         = "nexus-single-node-asg"
   cidr                = "192.168.0.0/16"
   public_subnet_cidrs = ["192.168.0.0/16"]
   region              = "${var.region}"
 }
 
-resource "aws_security_group" "gitlab-asg" {
-  name        = "gitlab-asg"
+resource "aws_security_group" "nexus-asg" {
+  name        = "nexus-asg"
   vpc_id      = "${module.vpc.vpc_id}"
   description = "Security group for the single-node autoscaling group"
 
@@ -49,22 +50,8 @@ resource "aws_security_group" "gitlab-asg" {
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8022
-    to_port     = 8022
+    from_port   = 8081
+    to_port     = 8081
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
