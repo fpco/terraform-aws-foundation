f6ae5993b79ee52ee5cf0161cc3a77a6faa9ffe8
user <ketzacoatl@users.noreply.github.com>
2017-11-09 11:15:04 -0500

    consul-leader-wan-sg: whitespace fixup

--- tf-modules/consul-leader-wan-sg/outputs.tf
+++ tf-modules/consul-leader-wan-sg/outputs.tf
@@ -1,12 +1,14 @@
 //`id`, exported from the `aws_security_group`
 output "id" {
-    value = "${aws_security_group.main.id}"
+  value = "${aws_security_group.main.id}"
 }
+
 //`name`, exported from the `aws_security_group`
 output "name" {
-    value = "${aws_security_group.main.name}"
+  value = "${aws_security_group.main.name}"
 }
+
 //`ingress`, exported from the `aws_security_group`
 output "ingress" {
-    value = "${aws_security_group.main.ingress}"
+  value = "${aws_security_group.main.ingress}"
 }
--- tf-modules/consul-leader-wan-sg/variables.tf
+++ tf-modules/consul-leader-wan-sg/variables.tf
@@ -1,14 +1,17 @@
 variable "name" {
-    description = "A short identifier to use in the name of the security group"
+  description = "A short identifier to use in the name of the security group"
 }
+
 variable "vpc_id" {
-    description = "The ID of the VPC to associate the security group with"
+  description = "The ID of the VPC to associate the security group with"
 }
+
 variable "cidr_blocks" {
-    description = "The list of CIDR IP blocks allowed to access the consul WAN port"
-    type        = "list"
+  description = "The list of CIDR IP blocks allowed to access the consul WAN port"
+  type        = "list"
 }
+
 variable "wan_port" {
-    default = "8302"
-    description = "The port to use for consul WAN"
+  default     = "8302"
+  description = "The port to use for consul WAN"
 }
