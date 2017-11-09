7a8dce35fc03768eab26c1622a6ca834f6f81d4c
user <ketzacoatl@users.noreply.github.com>
2017-11-09 01:59:12 -0500

    ssh-sg: whitespace fixup

--- tf-modules/ssh-sg/variables.tf
+++ tf-modules/ssh-sg/variables.tf
@@ -1,10 +1,12 @@
 variable "name" {
-    description = "The name prefix for this SSH security group"
+  description = "The name prefix for this SSH security group"
 }
+
 variable "allowed_cidr_blocks" {
-    description = "The CIDR block to allow access to"
-    default     = ["0.0.0.0/0"]
+  description = "The CIDR block to allow access to"
+  default     = ["0.0.0.0/0"]
 }
+
 variable "vpc_id" {
-    description = "The ID of the VPC to deploy to"
+  description = "The ID of the VPC to deploy to"
 }
