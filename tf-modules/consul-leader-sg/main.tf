4642a82922646ed929a0a0d02cd2e3a9b90cca91
user <ketzacoatl@users.noreply.github.com>
2017-11-08 19:51:17 -0500

    consul-leader-sg: whitespace fixup

--- tf-modules/consul-leader-sg/main.tf
+++ tf-modules/consul-leader-sg/main.tf
@@ -9,62 +9,71 @@
  *
  */
 resource "aws_security_group" "main" {
-    vpc_id = "${var.vpc_id}"
-    tags {
-        Name = "${var.name}-consul-leader"
-        Description = "Allow TCP and UDP ports to consul leaders in ${var.name}"
-    }
-    # Server RPC, used by servers to handle incoming requests from other agents. TCP only.
-    ingress {
-        from_port = 8300
-        to_port = 8300
-        protocol = "tcp"
-        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
-    }
-    ingress {
-        from_port = 8300
-        to_port = 8300
-        protocol = "udp"
-        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
-    }
-    # Serf LAN, used to handle gossip in the LAN. Required by all agents. TCP and UDP.
-    ingress {
-        from_port = 8301
-        to_port = 8301
-        protocol = "tcp"
-        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
-    }
-    ingress {
-        from_port = 8301
-        to_port = 8301
-        protocol = "udp"
-        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
-    }
-    # consul CLI RPC, used by all agents to handle RPC from the CLI. TCP only.
-    ingress {
-        from_port = 8400
-        to_port = 8400
-        protocol = "tcp"
-        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
-    }
-    # HTTP API, used by clients to talk to the HTTP API. TCP only.
-    ingress {
-        from_port = 8500
-        to_port = 8500
-        protocol = "tcp"
-        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
-    }
-    # DNS Interface, used to resolve DNS queries. TCP and UDP.
-    ingress {
-        from_port = 8600
-        to_port = 8600
-        protocol = "tcp"
-        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
-    }
-    ingress {
-        from_port = 8600
-        to_port = 8600
-        protocol = "udp"
-        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
-    }
+  vpc_id = "${var.vpc_id}"
+
+  tags {
+    Name        = "${var.name}-consul-leader"
+    Description = "Allow TCP and UDP ports to consul leaders in ${var.name}"
+  }
+
+  # Server RPC, used by servers to handle incoming requests from other agents. TCP only.
+  ingress {
+    from_port   = 8300
+    to_port     = 8300
+    protocol    = "tcp"
+    cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
+  }
+
+  ingress {
+    from_port   = 8300
+    to_port     = 8300
+    protocol    = "udp"
+    cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
+  }
+
+  # Serf LAN, used to handle gossip in the LAN. Required by all agents. TCP and UDP.
+  ingress {
+    from_port   = 8301
+    to_port     = 8301
+    protocol    = "tcp"
+    cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
+  }
+
+  ingress {
+    from_port   = 8301
+    to_port     = 8301
+    protocol    = "udp"
+    cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
+  }
+
+  # consul CLI RPC, used by all agents to handle RPC from the CLI. TCP only.
+  ingress {
+    from_port   = 8400
+    to_port     = 8400
+    protocol    = "tcp"
+    cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
+  }
+
+  # HTTP API, used by clients to talk to the HTTP API. TCP only.
+  ingress {
+    from_port   = 8500
+    to_port     = 8500
+    protocol    = "tcp"
+    cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
+  }
+
+  # DNS Interface, used to resolve DNS queries. TCP and UDP.
+  ingress {
+    from_port   = 8600
+    to_port     = 8600
+    protocol    = "tcp"
+    cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
+  }
+
+  ingress {
+    from_port   = 8600
+    to_port     = 8600
+    protocol    = "udp"
+    cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
+  }
 }
--- tf-modules/consul-leader-sg/variables.tf
+++ tf-modules/consul-leader-sg/variables.tf
@@ -1,10 +1,11 @@
 variable "name" {
-    description = "A short identifier to use in the name of the security group"
-}
-variable "vpc_id" {
-    description = "The ID of the VPC to associate the security group with"
-}
-variable "cidr_blocks" {
-    description = "The list of CIDR IP blocks allowed to access the consul ports (as a string)"
+  description = "A short identifier to use in the name of the security group"
 }
 
+variable "vpc_id" {
+  description = "The ID of the VPC to associate the security group with"
+}
+
+variable "cidr_blocks" {
+  description = "The list of CIDR IP blocks allowed to access the consul ports (as a string)"
+}
