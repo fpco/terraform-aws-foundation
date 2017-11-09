a6e9fe80f489b8b86169437bfc64c8c93436f543
user <ketzacoatl@users.noreply.github.com>
2017-11-09 11:10:36 -0500

    consul-agent-sg: whitespace fixup

--- tf-modules/consul-agent-sg/main.tf
+++ tf-modules/consul-agent-sg/main.tf
@@ -27,22 +27,25 @@
  */
 # Security group to cover Consul Agent
 resource "aws_security_group" "main" {
-    vpc_id = "${var.vpc_id}"
-    tags {
-        Name = "${var.name}-consul-agent"
-        Description = "Allow TCP and UDP ports to consul agent in ${var.name}"
-    }
-    # Serf LAN, used to handle gossip in the LAN. Required by all agents. TCP and UDP.
-    ingress {
-        from_port = 8301
-        to_port = 8301
-        protocol = "tcp"
-        cidr_blocks = ["${var.cidr_blocks}"]
-    }
-    ingress {
-        from_port = 8301
-        to_port = 8301
-        protocol = "udp"
-        cidr_blocks = ["${var.cidr_blocks}"]
-    }
+  vpc_id = "${var.vpc_id}"
+
+  tags {
+    Name        = "${var.name}-consul-agent"
+    Description = "Allow TCP and UDP ports to consul agent in ${var.name}"
+  }
+
+  # Serf LAN, used to handle gossip in the LAN. Required by all agents. TCP and UDP.
+  ingress {
+    from_port   = 8301
+    to_port     = 8301
+    protocol    = "tcp"
+    cidr_blocks = ["${var.cidr_blocks}"]
+  }
+
+  ingress {
+    from_port   = 8301
+    to_port     = 8301
+    protocol    = "udp"
+    cidr_blocks = ["${var.cidr_blocks}"]
+  }
 }
--- tf-modules/consul-agent-sg/outputs.tf
+++ tf-modules/consul-agent-sg/outputs.tf
@@ -1,12 +1,14 @@
 //Security group ID
 output "id" {
-    value = "${aws_security_group.main.id}"
+  value = "${aws_security_group.main.id}"
 }
+
 //Security group name
 output "name" {
-    value = "${aws_security_group.main.name}"
+  value = "${aws_security_group.main.name}"
 }
+
 //Security group ingress rules
 output "ingress" {
-    value = "${aws_security_group.main.ingress}"
+  value = "${aws_security_group.main.ingress}"
 }
