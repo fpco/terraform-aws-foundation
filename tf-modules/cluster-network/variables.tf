e6180d8780ed469f46fd3f2626d00a7866caa0f7
user <ketzacoatl@users.noreply.github.com>
2017-11-11 11:57:58 -0500

    cluster-network: whitespace fixup

--- tf-modules/cluster-network/variables.tf
+++ tf-modules/cluster-network/variables.tf
@@ -1,30 +1,37 @@
 variable "name" {
-    default = ""
-    description = "The name of this network"
+  default     = ""
+  description = "The name of this network"
 }
+
 variable "description" {
-    default = ""
-    description = "The description to give the subnets created for this network"
+  default     = ""
+  description = "The description to give the subnets created for this network"
 }
+
 variable "cidr_a" {
-    default = ""
-    description = "The CIDR block for subnet a, eg: 10.100.7.0/24"
+  default     = ""
+  description = "The CIDR block for subnet a, eg: 10.100.7.0/24"
 }
+
 variable "cidr_c" {
-    default = ""
-    description = "The CIDR block for subnet c, eg: 10.100.8.0/24"
+  default     = ""
+  description = "The CIDR block for subnet c, eg: 10.100.8.0/24"
 }
+
 variable "region" {
-    description = "AWS region to deploy to"
-    default = ""
+  description = "AWS region to deploy to"
+  default     = ""
 }
+
 variable "vpc_id" {
-    description = "The ID of the VPC to deploy to"
+  description = "The ID of the VPC to deploy to"
 }
+
 variable "route_table_id" {
-    description = "The ID of the routing table to use"
+  description = "The ID of the routing table to use"
 }
+
 variable "public_ip" {
-    default = "true"
-    description = "Boolean flag to enable/disable `map_public_ip_on_launch` in each `aws_subnet`"
+  default     = "true"
+  description = "Boolean flag to enable/disable `map_public_ip_on_launch` in each `aws_subnet`"
 }
