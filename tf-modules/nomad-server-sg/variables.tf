9592303c6d62aa29eaf70ba85c72a2e806fb4a10
user <ketzacoatl@users.noreply.github.com>
2017-11-12 11:02:47 -0500

    nomad-server-sg: whitespace fixup

--- tf-modules/nomad-server-sg/variables.tf
+++ tf-modules/nomad-server-sg/variables.tf
@@ -1,14 +1,16 @@
 variable "name" {
-    default = "test"
-    description = "A short identifier to use in the name of the security group"
-}
-variable "vpc_id" {
-    description = "The ID of the VPC to deploy to"
-}
-variable "server_cidr_blocks" {
-    description = "The list of CIDR IP blocks where nomad servers run (as a string-list)"
-}
-variable "worker_cidr_blocks" {
-    description = "The list of CIDR IP blocks where nomad agents run (as a string-list)"
+  default     = "test"
+  description = "A short identifier to use in the name of the security group"
 }
 
+variable "vpc_id" {
+  description = "The ID of the VPC to deploy to"
+}
+
+variable "server_cidr_blocks" {
+  description = "The list of CIDR IP blocks where nomad servers run (as a string-list)"
+}
+
+variable "worker_cidr_blocks" {
+  description = "The list of CIDR IP blocks where nomad agents run (as a string-list)"
+}
