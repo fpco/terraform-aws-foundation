4564c75e8621699b7df328107f01c9a6f8507a60
user <ketzacoatl@users.noreply.github.com>
2017-11-12 11:02:22 -0500

    nomad-agent-sg: whitespace fixup

--- tf-modules/nomad-agent-sg/main.tf
+++ tf-modules/nomad-agent-sg/main.tf
@@ -6,23 +6,25 @@
  * NOTE: need to make the agent port range (20000 to 60000) a variable.
  */
 resource "aws_security_group" "main" {
-    vpc_id = "${var.vpc_id}"
-    tags {
-        Name = "${var.name}-nomad-agent"
-        Description = "Allow port 4646 (TCP/HTTP) to nomad agents in ${var.name}"
-    }
-    # open port 4646 (nomad http) tcp
-    ingress {
-        from_port = 4646
-        to_port = 4646
-        protocol = "tcp"
-        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
-    }
-    ingress {
-        from_port = 20000
-        to_port = 60000
-        protocol = "tcp"
-        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
-    }
+  vpc_id = "${var.vpc_id}"
 
+  tags {
+    Name        = "${var.name}-nomad-agent"
+    Description = "Allow port 4646 (TCP/HTTP) to nomad agents in ${var.name}"
+  }
+
+  # open port 4646 (nomad http) tcp
+  ingress {
+    from_port   = 4646
+    to_port     = 4646
+    protocol    = "tcp"
+    cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
+  }
+
+  ingress {
+    from_port   = 20000
+    to_port     = 60000
+    protocol    = "tcp"
+    cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
+  }
 }
