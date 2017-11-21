5de9b02000032b20fd3b1ae9a190ba27437021bd
user <ketzacoatl@users.noreply.github.com>
2017-11-21 04:39:44 -0500

    mongo-server-sg: refactor to use aws_security_group_rule

--- tf-modules/mongo-server-sg/main.tf
+++ tf-modules/mongo-server-sg/main.tf
@@ -1,47 +1,33 @@
 /**
  * ## MongoDB Server Security Group
  *
- * Security group for use with MongoDB servers.
+ * Security group rules for use with MongoDB servers.
  */
-variable "name" {
-    description = "The name prefix for this SSH security group"
+
+variable "security_group_id" {
+  description = "security group to attach the ingress rules to"
 }
-variable "name_suffix" {
-    default = "mongo-server"
-    description = "The suffix in forming the name for this security group"
+
+variable "cidr_blocks" {
+  description = "The list of CIDR IP blocks allowed to access mongod"
+  type        = "list"
 }
-variable "allowed_cidr_blocks" {
-    description = "The CIDR block to allow access to"
+
+# Security group for mongod servers
+resource "aws_security_group_rule" "mongo_tcp_1" {
+  type              = "ingress"
+  from_port         = "27017"
+  to_port           = "27017"
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.cidr_blocks}"]
+  security_group_id = "${var.security_group_id}"
 }
-variable "vpc_id" {
-    description = "The ID of the VPC to deploy to"
-}
-resource "aws_security_group" "mongo" {
-    name = "${var.name}-${var.name_suffix}"
-    vpc_id = "${var.vpc_id}"
-    tags {
-        Name = "${var.name}-${var.name_suffix}"
-        Description = "Allow access to mongodb servers in ${var.name}"
-    }
-    # mongodb
-    ingress {
-        from_port = 27017
-        to_port = 27017
-        protocol = "tcp"
-        cidr_blocks = ["${split(",", replace(var.allowed_cidr_blocks, " ", ""))}"]
-    }
-    ingress {
-        from_port = 28017
-        to_port = 28017
-        protocol = "tcp"
-        cidr_blocks = ["${split(",", replace(var.allowed_cidr_blocks, " ", ""))}"]
-    }
-}
-//`id` exported from `aws_security_group`
-output "id" {
-    value = "${aws_security_group.mongo.id}"
-}
-//`name` exported from `aws_security_group`
-output "name" {
-    value = "${aws_security_group.mongo.name}"
+
+resource "aws_security_group_rule" "mongo_tcp_2" {
+  type              = "ingress"
+  from_port         = "28017"
+  to_port           = "28017"
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.cidr_blocks}"]
+  security_group_id = "${var.security_group_id}"
 }
