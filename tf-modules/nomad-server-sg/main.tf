5c9dbedfc64ebb642d2e772d4c1f21639a284ecb
user <ketzacoatl@users.noreply.github.com>
2017-11-21 04:57:09 -0500

    nomad-server-sg: refactor to use aws_security_group_rule

--- tf-modules/nomad-server-sg/main.tf
+++ tf-modules/nomad-server-sg/main.tf
@@ -3,35 +3,48 @@
  *
  * Security group for the Nomad Servers/Leaders.
  */
-resource "aws_security_group" "main" {
-  vpc_id = "${var.vpc_id}"
 
-  tags {
-    Name        = "${var.name}-nomad-server"
-    Description = "Allow 4646/4647 (TCP, HTTP/RPC) to both worker/servers, and 4848 (serf, tcp/udp) to servers only in ${var.name}"
-  }
+variable "security_group_id" {
+  description = "security group to attach the ingress rules to"
+}
 
-  # open port 4646/4647 (nomad-server) tcp for the leaders
-  ingress {
-    from_port = 4646
-    to_port   = 4647
-    protocol  = "tcp"
+variable "server_cidr_blocks" {
+  description = "The list of CIDR IP blocks where nomad servers run"
+  type        = "list"
+}
 
-    cidr_blocks = ["${distinct(concat(var.server_cidr_blocks, var.worker_cidr_blocks))}"]
-  }
+variable "worker_cidr_blocks" {
+  description = "The list of CIDR IP blocks where nomad workers run"
+  type        = "list"
+}
 
-  # open port 4648 (nomad) tcp/udp for the leaders
-  ingress {
-    from_port   = 4648
-    to_port     = 4648
-    protocol    = "tcp"
-    cidr_blocks = ["${var.server_cidr_blocks}"]
-  }
+# nomad ports 4646 (http)/4647 (rpc), open to both workers and servers/leaders
+# including 4646 here might lead it issues with it also being in the agent module
+resource "aws_security_group_rule" "nomad_http_rpc_tcp" {
+  type              = "ingress"
+  from_port         = "4646"
+  to_port           = "4647"
+  protocol          = "tcp"
+  cidr_blocks       = ["${distinct(concat(var.server_cidr_blocks,
+                                          var.worker_cidr_blocks))}"]
+  security_group_id = "${var.security_group_id}"
+}
 
-  ingress {
-    from_port   = 4648
-    to_port     = 4648
-    protocol    = "udp"
-    cidr_blocks = ["${var.server_cidr_blocks}"]
-  }
+# open port 4648 (nomad) tcp/udp for the leaders
+resource "aws_security_group_rule" "nomad_serf_tcp" {
+  type              = "ingress"
+  from_port         = "4648"
+  to_port           = "4648"
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.server_cidr_blocks}"]
+  security_group_id = "${var.security_group_id}"
+}
+
+resource "aws_security_group_rule" "nomad_serf_udp" {
+  type              = "ingress"
+  from_port         = "4648"
+  to_port           = "4648"
+  protocol          = "udp"
+  cidr_blocks       = ["${var.server_cidr_blocks}"]
+  security_group_id = "${var.security_group_id}"
 }
