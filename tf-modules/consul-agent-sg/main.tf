c95ec2377ad270ea139b556a79173006aa063486
user <ketzacoatl@users.noreply.github.com>
2017-11-21 04:15:37 -0500

    consul-agent-sg: refactor to use aws_security_group_rule

--- tf-modules/consul-agent-sg/main.tf
+++ tf-modules/consul-agent-sg/main.tf
@@ -1,22 +1,20 @@
 /**
- *## Security Group for Consul Agents
+ *## Ingress Rules for Consul Agents
  *
- *This module creates a _single_ `aws_security_group` resource. This resource has
- *an ingress rule to allow port `8301`, for TCP and UDP each. Use the security
- *group on any nodes you wish to use the consul agent on:
+ *This module creates `aws_security_group_rule` resources, defining an ingress
+ *rule to allow port `8301`, for TCP and UDP each. Use with a security
+ *group on any nodes you wish to use the consul agent on.
  *
  *
  *### Example
  *
+ * UPDATE THESE DOCS
+ *
  *```
  *# boxed security group for consul leader services, no egress/custom rules
  *module "consul-agent-sg" {
- *    source = "../tf-modules/consul-agent-sg"
- *    name = "${var.name}"
- *    vpc_id = "${module.test-vpc.id}"
- *    access_key = "${var.access_key}"
- *    secret_key = "${var.secret_key}"
- *    cidr_blocks = "${module.test-vpc.cidr_block}"
+ *    source      = "../tf-modules/consul-agent-sg"
+ *    cidr_blocks = ["${module.test-vpc.cidr_block}"]
  *}
  *
  *module "my-cluster" {
@@ -25,27 +23,33 @@
  *    cluster_security_group_ids = "${module.consul-agent-sg.id}, ${aws_security_group.worker-service.id}, ${module.public-ssh-sg.id}"
  *```
  */
-# Security group to cover Consul Agent
-resource "aws_security_group" "main" {
-  vpc_id = "${var.vpc_id}"
 
-  tags {
-    Name        = "${var.name}-consul-agent"
-    Description = "Allow TCP and UDP ports to consul agent in ${var.name}"
-  }
+# ingress rules for consul agents. Required by all agents. 
 
-  # Serf LAN, used to handle gossip in the LAN. Required by all agents. TCP and UDP.
-  ingress {
-    from_port   = 8301
-    to_port     = 8301
-    protocol    = "tcp"
-    cidr_blocks = ["${var.cidr_blocks}"]
-  }
+variable "security_group_id" {
+  description = "security group to attach the ingress rules to"
+}
 
-  ingress {
-    from_port   = 8301
-    to_port     = 8301
-    protocol    = "udp"
-    cidr_blocks = ["${var.cidr_blocks}"]
-  }
+variable "cidr_blocks" {
+  description = "The list of CIDR IP blocks allowed to access the consul ports"
+  type        = "list"
+}
+
+# Serf LAN, used to handle gossip in the LAN. TCP and UDP.
+resource "aws_security_group_rule" "serf_lan_tcp" {
+  type              = "ingress"
+  from_port         = "8301"
+  to_port           = "8301"
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.cidr_blocks}"]
+  security_group_id = "${var.security_group_id}"
+}
+
+resource "aws_security_group_rule" "serf_lan_udp" {
+  type              = "ingress"
+  from_port         = "8301"
+  to_port           = "8301"
+  protocol          = "udp"
+  cidr_blocks       = ["${var.cidr_blocks}"]
+  security_group_id = "${var.security_group_id}"
 }
