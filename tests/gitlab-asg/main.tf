64316b182e5246c893ae023c531468512495fb9a
user <ketzacoatl@users.noreply.github.com>
2017-11-21 11:14:07 -0500

    tests/gitlab-asg: improve reusability of env and speed to deploy
    * use the `name` variable convention we've done elsewhere
    * drop the `az` variable, the user should not have to think about AZs
    (other than the number of AZ to use in the deploy)
    * lookup availability zones with the data source
    * drop the `key_name` variable, use the `name` variable instead
    * use `ami-ubuntu` module to lookup current AMI for Xenial
    * drop a default for key_file, make the user specify
    * setup the key pair for the user, don't require them to do that

--- tests/gitlab-asg/main.tf
+++ tests/gitlab-asg/main.tf
@@ -1,27 +1,35 @@
+variable "name" {
+  default = "gitlab-asg-test"
+}
+
 variable "region" {
   default = "us-east-1"
 }
 
-variable "az" {
-  default = "us-east-1a"
-}
-
-variable "key_name" {
-  description = "Keypair name to log into the ec2 instance"
-}
-
 variable "key_file" {
   description = "The secret key file to use"
-  default     = "~/.ssh/id_rsa"
+}
+
+data "aws_availability_zones" "available" {}
+
+module "ubuntu-xenial-ami" {
+  source  = "../../tf-modules/ami-ubuntu"
+  release = "16.04"
+} 
+
+resource "aws_key_pair" "main" {
+  key_name   = "${var.name}"
+  public_key = "${file(var.key_file)}"
 }
 
 module "setup" {
-  name_prefix        = "test"
+  name_prefix        = "${var.name}"
   source             = "../../tf-modules/gitlab-single-node-asg"
   region             = "${var.region}"
-  az                 = "${var.az}"
+  az                 = "${element(data.aws_availability_zones.available.names, 0)}"
   key_file           = "${var.key_file}"
-  key_name           = "${var.key_name}"
+  key_name           = "${aws_key_pair.main.key_name}"
+  instance_ami       = "${module.ubuntu-xenial-ami.id}"
   subnet_id          = "${module.vpc.public_subnet_ids[0]}"
   vpc_id             = "${module.vpc.vpc_id}"
   security_group_ids = ["${aws_security_group.gitlab-asg.id}"]
@@ -29,8 +37,8 @@ module "setup" {
 
 module "vpc" {
   source              = "../../tf-modules/vpc-scenario-1"
-  azs                 = ["${var.az}"]
-  name_prefix         = "gitlab-single-node-asg"
+  azs                 = ["${slice(data.aws_availability_zones.available.names, 0, 1)}"]
+  name_prefix         = "${var.name}"
   cidr                = "192.168.0.0/16"
   public_subnet_cidrs = ["192.168.0.0/16"]
   region              = "${var.region}"
