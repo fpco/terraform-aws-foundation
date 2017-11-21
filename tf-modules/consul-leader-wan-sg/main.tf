d54886a834283f05a565810d26e17eb340f6cf14
user <ketzacoatl@users.noreply.github.com>
2017-11-21 04:33:45 -0500

    consul-leader-wan-sg: refactor to use aws_security_group_rule

--- tf-modules/consul-leader-wan-sg/main.tf
+++ tf-modules/consul-leader-wan-sg/main.tf
@@ -1,33 +1,44 @@
 /**
  *## Consul Leaders WAN Security Group
  *
- *Boxed security group for use with a cluster of consul leaders. These are the
- *ingress rules for leaders connecting to other leaders across the WAN.
+ *Boxed set of security group rules for use with a cluster of consul leaders.
+ *These are the ingress rules for leaders connecting to other leaders across
+ *the WAN.
  *
  *See `consul-agents-sg` for Agents, or `consul-leaders-sg` for the
- *inter-datacenter leader communication.
+ *intra-datacenter leader communication.
  *
  */
-resource "aws_security_group" "main" {
-  vpc_id = "${var.vpc_id}"
 
-  tags {
-    Name        = "${var.name}-consul-leader-wan"
-    Description = "Allow TCP and UDP to WAN port (${var.wan_port}) for consul leaders in ${var.name}"
-  }
+variable "security_group_id" {
+  description = "security group to attach the ingress rules to"
+}
 
-  # Serf WAN, used by servers to gossip over the WAN to other servers. TCP and UDP.
-  ingress {
-    from_port   = "${var.wan_port}"
-    to_port     = "${var.wan_port}"
-    protocol    = "tcp"
-    cidr_blocks = ["${var.cidr_blocks}"]
-  }
+variable "cidr_blocks" {
+  description = "The list of CIDR IP blocks allowed to access the consul ports"
+  type        = "list"
+}
 
-  ingress {
-    from_port   = "${var.wan_port}"
-    to_port     = "${var.wan_port}"
-    protocol    = "udp"
-    cidr_blocks = ["${var.cidr_blocks}"]
-  }
+variable "wan_port" {
+  default     = "8302"
+  description = "The port to use for consul WAN"
+}
+
+# TCP/UDP for serf WAN communication
+resource "aws_security_group_rule" "serf_wan_tcp" {
+  type              = "ingress"
+  from_port         = "${var.wan_port}"
+  to_port           = "${var.wan_port}"
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.cidr_blocks}"]
+  security_group_id = "${var.security_group_id}"
+}
+
+resource "aws_security_group_rule" "serf_wan_udp" {
+  type              = "ingress"
+  from_port         = "${var.wan_port}"
+  to_port           = "${var.wan_port}"
+  protocol          = "udp"
+  cidr_blocks       = ["${var.cidr_blocks}"]
+  security_group_id = "${var.security_group_id}"
 }
