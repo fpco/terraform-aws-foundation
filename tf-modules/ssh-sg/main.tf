888704941226cbaf2fc5851017850b0ac8cae7c3
user <ketzacoatl@users.noreply.github.com>
2017-11-21 05:00:00 -0500

    ssh-sg: refactor to use aws_security_group_rule

--- tf-modules/ssh-sg/main.tf
+++ tf-modules/ssh-sg/main.tf
@@ -3,20 +3,27 @@
  *
  *Security group to allow inbound SSH for administration
  */
-resource "aws_security_group" "ssh" {
-  name   = "${var.name}-ssh"
-  vpc_id = "${var.vpc_id}"
 
-  tags {
-    Name        = "${var.name}-ssh"
-    Description = "Allow SSH to hosts in ${var.name}"
-  }
+variable "security_group_id" {
+  description = "security group to attach the ingress rules to"
+}
 
-  # SSH
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${var.allowed_cidr_blocks}"]
-  }
+variable "cidr_blocks" {
+  description = "The list of CIDR IP blocks allowed to access the SSH port"
+  type        = "list"
+}
+
+variable "ssh_port" {
+  description = "The port to use"
+  default     = "22"
+}
+
+# Security group for etcd servers
+resource "aws_security_group_rule" "ssh_tcp" {
+  type              = "ingress"
+  from_port         = "${var.ssh_port}"
+  to_port           = "${var.ssh_port}"
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.cidr_blocks}"]
+  security_group_id = "${var.security_group_id}"
 }
--- tf-modules/ssh-sg/outputs.tf
+++
@@ -1,9 +0,0 @@
-//`id` exported from `aws_security_group`
-output "id" {
-  value = "${aws_security_group.ssh.id}"
-}
-
-//`name` exported from `aws_security_group`
-output "name" {
-  value = "${aws_security_group.ssh.name}"
-}
