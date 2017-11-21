2bf651e62a7947194dbd90af52b8a15c2ac111ab
user <ketzacoatl@users.noreply.github.com>
2017-11-21 05:06:26 -0500

    prometheus-node-exporter-sg: refactor to use aws_security_group_rule

--- tf-modules/prometheus-node-exporter-sg/main.tf
+++ tf-modules/prometheus-node-exporter-sg/main.tf
@@ -1,19 +1,23 @@
 /**
- *##Prometheus Node Exporter Security Group
+ *##Prometheus Node Exporter Security Group Rule
  *
- * Security group for the Prometheus node exporter.
+ * Security group rule for the Prometheus node exporter.
  */
-resource "aws_security_group" "main" {
-    vpc_id = "${var.vpc_id}"
-    tags {
-        Name = "${var.name}-prometheus-node-exporter"
-        Description = "Allow prometheus server access to node-exporter in ${var.name}"
-    }
-    # the prometheus node-exporter listens on 9100
-    ingress {
-        from_port = 9100
-        to_port = 9100
-        protocol = "tcp"
-        cidr_blocks = ["${split(",", replace(var.cidr_blocks, " ", ""))}"]
-    }
+variable "security_group_id" {
+  description = "security group to attach the ingress rules to"
+}
+
+variable "cidr_blocks" {
+  description = "The list of CIDR IP blocks allowed to access the node exporter port"
+  type        = "list"
+}
+
+# Security group ingress rule for the node exporter
+resource "aws_security_group_rule" "node_exporter_tcp" {
+  type              = "ingress"
+  from_port         = "9100"
+  to_port           = "9100"
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.cidr_blocks}"]
+  security_group_id = "${var.security_group_id}"
 }
