dac30e7f66e1b754dfdd5e38e2a9ee22dda784b4
user <ketzacoatl@users.noreply.github.com>
2017-11-22 05:45:29 -0500

    security-group-base: add new module

---
+++ tf-modules/security-group-base/main.tf
@@ -0,0 +1,45 @@
+/*
+ * ## Security Group Base
+ *
+ * Add DOCS
+ *
+ */
+
+variable "name" {
+  description = "security group `name`"
+  type        = "string"
+}
+
+variable "description" {
+  description = "security group `description`"
+  type        = "string"
+}
+
+variable "vpc_id" {
+  description = "ID of VPC to associate SG with"
+  type        = "string"
+}
+
+variable "extra_tags" {
+  description = "map of name,value pairs to tag the security group (append to Name tag)"
+  default     = {}
+  type        = "map"
+}
+
+resource "aws_security_group" "main" {
+  name        = "${var.name}"
+  description = "${var.description}"
+  vpc_id      = "${var.vpc_id}"
+
+  tags = "${merge(map("Name", "${var.name}"), "${var.extra_tags}")}"
+}
+
+// ID of the Security Group created
+output "id" {
+  value = "${aws_security_group.main.id}"
+}
+
+// Name of the Security Group created
+output "name" {
+  value = "${aws_security_group.main.name}"
+}
