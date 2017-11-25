c0dd27bd44e34a4321d8da4574da91d90c016963
Ryan King <ryan.king@gosynesis.com>
2017-11-24 19:35:36 -0800

    * Added output for the instance private ip address

--- tf-modules/ec2-nat-instance/outputs.tf
+++ tf-modules/ec2-nat-instance/outputs.tf
@@ -3,6 +3,11 @@ output "id" {
   value = "${aws_instance.ec2-nat.id}"
 }
 
+// Private IP of EC2 instance
+output "private_ip" {
+  value = "${aws_instance.ec2-nat.private_ip}"
+}
+
 // Public IP of EC2 instance
 output "public_ip" {
   value = "${aws_instance.ec2-nat.public_ip}"
