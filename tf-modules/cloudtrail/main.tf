b2dc73f44adfe88d724bf26973b5167bf9bde7e7
Yghor Kerscher <kerscher@outlook.com>
2017-11-29 22:57:06 +0100

    cloudtrail: add module

---
+++ tf-modules/cloudtrail/main.tf
@@ -0,0 +1,52 @@
+resource "aws_cloudtrail" "cloudtrail" {
+  name           = "${var.name_prefix}-cloudtrail"
+  s3_bucket_name = "${aws_s3_bucket.cloudtrail.id}"
+
+  include_global_service_events = "${var.include_global_service_events}"
+  enable_logging                = "${var.enable_logging}"
+  is_multi_region_trail         = false
+
+  tags = "${merge(map("Name", "${var.name_prefix}-cloudtrail"), "${var.extra_tags}")}"
+}
+
+resource "aws_s3_bucket" "cloudtrail" {
+  bucket = "${var.name_prefix}-cloudtrail"
+  acl    = "private"
+  policy = "${data.aws_iam_policy_document.cloudtrail-bucket.json}"
+
+  tags = "${merge(map("Name", "${var.name_prefix}-cloudtrail"), "${var.extra_tags}")}"
+}
+
+data "aws_iam_policy_document" "cloudtrail-bucket" {
+  statement {
+    sid    = "AWSCloudTrailAclCheck"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:GetBucketAcl"]
+    resources = ["arn:${var.aws_cloud}:::${var.name_prefix}-cloudtrail"]
+  }
+
+  statement {
+    sid    = "AWSCloudTrailWrite"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:PutObject"]
+    resources = ["arn:${var.aws_cloud}:::${var.name_prefix}-cloudtrail/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+  }
+}
---
+++ tf-modules/cloudtrail/variables.tf
@@ -0,0 +1,22 @@
+variable "name_prefix" {
+  description = "name to be prepended to all resources"
+}
+
+variable "aws_cloud" {
+  description = "set to 'aws-us-gov' if using GovCloud, otherwise leave the default"
+  default     = "aws"
+}
+
+variable "include_global_service_events" {
+  description = "boolean, if true logs IAM events as well as region-specific ones"
+  default     = true
+}
+
+variable "enable_logging" {
+  description = "boolean, defines if logging should be started or stopped"
+  default     = true
+}
+
+variable "extra_tags" {
+  default = {}
+}
