a056a512438261fc987d59306c1d40efe2080a5b
user <ketzacoatl@users.noreply.github.com>
2017-12-01 15:12:05 -0500

    init-snippet-config-consul-leader: whitespace fixup

--- tf-modules/init-snippet-config-consul-leader/main.tf
+++ tf-modules/init-snippet-config-consul-leader/main.tf
@@ -2,32 +2,40 @@
 variable "encrypt" {
   description = "base64 encoded key for consul gossip encryption"
 }
+
 variable "data_dir" {
-  default = "/opt/consul"
+  default     = "/opt/consul"
   description = "path on consul server for -data-dir"
 }
+
 variable "datacenter" {
   description = "name of consul datacenter, maps to -datacenter config"
 }
+
 variable "config_file" {
   default     = "/etc/consul.json"
   description = "path to consul config file"
 }
+
 variable "init_prefix" {
-  default = ""
+  default     = ""
   description = "initial init (shellcode) to prefix this snippet with"
 }
+
 variable "init_suffix" {
-  default = ""
+  default     = ""
   description = "init (shellcode) to append to the end of this snippet"
 }
+
 variable "log_prefix" {
-  default = "OPS: "
+  default     = "OPS: "
   description = "string to prefix log messages with"
 }
+
 # render init script snippet from the template
 data "template_file" "init_snippet" {
   template = "${file("${path.module}/snippet.tpl")}"
+
   vars {
     encrypt     = "${var.encrypt}"
     data_dir    = "${var.data_dir}"
@@ -38,6 +46,7 @@ data "template_file" "init_snippet" {
     log_prefix  = "${var.log_prefix}"
   }
 }
+
 output "init_snippet" {
   value = "${data.template_file.init_snippet.rendered}"
 }
