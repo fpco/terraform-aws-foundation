88c98e10d477d6157a52d2d9677ed2e16612c826
user <ketzacoatl@users.noreply.github.com>
2017-12-01 15:20:32 -0500

    init-snippet-exec: whitespace fixup

--- tf-modules/init-snippet-exec/main.tf
+++ tf-modules/init-snippet-exec/main.tf
@@ -1,12 +1,15 @@
 variable "init" {
   description = "init shellcode for this snippet"
 }
+
 data "template_file" "init_snippet" {
   template = "${file("${path.module}/snippet.tpl")}"
+
   vars {
     init = "${var.init}"
   }
 }
+
 output "init_snippet" {
   value = "${data.template_file.init_snippet.rendered}"
 }
