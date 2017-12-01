c993d2535e9fb23d418ceab75278ccc64cfc8998
user <ketzacoatl@users.noreply.github.com>
2017-12-01 15:14:17 -0500

    web-alb: whitespace fixup

--- tf-modules/web-alb/variables.tf
+++ tf-modules/web-alb/variables.tf
@@ -1,5 +1,5 @@
 variable "name_prefix" {
-  default = "dev"
+  default     = "dev"
   description = "Prefix that will be added to names of all resources"
 }
 
@@ -9,16 +9,16 @@ variable "vpc_id" {
 
 variable "subnet_ids" {
   description = "A list of subnet ids to attach ALB to"
-  type = "list"
+  type        = "list"
 }
 
 variable "internal" {
-  default = true
+  default     = true
   description = "Should this ALB be internal or not."
 }
 
 variable "app_names" {
-  default = ""
+  default     = ""
   description = "Names of other apps this ALB will be used for, eg. 'Kibana+Elasticsearch'"
 }
 
@@ -27,13 +27,13 @@ variable "dns_name" {
 }
 
 variable "dns_ssl_name" {
-  default = ""
+  default     = ""
   description = "DNS name for Web Server endpoint SSL. An SSL certificate is expected to be present in ACM for this domain. If left empty 'dns_name' will be checked instead."
 }
 
 variable "user_ingress_cidrs" {
-  type = "list"
-  default = []
+  type        = "list"
+  default     = []
   description = "Optional list of CIDRs that will have access to ALBs ports 80 and 443."
 }
 
