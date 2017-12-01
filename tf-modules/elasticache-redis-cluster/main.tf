8186b44c2f8d72403e6d7a840b040ec8f9c89243
user <ketzacoatl@users.noreply.github.com>
2017-12-01 15:11:40 -0500

    elasticache-redis-cluster: whitespace fixup

--- tf-modules/elasticache-redis-cluster/main.tf
+++ tf-modules/elasticache-redis-cluster/main.tf
@@ -45,68 +45,76 @@
  * 
  */
 resource "aws_elasticache_cluster" "redis" {
-    cluster_id = "${var.name}"
-    engine = "redis"
-    engine_version = "${var.engine_version}"
-    node_type = "${var.instance_type}"
-    port = 6379
-    num_cache_nodes = 1
-#   parameter_group_name = "default.memcached1.4"
-    subnet_group_name = "${aws_elasticache_subnet_group.redis.name}"
-    security_group_ids =["${aws_security_group.redis-internal.id}",
-                         "${var.inbound_security_group}"]
+  cluster_id      = "${var.name}"
+  engine          = "redis"
+  engine_version  = "${var.engine_version}"
+  node_type       = "${var.instance_type}"
+  port            = 6379
+  num_cache_nodes = 1
+
+  #   parameter_group_name = "default.memcached1.4"
+  subnet_group_name = "${aws_elasticache_subnet_group.redis.name}"
+
+  security_group_ids = ["${aws_security_group.redis-internal.id}",
+    "${var.inbound_security_group}",
+  ]
 }
 
 resource "aws_elasticache_subnet_group" "redis" {
-    name = "${var.name}-${var.region}"
-    description = "Redis on Elasticache for ${var.name}"
-    subnet_ids = ["${aws_subnet.a.id}", "${aws_subnet.c.id}"]
+  name        = "${var.name}-${var.region}"
+  description = "Redis on Elasticache for ${var.name}"
+  subnet_ids  = ["${aws_subnet.a.id}", "${aws_subnet.c.id}"]
 }
 
 # Subnets in each of two AZ in this region (a and c are the only two in all three US regions)
 resource "aws_subnet" "a" {
-    availability_zone = "${var.region}a"
-    cidr_block = "${var.cidr_a}"
-    map_public_ip_on_launch = false
-    vpc_id = "${var.vpc_id}"
-    tags {
-        Name = "${var.name}-redis-a-${var.region}"
-        Description = "Elasticache subnet for ${var.name}"
-    }
+  availability_zone       = "${var.region}a"
+  cidr_block              = "${var.cidr_a}"
+  map_public_ip_on_launch = false
+  vpc_id                  = "${var.vpc_id}"
+
+  tags {
+    Name        = "${var.name}-redis-a-${var.region}"
+    Description = "Elasticache subnet for ${var.name}"
+  }
 }
+
 resource "aws_subnet" "c" {
-    availability_zone = "${var.region}c"
-    cidr_block = "${var.cidr_c}"
-    map_public_ip_on_launch = false
-    vpc_id = "${var.vpc_id}"
-    tags {
-        Name = "${var.name}-redis-c-${var.region}"
-        Description = "Elasticache subnet for ${var.name}"
-    }
+  availability_zone       = "${var.region}c"
+  cidr_block              = "${var.cidr_c}"
+  map_public_ip_on_launch = false
+  vpc_id                  = "${var.vpc_id}"
+
+  tags {
+    Name        = "${var.name}-redis-c-${var.region}"
+    Description = "Elasticache subnet for ${var.name}"
+  }
 }
+
 # Routing table association for each minion subnet to the VPC IGW
 resource "aws_route_table_association" "a" {
-    route_table_id = "${var.route_table_id}"
-    subnet_id = "${aws_subnet.a.id}"
+  route_table_id = "${var.route_table_id}"
+  subnet_id      = "${aws_subnet.a.id}"
 }
+
 resource "aws_route_table_association" "c" {
-    route_table_id = "${var.route_table_id}"
-    subnet_id = "${aws_subnet.c.id}"
+  route_table_id = "${var.route_table_id}"
+  subnet_id      = "${aws_subnet.c.id}"
 }
 
 # Security group to allow communication from the specified networks
 resource "aws_security_group" "redis-internal" {
-    name = "${var.name}-${var.region}-redis-internal"
-    vpc_id = "${var.vpc_id}"
-    tags {
-        Description = "Allow redis to instances in the elasticache subnets for ${var.name}"
-    }
-    ingress {
-        from_port = 6379
-        to_port = 6379
-        protocol = "tcp"
-        cidr_blocks = ["${aws_subnet.a.cidr_block}", "${aws_subnet.c.cidr_block}"]
-    }
+  name   = "${var.name}-${var.region}-redis-internal"
+  vpc_id = "${var.vpc_id}"
+
+  tags {
+    Description = "Allow redis to instances in the elasticache subnets for ${var.name}"
+  }
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_subnet.a.cidr_block}", "${aws_subnet.c.cidr_block}"]
+  }
 }
-
-
--- tf-modules/elasticache-redis-cluster/variables.tf
+++ tf-modules/elasticache-redis-cluster/variables.tf
@@ -1,34 +1,42 @@
 variable "name" {
-    default = ""
-    description = "The name of this elasticache cluster, this should be unique"
+  default     = ""
+  description = "The name of this elasticache cluster, this should be unique"
 }
+
 variable "instance_type" {
-    default = "cache.m3.medium"
-    description = "See http://aws.amazon.com/elasticache/details/#Available_Cache_Node_Types"
+  default     = "cache.m3.medium"
+  description = "See http://aws.amazon.com/elasticache/details/#Available_Cache_Node_Types"
 }
+
 variable "inbound_security_group" {
-    default = ""
-    description = "The ID of the security group to use for incoming connections (to redis)"
+  default     = ""
+  description = "The ID of the security group to use for incoming connections (to redis)"
 }
+
 variable "engine_version" {
-    default = "2.8.22"
-    description = "See http://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/SelectEngine.html"
+  default     = "2.8.22"
+  description = "See http://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/SelectEngine.html"
 }
+
 variable "region" {
-    description = "AWS region to deploy to"
-    default = ""
+  description = "AWS region to deploy to"
+  default     = ""
 }
+
 variable "vpc_id" {
-    description = "The ID of the experimental VPC to deploy to, depends on the AWS region"
+  description = "The ID of the experimental VPC to deploy to, depends on the AWS region"
 }
+
 variable "route_table_id" {
-    description = "The ID of the experimental routing table to use, depends on the AWS region"
+  description = "The ID of the experimental routing table to use, depends on the AWS region"
 }
+
 variable "cidr_a" {
-    default = ""
-    description = "The CIDR block for subnet a"
+  default     = ""
+  description = "The CIDR block for subnet a"
 }
+
 variable "cidr_c" {
-    default = ""
-    description = "The CIDR block for subnet c"
+  default     = ""
+  description = "The CIDR block for subnet c"
 }
