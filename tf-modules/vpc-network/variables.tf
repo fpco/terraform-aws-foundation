4989d093f774e27ad84636f8896dc93b741e7d72
user <ketzacoatl@users.noreply.github.com>
2017-12-01 15:13:57 -0500

    vpc-network: whitespace fixup

--- tf-modules/vpc-network/variables.tf
+++ tf-modules/vpc-network/variables.tf
@@ -1,10 +1,12 @@
 variable "name" {
-    description = "The name of this network"
+  description = "The name of this network"
 }
+
 variable "vpc_cidr_prefix" {
-    description = "The prefix to the VPC CIDR block, eg: 10.100"
+  description = "The prefix to the VPC CIDR block, eg: 10.100"
 }
+
 variable "enable_dns_hostnames" {
-    default = "true"
-    description = "Boolean to set the `enable_dns_hostnames` flag for the VPC"
+  default     = "true"
+  description = "Boolean to set the `enable_dns_hostnames` flag for the VPC"
 }
