8c5e3e86c925288ac427e61b463c009fab7f620b
user <ketzacoatl@users.noreply.github.com>
2017-12-01 15:16:57 -0500

    snapshot-bucket: whitespace fixup

--- tf-modules/snapshot-bucket/main.tf
+++ tf-modules/snapshot-bucket/main.tf
@@ -5,15 +5,18 @@
  * key that provide access to that bucket.
  */
 resource "aws_iam_user" "s3" {
-    name = "${var.name}"
-    path = "/"
+  name = "${var.name}"
+  path = "/"
 }
+
 resource "aws_iam_access_key" "s3" {
-    user = "${aws_iam_user.s3.name}"
+  user = "${aws_iam_user.s3.name}"
 }
+
 resource "aws_iam_policy" "s3" {
-    name = "${var.name}_all_access"
-    policy = <<EOF
+  name = "${var.name}_all_access"
+
+  policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -42,17 +45,20 @@ resource "aws_iam_policy" "s3" {
 }
 EOF
 }
+
 resource "aws_iam_policy_attachment" "s3" {
-    name = "${var.name}_all_access_attachment"
-    users = ["${aws_iam_user.s3.name}"]
-    groups = ["admin"]
-    policy_arn = "${aws_iam_policy.s3.arn}"
+  name       = "${var.name}_all_access_attachment"
+  users      = ["${aws_iam_user.s3.name}"]
+  groups     = ["admin"]
+  policy_arn = "${aws_iam_policy.s3.arn}"
 }
+
 resource "aws_s3_bucket" "s3" {
-    depends_on = ["aws_iam_user.s3"]
-    bucket = "${var.name}"
-    acl = "private"
-    tags {
-        Name = "${var.name}"
-    }
+  depends_on = ["aws_iam_user.s3"]
+  bucket     = "${var.name}"
+  acl        = "private"
+
+  tags {
+    Name = "${var.name}"
+  }
 }
