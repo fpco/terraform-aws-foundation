bb9b24605ae3d6bbd3ac235731d19bb0286024cc
user <ketzacoatl@users.noreply.github.com>
2017-12-01 15:32:09 -0500

    init-snippet-install-consul: whitespace fixup

--- tf-modules/init-snippet-install-consul/main.tf
+++ tf-modules/init-snippet-install-consul/main.tf
@@ -1,40 +1,48 @@
 # variables used by this snippet of init shellcode
 variable "version" {
-    default = "0.9.3"
-    description = "version of consul, to install"
+  default     = "0.9.3"
+  description = "version of consul, to install"
 }
+
 variable "base_url" {
-    default = "https://releases.hashicorp.com/consul"
-    description = "string to prefix log messages with"
+  default     = "https://releases.hashicorp.com/consul"
+  description = "string to prefix log messages with"
 }
+
 variable "data_dir" {
-    default = "/opt/consul"
-    description = "path to use for consul's -data-dir"
+  default     = "/opt/consul"
+  description = "path to use for consul's -data-dir"
 }
+
 variable "init_prefix" {
-    default = ""
-    description = "initial init (shellcode) to prefix this snippet with"
+  default     = ""
+  description = "initial init (shellcode) to prefix this snippet with"
 }
+
 variable "init_suffix" {
-    default = ""
-    description = "init (shellcode) to append to the end of this snippet"
+  default     = ""
+  description = "init (shellcode) to append to the end of this snippet"
 }
+
 variable "log_prefix" {
-    default = "OPS: "
-    description = "string to prefix log messages with"
+  default     = "OPS: "
+  description = "string to prefix log messages with"
 }
+
 # render init script snippet from the template
 data "template_file" "init_snippet" {
-    template = "${file("${path.module}/snippet.tpl")}"
-    vars {
-        version     = "${var.version}"
-        base_url    = "${var.base_url}"
-        data_dir    = "${var.data_dir}"
-        init_prefix = "${var.init_prefix}"
-        init_suffix = "${var.init_suffix}"
-        log_prefix  = "${var.log_prefix}"
-    }
+  template = "${file("${path.module}/snippet.tpl")}"
+
+  vars {
+    version     = "${var.version}"
+    base_url    = "${var.base_url}"
+    data_dir    = "${var.data_dir}"
+    init_prefix = "${var.init_prefix}"
+    init_suffix = "${var.init_suffix}"
+    log_prefix  = "${var.log_prefix}"
+  }
 }
+
 output "init_snippet" {
-    value = "${data.template_file.init_snippet.rendered}"
+  value = "${data.template_file.init_snippet.rendered}"
 }
