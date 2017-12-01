b4bdab29d180ea2d417420700a35e51945bc6195
user <ketzacoatl@users.noreply.github.com>
2017-12-01 16:08:52 -0500

    subnets: whitespace fixup

--- tf-modules/subnets/main.tf
+++ tf-modules/subnets/main.tf
@@ -7,10 +7,10 @@
  */
 
 resource "aws_subnet" "main" {
-  count             = "${length(var.cidr_blocks)}"
-  vpc_id            = "${var.vpc_id}"
-  cidr_block        = "${var.cidr_blocks[count.index]}"
-  availability_zone = "${element(var.azs, count.index)}"
-  tags              = "${merge(map("Name", "${var.name_prefix}-${format("%02d", count.index)}-${element(var.azs, count.index)}"), "${var.extra_tags}")}"
+  count                   = "${length(var.cidr_blocks)}"
+  vpc_id                  = "${var.vpc_id}"
+  cidr_block              = "${var.cidr_blocks[count.index]}"
+  availability_zone       = "${element(var.azs, count.index)}"
+  tags                    = "${merge(map("Name", "${var.name_prefix}-${format("%02d", count.index)}-${element(var.azs, count.index)}"), "${var.extra_tags}")}"
   map_public_ip_on_launch = "${var.public}"
 }
