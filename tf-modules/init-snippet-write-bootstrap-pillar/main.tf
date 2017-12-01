8ab8aca2171977799042ea137c61592e19dc939d
user <ketzacoatl@users.noreply.github.com>
2017-12-01 15:12:32 -0500

    init-snippet-write-bootstrap-pillar: whitespace fixup

--- tf-modules/init-snippet-write-bootstrap-pillar/main.tf
+++ tf-modules/init-snippet-write-bootstrap-pillar/main.tf
@@ -1,27 +1,33 @@
 variable "bootstrap_pillar_file" {
-    default = "/srv/pillar/bootstrap.sls"
-    description = "path, to the 'bootstrap' pillar file"
+  default     = "/srv/pillar/bootstrap.sls"
+  description = "path, to the 'bootstrap' pillar file"
 }
+
 variable "init_prefix" {
-    default = ""
-    description = "initial init (shellcode) to prefix this snippet with"
+  default     = ""
+  description = "initial init (shellcode) to prefix this snippet with"
 }
+
 variable "init_suffix" {
-    default = ""
-    description = "init (shellcode) to append to the end of this snippet"
+  default     = ""
+  description = "init (shellcode) to append to the end of this snippet"
 }
+
 variable "pillar" {
-    description = "salt pillar to write out to bootstrap.sls"
+  description = "salt pillar to write out to bootstrap.sls"
 }
+
 data "template_file" "init_snippet" {
-    template = "${file("${path.module}/snippet.tpl")}"
-    vars {
-        bootstrap_pillar_file = "${var.bootstrap_pillar_file}"
-        init_prefix = "${var.init_prefix}"
-        init_suffix = "${var.init_suffix}"
-        pillar = "${var.pillar}"
-    }
+  template = "${file("${path.module}/snippet.tpl")}"
+
+  vars {
+    bootstrap_pillar_file = "${var.bootstrap_pillar_file}"
+    init_prefix           = "${var.init_prefix}"
+    init_suffix           = "${var.init_suffix}"
+    pillar                = "${var.pillar}"
+  }
 }
+
 output "init_snippet" {
-    value = "${data.template_file.init_snippet.rendered}"
+  value = "${data.template_file.init_snippet.rendered}"
 }
