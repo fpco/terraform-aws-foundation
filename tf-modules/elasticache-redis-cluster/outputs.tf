6a5a631d17fb8172b5ab3b706c89f9934cde274a
user <ketzacoatl@users.noreply.github.com>
2017-12-01 15:11:40 -0500

    elasticache-redis-cluster: whitespace fixup

--- tf-modules/elasticache-redis-cluster/outputs.tf
+++ tf-modules/elasticache-redis-cluster/outputs.tf
@@ -1,12 +1,14 @@
 //`id`, exported from `aws_subnet` A
 output "subnet_a_id" {
-    value = "${aws_subnet.a.id}"
+  value = "${aws_subnet.a.id}"
 }
+
 //`id`, exported from `aws_subnet` C
 output "subnet_c_id" {
-    value = "${aws_subnet.a.id}"
+  value = "${aws_subnet.a.id}"
 }
+
 //`redis_url`, exported from the `aws_elasticache_cluster`
 output "redis_url" {
-    value = "${aws_elasticache_cluster.redis.cache_nodes.0.address}"
+  value = "${aws_elasticache_cluster.redis.cache_nodes.0.address}"
 }
