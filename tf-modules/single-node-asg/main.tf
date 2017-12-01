2939b3fc1a40a38a49c0e793420ea81fb2866d4f
user <ketzacoatl@users.noreply.github.com>
2017-12-01 15:06:53 -0500

    single-node-asg: whitespace fixup

--- tf-modules/single-node-asg/main.tf
+++ tf-modules/single-node-asg/main.tf
@@ -23,33 +23,38 @@ module "service-data" {
   kms_key_id  = "${var.data_volume_kms_key_id}"
   snapshot_id = "${var.data_volume_snapshot_id}"
 }
+
 module "server" {
   source             = "../asg"
   security_group_ids = "${var.security_group_ids}"
   name               = "${var.name}"
+
   # append this to the ASG name
-  suffix             = "${var.name_suffix}-${var.az}"
-  instance_type      = "${var.instance_type}"
-  ami                = "${var.ami}"
-  subnet_ids         = ["${var.subnet_id}"]
-  azs                = ["${var.az}"]
-  public_ip          = "${var.public_ip}"
-  key_name           = "${var.key_name}"
-  elb_names          = ["${var.load_balancers}"]
-  desired_capacity   = 1
-  max_nodes          = 1
-  min_nodes          = 1
-  root_volume_type   = "${var.root_volume_type}"
-  root_volume_size   = "${var.root_volume_size}"
+  suffix           = "${var.name_suffix}-${var.az}"
+  instance_type    = "${var.instance_type}"
+  ami              = "${var.ami}"
+  subnet_ids       = ["${var.subnet_id}"]
+  azs              = ["${var.az}"]
+  public_ip        = "${var.public_ip}"
+  key_name         = "${var.key_name}"
+  elb_names        = ["${var.load_balancers}"]
+  desired_capacity = 1
+  max_nodes        = 1
+  min_nodes        = 1
+  root_volume_type = "${var.root_volume_type}"
+  root_volume_size = "${var.root_volume_size}"
+
   #
-  iam_profile        = "${module.service-data.iam_profile}"
-  user_data          = <<END_INIT
+  iam_profile = "${module.service-data.iam_profile}"
+
+  user_data = <<END_INIT
 #!/bin/bash
 ${var.init_prefix}
 ${module.init-attach-ebs.init_snippet}
 ${var.init_suffix}
 END_INIT
 }
+
 # boxed module to attach the EBS volume to the node
 module "init-attach-ebs" {
   source    = "../init-snippet-attach-ebs-volume"
--- tf-modules/single-node-asg/outputs.tf
+++ tf-modules/single-node-asg/outputs.tf
@@ -1,4 +1,4 @@
 //`name` exported from the Server `aws_autoscaling_group`
 output "asg_name" {
-    value = "${module.server.name}"
+  value = "${module.server.name}"
 }
