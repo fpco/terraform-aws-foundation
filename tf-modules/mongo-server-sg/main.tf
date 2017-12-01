cea0a2d7a06d7e31fffacb85fe26ac58e0740675
user <ketzacoatl@users.noreply.github.com>
2017-12-01 08:35:07 -0500

    mongo-server-sg: add `description` variable, for ingress rules

--- tf-modules/mongo-server-sg/main.tf
+++ tf-modules/mongo-server-sg/main.tf
@@ -13,9 +13,14 @@ variable "cidr_blocks" {
   type        = "list"
 }
 
+variable "description" {
+  description = "use this string to generate a description for the SG rules"
+  default     = "Allow ingress, mongod's "
+}
 # Security group for mongod servers
 resource "aws_security_group_rule" "mongo_tcp_1" {
   type              = "ingress"
+  description       = "${var.description} client port 27017 (TCP)"
   from_port         = "27017"
   to_port           = "27017"
   protocol          = "tcp"
@@ -25,6 +30,7 @@ resource "aws_security_group_rule" "mongo_tcp_1" {
 
 resource "aws_security_group_rule" "mongo_tcp_2" {
   type              = "ingress"
+  description       = "${var.description} admin port 28017 (TCP)"
   from_port         = "28017"
   to_port           = "28017"
   protocol          = "tcp"
