4c50052347a7fc40c9cc7a84c90fbec1a482411f
user <ketzacoatl@users.noreply.github.com>
2017-12-01 16:07:53 -0500

    init-snippet-nomad-agent: whitespace fixup

--- tf-modules/init-snippet-nomad-agent/variables.tf
+++ tf-modules/init-snippet-nomad-agent/variables.tf
@@ -1,23 +1,28 @@
 variable "bootstrap_pillar_file" {
-    default = "/srv/pillar/bootstrap.sls"
-    description = "path, to the 'bootstrap' pillar file"
+  default     = "/srv/pillar/bootstrap.sls"
+  description = "path, to the 'bootstrap' pillar file"
 }
+
 variable "nomad_pillar" {
-    description = "pillar to pass to bootstrap.sls, for nomad.service formula"
+  description = "pillar to pass to bootstrap.sls, for nomad.service formula"
 }
+
 variable "init_prefix" {
-    default = ""
-    description = "initial init (shellcode) to prefix this snippet with"
+  default     = ""
+  description = "initial init (shellcode) to prefix this snippet with"
 }
+
 variable "init_suffix" {
-    default = ""
-    description = "init (shellcode) to append to the end of this snippet"
+  default     = ""
+  description = "init (shellcode) to append to the end of this snippet"
 }
+
 variable "log_level" {
-    default = "info"
-    description = "set log verbosity on node init, for CM with saltstack"
+  default     = "info"
+  description = "set log verbosity on node init, for CM with saltstack"
 }
+
 variable "log_prefix" {
-    default = "OPS:"
-    description = "string to prefix log messages with"
+  default     = "OPS:"
+  description = "string to prefix log messages with"
 }
