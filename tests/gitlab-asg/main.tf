bd49ebb53edc2521fc0cd3937a4c041aa74d3816
user <ketzacoatl@users.noreply.github.com>
2017-12-02 16:37:58 -0500

    tests/gitlab-asg: sync with the gitlab-asg and init-snippet modules
    Drop the `gitlab-asg` module in favor of `init-snippet-gitlab-docker`,
    focus on providing an init-snippet for running gitlab with docker. This
    commit updates the test environment that uses / tests the init-snippet
    module and demos how to run gitlab with an ASG and EBS.

--- tests/gitlab-asg/main.tf
+++ tests/gitlab-asg/main.tf
@@ -1,3 +1,25 @@
+/**
+ * ## Test Gitlab on a Single-Node ASG
+ * 
+ * Run a Gitlab on a single EC2 instance.
+ * This instance will be part of a single-node autoscaling group
+ * that shares an EBS volume to store data.
+ *
+ * Note that there is a peculiarity with the EBS volume in that it
+ * requires some manual setup the very first time to make it available
+ * for use (unless a snapshot id is supplied):
+ *
+ * parted --script /dev/xvdf -- mklabel msdos
+ * parted --script /dev/xvdf -- mkpart primary 0 -1
+ * mkfs -t ext4 -F /dev/xvdf1
+ * e2label /dev/xvdf1 gitlab
+ *
+ * After running the above code to initialise the EBS, terminate the instance
+ * and the autoscaling group will bring up a new instance that will be running
+ * gitlab once it is done initialising.
+ * 
+ */
+
 variable "name" {
   default = "gitlab-asg-test"
 }
@@ -6,8 +28,12 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "key_file" {
-  description = "The secret key file to use"
+variable "ssh_pubkey" {
+  description = "The path to the SSH pub key to use"
+}
+
+provider "aws" {
+  region = "${var.region}"
 }
 
 data "aws_availability_zones" "available" {}
@@ -19,20 +45,51 @@ module "ubuntu-xenial-ami" {
 
 resource "aws_key_pair" "main" {
   key_name   = "${var.name}"
-  public_key = "${file(var.key_file)}"
+  public_key = "${file(var.ssh_pubkey)}"
 }
 
-module "setup" {
-  name_prefix        = "${var.name}"
-  source             = "../../tf-modules/gitlab-single-node-asg"
-  region             = "${var.region}"
-  az                 = "${element(data.aws_availability_zones.available.names, 0)}"
-  key_file           = "${var.key_file}"
-  key_name           = "${aws_key_pair.main.key_name}"
-  instance_ami       = "${module.ubuntu-xenial-ami.id}"
-  subnet_id          = "${module.vpc.public_subnet_ids[0]}"
-  vpc_id             = "${module.vpc.vpc_id}"
-  security_group_ids = ["${aws_security_group.gitlab.id}"]
+module "gitlab-asg" {
+  source        = "../../tf-modules/single-node-asg"
+  name_prefix   = "${var.name}"
+  name_suffix   = "gitlab-server"
+  region        = "${var.region}"
+  az            = "${element(data.aws_availability_zones.available.names, 0)}"
+  key_name      = "${aws_key_pair.main.key_name}"
+  ami           = "${module.ubuntu-xenial-ami.id}"
+  instance_type = "t2.medium"
+  subnet_id     = "${module.vpc.public_subnet_ids[0]}"
+
+  security_group_ids    = ["${aws_security_group.gitlab.id}"]
+  data_volume_encrypted = false
+
+  init_prefix = <<END_INIT
+apt-get update
+${module.init-install-awscli.init_snippet}
+${module.init-install-ops.init_snippet}
+END_INIT
+
+  init_suffix = <<END_INIT
+mkdir -p /gitlab
+mount /dev/xvdf1 /gitlab
+
+cp /etc/fstab /etc/fstab.orig
+echo "LABEL=gitlab            /gitlab  ext4   defaults,nofail     0 2" >> /etc/fstab
+
+apt-get install -y docker docker.io
+${module.init-gitlab-docker.init_snippet}
+END_INIT
+}
+
+module "init-install-awscli" {
+  source = "../../tf-modules/init-snippet-install-awscli"
+}
+
+module "init-install-ops" {
+  source = "../../tf-modules/init-snippet-install-ops"
+}
+
+module "init-gitlab-docker" {
+  source = "../../tf-modules/init-snippet-gitlab-docker"
 }
 
 module "vpc" {
@@ -59,6 +116,7 @@ module "ssh-rule" {
 module "http-rule" {
   source            = "../../tf-modules/single-port-sg"
   port              = 80
+  description       = "Allow ingress for HTTP, port 80 (TCP)"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.gitlab.id}"
 }
@@ -66,6 +124,7 @@ module "http-rule" {
 module "https-rule" {
   source            = "../../tf-modules/single-port-sg"
   port              = 443
+  description       = "Allow ingress for HTTPS, port 443 (TCP)"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.gitlab.id}"
 }
@@ -73,6 +132,7 @@ module "https-rule" {
 module "gitlab-ssh-rule" {
   source            = "../../tf-modules/single-port-sg"
   port              = 8022
+  description       = "Allow ingress for Git over SSH, port 8022 (TCP)"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.gitlab.id}"
 }
