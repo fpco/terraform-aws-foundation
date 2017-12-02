289854163c7dfe8c98749fd77447a0687c120723
user <ketzacoatl@users.noreply.github.com>
2017-12-02 13:48:08 -0500

    ec2-fixed-ip-auto-recover-instances: add support for NAT
    Use this module for NAT is now possible:
    
    * add support for public instances via a boolean (`public_ip` variable)
    * make `source_dest_check` available
    * let `iam_profiles` be empty, without erroring on use of `element()`
    * add `public_ips` output

--- tf-modules/ec2-fixed-ip-auto-recover-instances/outputs.tf
+++ tf-modules/ec2-fixed-ip-auto-recover-instances/outputs.tf
@@ -1,9 +1,14 @@
-// Instance ID(s) of the server(s)
+// List of instance ID(s) of the server(s)
 output "instance_ids" {
   value = ["${aws_instance.auto-recover.*.id}"]
 }
 
-// Private IP address(es) of the server(s)
+// List of private IP address(es) of the server(s)
 output "private_ips" {
   value = ["${var.private_ips}"]
+}
+
+// List of public IP address(es) for the server(s)
+output "public_ips" {
+  value = ["${aws_instance.auto-recover.*.public_ip}"]
 }
