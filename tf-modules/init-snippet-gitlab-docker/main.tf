97789713126e5c7b089582e471be774550ebdd7f
user <ketzacoatl@users.noreply.github.com>
2017-12-02 16:36:32 -0500

    init-snippet-gitlab-docker: refactor into init_snippet
    don't wrap the single-node-asg, just provide init_snippet to run gitlab
    with docker.

--- tf-modules/init-snippet-gitlab-docker/main.tf
+++ tf-modules/init-snippet-gitlab-docker/main.tf
@@ -1,75 +1,72 @@
 /**
- * Run a Gitlab on a single EC2 instance.
- * This instance will be part of a single-node autoscaling group
- * that shares an EBS volume to store data.
- *
- * Note that there is a peculiarity with the EBS volume in that it
- * requires some manual setup the very first time to make it available
- * for use (unless a snapshot id is supplied):
- *
- * parted --script /dev/xvdf -- mklabel msdos
- * parted --script /dev/xvdf -- mkpart primary 0 -1
- * mkfs -t ext4 -F /dev/xvdf1
- * e2label /dev/xvdf1 gitlab
- *
- * After running the above code to initialise the EBS, terminate the instance
- * and the autoscaling group will bring up a new instance that will be running
- * gitlab once it is done initialising.
+ * ## init-snippet to run gitlab w/ docker
  */
 
-module "gitlab-asg" {
-  source                  = "../single-node-asg"
-  name                    = "${var.name_prefix}"
-  az                      = "${var.az}"
-  key_name                = "${var.key_name}"
-  ami                     = "${var.instance_ami}"
-  instance_type           = "${var.instance_type}"
-  name_suffix             = "gitlab-asg"
-  subnet_id               = "${var.subnet_id}"
-  security_group_ids      = "${var.security_group_ids}"
-  region                  = "${var.region}"
-  root_volume_type        = "${var.root_volume_type}"
-  root_volume_size        = "${var.root_volume_size}"
-  data_volume_type        = "${var.data_volume_type}"
-  data_volume_size        = "${var.data_volume_size}"
-  data_volume_encrypted   = "${var.data_volume_encrypted}"
-  data_volume_kms_key_id  = "${var.data_volume_kms_key_id}"
-  data_volume_snapshot_id = "${var.data_volume_snapshot_id}"
-  data_volume_iops        = "${var.data_volume_iops}"
-  aws_cloud               = "${var.aws_cloud}"
+variable "init_prefix" {
+  default     = ""
+  description = "initial init (shellcode) to prefix this snippet with"
+}
 
-  init_prefix = <<END_INIT
-apt-get update
-${module.init-install-awscli.init_snippet}
-${module.init-install-ops.init_snippet}
-END_INIT
+variable "init_suffix" {
+  default     = ""
+  description = "init (shellcode) to append to the end of this snippet"
+}
 
-  init_suffix = <<END_INIT
-mkdir -p /gitlab
-mount /dev/xvdf1 /gitlab
+variable "log_prefix" {
+  default     = "OPS: "
+  description = "string to prefix log messages with"
+}
 
-cp /etc/fstab /etc/fstab.orig
-echo "LABEL=gitlab            /gitlab  ext4   defaults,nofail     0 2" >> /etc/fstab
+variable "gitlab_ssh_port" {
+  default     = "8022"
+  description = "The port to use for ssh access to the gitlab instance"
+}
 
-apt-get install -y docker docker.io
+variable "gitlab_http_port" {
+  default     = "80"
+  description = "The port to use for http access to the gitlab instance"
+}
+
+variable "gitlab_https_port" {
+  default     = "443"
+  description = "The port to use for https access to the gitlab instance"
+}
+
+variable "gitlab_image_tag" {
+  default     = "latest"
+  description = "The tag for the docker image"
+}
+
+variable "gitlab_image_repo" {
+  default     = "gitlab/gitlab-ce"
+  description = "The name (repo) of the docker image"
+}
+
+variable "gitlab_data_path" {
+  default     = "/gitlab"
+  description = "path for gitlab data"
+}
+
+# render init script snippet from the template
+data "template_file" "init_snippet" {
+  template = <<END_INIT
+# start snippet - run gitlab docker image
+${var.init_prefix}
 cmd="docker run --detach \
+    --restart always \
     --publish ${var.gitlab_https_port}:443 \
     --publish ${var.gitlab_http_port}:80 \
     --publish ${var.gitlab_ssh_port}:22 \
-    --restart always \
-    --volume /gitlab/config:/etc/gitlab \
-    --volume /gitlab/logs:/var/log/gitlab \
-    --volume /gitlab/data:/var/opt/gitlab \
-    gitlab/gitlab-ce"
+    --volume ${var.gitlab_data_path}/config:/etc/gitlab \
+    --volume ${var.gitlab_data_path}/logs:/var/log/gitlab \
+    --volume ${var.gitlab_data_path}/data:/var/opt/gitlab \
+    ${var.gitlab_image_repo}:${var.gitlab_image_tag}"
 echo "$cmd" > /etc/rc.local
-$cmd
+$$cmd
+${var.init_suffix}
 END_INIT
 }
 
-module "init-install-awscli" {
-  source = "../../tf-modules/init-snippet-install-awscli"
-}
-
-module "init-install-ops" {
-  source = "../../tf-modules/init-snippet-install-ops"
+output "init_snippet" {
+  value = "${data.template_file.init_snippet.rendered}"
 }
