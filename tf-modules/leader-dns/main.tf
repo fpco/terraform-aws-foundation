aa07aa9777f4ec46e1d7effe0868b6e7800f4413
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:58:14 -0500

    leader-dns: doc fixup

--- tf-modules/leader-dns/main.tf
+++ tf-modules/leader-dns/main.tf
@@ -1,15 +1,17 @@
 /**
- *## AWS Route 53 Zone and Records for Consul Leaders
+ * ## AWS Route 53 Zone and Records for Consul Leaders
  *
- *This module is for use with a cluster of Consul leaders, it provides a DNS
- *zone and records that point to the IPs where the consul leaders _might_ be.
- *While there may only be 3 to 9 Consul leaders, those leaders may move around
- *(they are an auto-scaling group), and we don't knoe _exactly_ where they are,
- *but if a consul agent uses the DNS record from this module, the agent will
- *continue to retry until it has found a leader. In practice, this works well,
- *and Consul agents will find their leaders in 5 - 90 seconds (depending on
- *the retry period and other settings configured in each agent).
+ * This module is for use with a cluster of Consul leaders, it provides a DNS
+ * zone and records that point to the IPs where the consul leaders _might_ be.
+ * While there may only be 3 to 9 Consul leaders, those leaders may move around
+ * (they are an auto-scaling group), and we don't knoe _exactly_ where they are,
+ * but if a consul agent uses the DNS record from this module, the agent will
+ * continue to retry until it has found a leader. In practice, this works well,
+ * and Consul agents will find their leaders in 5 - 90 seconds (depending on
+ * the retry period and other settings configured in each agent).
+ *
  */
+
 resource "aws_route53_zone" "leaders" {
   name   = "${var.domain}"
   vpc_id = "${var.vpc_id}"
