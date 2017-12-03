25d11dad3ccc7774e81e20c346f059dfbed4998e
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:50:50 -0500

    snapshot-bucket: doc fixup

--- tf-modules/snapshot-bucket/main.tf
+++ tf-modules/snapshot-bucket/main.tf
@@ -1,8 +1,11 @@
 /**
- *## Snapshot Bucket
+ * ## Snapshot Bucket
+ *
+ * **DEPRECATED: WILL BE REMOVED IN A FUTURE RELEASE**
  *
  * This module will create an S3 bucket with an associated IAM policy and access
  * key that provide access to that bucket.
+ *
  */
 resource "aws_iam_user" "s3" {
   name = "${var.name}"
