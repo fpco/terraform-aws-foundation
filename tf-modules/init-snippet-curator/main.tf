1ef483490cfefe9f17f893103654474d118ee46d
user <ketzacoatl@users.noreply.github.com>
2017-12-03 12:02:12 -0500

    init-snippet-curator: doc fixup

--- tf-modules/init-snippet-curator/main.tf
+++ tf-modules/init-snippet-curator/main.tf
@@ -1,3 +1,19 @@
+/**
+ * ## Init Snippet: Curator
+ *
+ * This module generates an init snippet that does the following for an
+ * ubuntu or other apt-based system:
+ *
+ * * `apt-get update`
+ * * install `python-pip` with apt
+ * * install `elasticsearch-curator` with `pip`
+ * * add a user `curator`
+ * * make the `/var/log/curator/` directory, owned by the `curator` user
+ * * add a cronjob for this user, to run `/usr/local/bin/curator` at `30 1 * * *`
+ * * write out the config files needed by the curator
+ *
+ */
+
 data "template_file" "curator-setup" {
   template = "${file("${path.module}/snippet.tpl.sh")}"
 
