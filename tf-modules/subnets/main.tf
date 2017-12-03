01e5810be8f6a08d72e8dc577f7d9ecf039ae7bb
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:50:19 -0500

    subnets: doc fixup

--- tf-modules/subnets/main.tf
+++ tf-modules/subnets/main.tf
@@ -1,8 +1,10 @@
 /**
- *## Subnets creation
+ * ## AWS Subnets
  *
  * This module creates one or more subnets, interleaving them across a list of
- * availiability zones.
+ * availiability zones, supports `extra_tags`, and enabling/disabling public
+ * IPs by default. Use this module multiple times to create different sets of
+ * subnets for different purposes or characteristics.
  *
  */
 
