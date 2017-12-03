556bc37fa23c045528e1acd5d102ada98c1d52ff
user <ketzacoatl@users.noreply.github.com>
2017-12-03 12:02:00 -0500

    init-snippet-exec: doc fixup

--- tf-modules/init-snippet-exec/main.tf
+++ tf-modules/init-snippet-exec/main.tf
@@ -1,3 +1,12 @@
+/**
+ * ## Init Snippet: Shell Exec
+ *
+ * This module is a bit more of a simple pass-through wrapper for the
+ * `template_file` resource so we maintain the same code-flow with "random"
+ * shell script for init snippets as we do our other "boxed" snippets.
+ *
+ */
+
 variable "init" {
   description = "init shellcode for this snippet"
 }
@@ -10,6 +19,7 @@ data "template_file" "init_snippet" {
   }
 }
 
+// Rendered "init snippet" from the template file
 output "init_snippet" {
   value = "${data.template_file.init_snippet.rendered}"
 }
