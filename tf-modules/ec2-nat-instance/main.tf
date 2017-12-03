c026853ccfc1f3b93c9ce37076173c1f1669dd5b
ketzacoatl <ketzacoatl@users.noreply.github.com>
2017-12-02 19:57:52 -0500

    ec2-nat-instance: one more test on terraform-docs
    OK, not the extra space.. but need to open with `/**`, not `/*`

--- tf-modules/ec2-nat-instance/main.tf
+++ tf-modules/ec2-nat-instance/main.tf
@@ -1,12 +1,12 @@
-/*
- *## EC2 NAT Instance
+/**
+ * ## EC2 NAT Instance
  *
- *This module creates a single EC2 instance in one public subnet, to provide
- *NAT to one or more private subnets. The instance will use the latest Ubuntu
- *16.04 AMI.
+ * This module creates a single EC2 instance in one public subnet, to provide
+ * NAT to one or more private subnets. The instance will use the latest Ubuntu
+ * 16.04 AMI.
  *
- *The module will output the instance ID for use in configuring a route table
- *for those private subnets.
+ * The module will output the instance ID for use in configuring a route table
+ * for those private subnets.
  *
  */
 
