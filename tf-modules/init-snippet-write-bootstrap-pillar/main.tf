1f23e9086c05ad1bb4ca787d5758bf771e51416e
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:59:33 -0500

    init-snippet-write-bootstrap-pillar: doc fixup

--- tf-modules/init-snippet-write-bootstrap-pillar/main.tf
+++ tf-modules/init-snippet-write-bootstrap-pillar/main.tf
@@ -1,3 +1,12 @@
+/**
+ * ## Init Snippet: Write Bootstrap Pillar
+ *
+ * Generate an init snippet that writes out bootstrap pillar to a specific
+ * file. This is helpful for generating a large chunk of bootstrap pillar
+ * all at once before applying formula with saltstack.
+ *
+ */
+
 variable "bootstrap_pillar_file" {
   default     = "/srv/pillar/bootstrap.sls"
   description = "path, to the 'bootstrap' pillar file"
