43b7ae2fe1a1cc706d96a7d3d6d1f528aa0a4ee0
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:40:57 -0500

    consul-leader-sg: doc fixup

--- tf-modules/consul-leader-sg/main.tf
+++ tf-modules/consul-leader-sg/main.tf
@@ -1,11 +1,37 @@
 /**
- *## Ingress Rules for Consul Leaders
+ * ## Ingress Rules for Consul Leaders
+ * 
+ * Set of security group ingress rules for use with a cluster of consul leaders.
+ * These are the ingress rules for leaders connecting to other leaders.
+ * 
+ * See `consul-agents-sg` for Agents, or `consul-leaders-wan-sg` for the
+ * cross-WAN leader communication.
  *
- *Set of security group ingress rules for use with a cluster of consul leaders.
- *These are the ingress rules for leaders connecting to other leaders.
+  * ### Example
+ * 
+ * ```
+ * # Security Group for nomad workers
+ * module "leaders-sg" {
+ *   source      = "../tf-modules/security-group-base"
+ *   name        = "${var.name}-leaders"
+ *   description = "security group for consul leader instances in the private subnet"
+ *   vpc_id      = "${module.vpc.vpc_id}"
+ * }
  *
- *See `consul-agents-sg` for Agents, or `consul-leaders-wan-sg` for the
- *cross-WAN leader communication.
+ * module "leaders-consul-leader-rules" {
+ *   source            = "../tf-modules/consul-leader-sg"
+ *   cidr_blocks       = ["${var.vpc_cidr}"]
+ *   security_group_id = "${module.leaders-sg.id}"
+ * }
+ *
+ * ...
+ * 
+ * module "my-cluster" {
+ *   source             = "../tf-modules/asg"
+ *   ...
+ *   security_group_ids = ["${module.leaders-sg.id}"]
+ * }
+ * ```
  *
  */
 
