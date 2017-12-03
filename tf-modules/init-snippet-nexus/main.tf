d694673020f48078be164a13691aabf912d93190
user <ketzacoatl@users.noreply.github.com>
2017-12-03 12:00:14 -0500

    init-snippet-nexus: doc fixup

--- tf-modules/init-snippet-nexus/main.tf
+++ tf-modules/init-snippet-nexus/main.tf
@@ -1,3 +1,16 @@
+/**
+ * ## Init Snippet: Run Nexus (w/ Docker)
+ *
+ * Generate an init snippet to use Docker to run the Nexus package manager:
+ *
+ * * use `apt-get` to install docker
+ * * create `/etc/rc.local` to create/chown the `data_path` and run the
+ *   docker image
+ *
+ * The `sonatype/nexus` docker image is run.
+ *
+ */
+
 variable "device_path" {
   default     = "/dev/xvdf"
   description = "path, to the device's path in /dev/"
