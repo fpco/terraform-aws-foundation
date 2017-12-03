adf761e3793aa4ab884c67d704b6577ab768011f
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:51:29 -0500

    single-node-asg: doc fixup

--- tf-modules/single-node-asg/main.tf
+++ tf-modules/single-node-asg/main.tf
@@ -1,11 +1,11 @@
 /**
- *## Single-Node Auto-Scaling Group
+ * ## Single-Node Auto-Scaling Group
  *
  * This module uses an EBS volume and an auto-scaling group with a single-node
  * to establish a reliable and robust pattern for HA in various forms.
  *
  * NOTE: This module assumes that all the dependencies for the
- * "../init-snippet-attach-ebs-volume" module are satisfied
+ * `../init-snippet-attach-ebs-volume` module are satisfied
  * by the `init_prefix`.
  * In particular, this means that the aws cli tool and ec2metadata are installed.
  *
