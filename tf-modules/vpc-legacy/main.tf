a1da4a4af748a60d6888d428c85d22a5976d08e2
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:47:31 -0500

    vpc-legacy: doc fixup

--- tf-modules/vpc-legacy/main.tf
+++ tf-modules/vpc-legacy/main.tf
@@ -1,8 +1,13 @@
 /**
- *## Virtual Private Cloud (VPC)
+ * ## Legacy VPC Module
  *
- * This module takes care of VPC deployment. Scenarios 1 and 2 are possible with
- * this module: http://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_Scenarios.html
+ * This module takes care of VPC (Virtual Private Cloud) deployment. Scenarios 1 and 2 are possible with
+ * this module:
+ * http://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_Scenarios.html
+ *
+ * We recommend using the new `vpc-scenario-*` modules instead of this one.
+ *
+ * **DEPRECATED: WILL BE REMOVED IN A FUTURE RELEASE**
  *
  */
 resource "aws_vpc" "main" {
