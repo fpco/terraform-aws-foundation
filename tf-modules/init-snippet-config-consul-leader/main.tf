ce0e22299208c9df23061a18effe5a26617679af
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:45:13 -0500

    init-snippet/config-consul-leader: doc fixup

--- tf-modules/init-snippet-config-consul-leader/main.tf
+++ tf-modules/init-snippet-config-consul-leader/main.tf
@@ -1,3 +1,11 @@
+/**
+ * ## Init Snippet: Config Consul Leader
+ *
+ * Without CM/SaltStack/Ansible/etc, just render a config file. Pretty static,
+ * very simple, but good enough for basic demos, proofs, and tests.
+ *
+ */
+
 # variables used by this snippet of init shellcode
 variable "encrypt" {
   description = "base64 encoded key for consul gossip encryption"
