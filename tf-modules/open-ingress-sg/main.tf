441d19547c17014b1d9250824ab12e99e5af25a5
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:56:08 -0500

    open-ingress-sg: doc fixup

--- tf-modules/open-ingress-sg/main.tf
+++ tf-modules/open-ingress-sg/main.tf
@@ -1,5 +1,5 @@
 /**
- *## Open Ingress Security Group Rule
+ * ## Open Ingress Security Group Rule
  *
  * Create a simple and reusable security group rule that opens all ports for
  * both TCP and UDP traffic (unrestricted).
