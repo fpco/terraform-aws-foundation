cd14cf7a1d943287e89c8cf68868c1bd54caecd3
user <ketzacoatl@users.noreply.github.com>
2017-12-03 12:00:49 -0500

    init-snippet-install-consul: doc fixup

--- tf-modules/init-snippet-install-consul/main.tf
+++ tf-modules/init-snippet-install-consul/main.tf
@@ -1,3 +1,18 @@
+/**
+ * ## Init Snippet: Install Consul
+ *
+ * Generate an init snippet to install the AWS cli tool on boot. This will:
+ *
+ * * wget the consul zip file based on the version and base url variables
+ * * unzip the archive to `/usr/local/bin/`
+ * * run `consul version` to confirm we can see/run the executable
+ * * add a `consul` user with `useradd`, with its home set to `data_dir`
+ *
+ * **NOTE: this module does not validate the release checksum or setup consul
+ * to run as a system service.
+ *
+ */
+
 # variables used by this snippet of init shellcode
 variable "version" {
   default     = "0.9.3"
