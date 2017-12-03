bfe53d49506d5fd45b69b88ce9038dd54a1dffa7
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:55:25 -0500

    persistent-ebs: doc fixup

--- tf-modules/persistent-ebs/main.tf
+++ tf-modules/persistent-ebs/main.tf
@@ -1,10 +1,11 @@
 /**
- *## Persistent Data on EBS
+ * ## Persistent Data on EBS
  *
  * This module provides an EBS volume and associated IAM profile/role to be
  * used with an EC2 instance or auto-scaling group. This module is best when
  * used in conjunction with a single-node auto-scaling group, and with the
  * init-snippet that attaches the named EBS volume on boot.
+ *
  */
 resource "aws_ebs_volume" "main" {
   availability_zone = "${var.az}"
