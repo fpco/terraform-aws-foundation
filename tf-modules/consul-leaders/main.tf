b0117d4dd466bc42b866dca5bdce1e0e2170103a
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:41:09 -0500

    consul-leaders: doc fixup

--- tf-modules/consul-leaders/main.tf
+++ tf-modules/consul-leaders/main.tf
@@ -1,23 +1,27 @@
 /**
- *## Consul Leaders (built on Auto-Scaling Groups)
+ * ## Consul Leaders (built on Auto-Scaling Groups)
+ * 
+ * **DEPRECATED**
  *
- *This module reuses two other modules in this suite:
+ * This module reuses two other modules in this suite:
+ * 
+ * * `asg`
+ * * `cluster-net`
+ * 
+ * The purpose of this module is to pair the ASG with subnets created specifically
+ * for the ASG. Similarly, the individual parts (`asg` / `cluster-net`), exist to
+ * allow flexibility, and to enable user-developers with the option of putting a
+ * new ASG into an existing network, and upfront or further down the line.
+ * 
+ * While this module is similar to `consul-cluster`, there are a few notable
+ * differences:
+ * 
+ * * The CIDR blocks are significantly smaller to limit the IP space, which
+ *   simplifies bootstrapping the leaders and client agents.
+ * * This module suggests different max/min node counts as defaults
  *
- ** `asg`
- ** `cluster-net`
- *
- *The purpose of this module is to pair the ASG with subnets created specifically
- *for the ASG. Similarly, the individual parts (`asg` / `cluster-net`), exist to
- *allow flexibility, and to enable user-developers with the option of putting a
- *new ASG into an existing network, and upfront or further down the line.
- *
- *While this module is similar to `consul-cluster`, there are a few notable
- *differences:
- *
- ** The CIDR blocks are significantly smaller to limit the IP space, which
- *  simplifies bootstrapping the leaders and client agents.
- ** This module suggests different max/min node counts as defaults
  */
+
 # ASG cluster with consul leaders
 module "leader-asg" {
   source             = "../asg"
