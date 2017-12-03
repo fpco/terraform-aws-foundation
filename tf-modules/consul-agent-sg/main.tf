cf299ed5d20ad7d0cbdf6e7cdb130763775f12e7
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:40:29 -0500

    consul-agent-sg: doc fixup

--- tf-modules/consul-agent-sg/main.tf
+++ tf-modules/consul-agent-sg/main.tf
@@ -1,27 +1,37 @@
 /**
- *## Ingress Rules for Consul Agents
+ * ## Ingress Rules for Consul Agents
+ * 
+ * This module creates `aws_security_group_rule` resources, defining an ingress
+ * rule to allow port `8301`, for TCP and UDP each. Use with a security
+ * group on any nodes you wish to use the consul agent on.
+ * 
+ * 
+ * ### Example
+ * 
+ * ```
+ * # Security Group for nomad workers
+ * module "workers-sg" {
+ *   source      = "../tf-modules/security-group-base"
+ *   name        = "${var.name}-workers"
+ *   description = "security group for worker instances in the private subnet"
+ *   vpc_id      = "${module.vpc.vpc_id}"
+ * }
  *
- *This module creates `aws_security_group_rule` resources, defining an ingress
- *rule to allow port `8301`, for TCP and UDP each. Use with a security
- *group on any nodes you wish to use the consul agent on.
+ * module "workers-consul-agent-rule" {
+ *   source            = "../tf-modules/consul-agent-sg"
+ *   cidr_blocks       = ["${var.vpc_cidr}"]
+ *   security_group_id = "${module.workers-sg.id}"
+ * }
  *
+ * ...
+ * 
+ * module "my-cluster" {
+ *   source             = "../tf-modules/asg"
+ *   ...
+ *   security_group_ids = ["${module.workers-sg.id}"]
+ * }
+ * ```
  *
- *### Example
- *
- * UPDATE THESE DOCS
- *
- *```
- *# boxed security group for consul leader services, no egress/custom rules
- *module "consul-agent-sg" {
- *    source      = "../tf-modules/consul-agent-sg"
- *    cidr_blocks = ["${module.test-vpc.cidr_block}"]
- *}
- *
- *module "my-cluster" {
- *    source = "../tf-modules/consul-cluster"
- *    ...
- *    cluster_security_group_ids = "${module.consul-agent-sg.id}, ${aws_security_group.worker-service.id}, ${module.public-ssh-sg.id}"
- *```
  */
 
 # ingress rules for consul agents. Required by all agents. 
