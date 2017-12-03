46cf756336727a1ac923b798a2aebbc964f8b3c6
user <ketzacoatl@users.noreply.github.com>
2017-12-03 12:01:37 -0500

    init-snippet-hostname-simple: doc fixup

--- tf-modules/init-snippet-hostname-simple/main.tf
+++ tf-modules/init-snippet-hostname-simple/main.tf
@@ -1,3 +1,13 @@
+/**
+ * ## Init Snippet: Hostname (simple shell version)
+ *
+ * Generate an init snippet that uses `hostnamectl` to update the hostname.
+ * The hostname is also updated in `/etc/hosts`. The hostname is derived from
+ * the instance ID and the `hostname_prefix` variable, and will look like:
+ * `web-server-i-101abd22`
+ *
+ */
+
 # variables used by this snippet of init shellcode
 variable "init_prefix" {
   default     = ""
