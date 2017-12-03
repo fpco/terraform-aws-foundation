d6c9b3c1734c57c56ff7affbc677a718c90c012a
user <ketzacoatl@users.noreply.github.com>
2017-12-03 12:00:01 -0500

    init-snippet-nomad-agent: doc fixup

--- tf-modules/init-snippet-nomad-agent/main.tf
+++ tf-modules/init-snippet-nomad-agent/main.tf
@@ -1,3 +1,15 @@
+/**
+ * ## Init Snippet: Nomad Agent
+ *
+ * Generate an init snippet to configure and run the `nomad` agent
+ * using SaltStack:
+ *
+ * * write out bootstrap pillar
+ * * use `salt-call` to apply the `nomad.service` formula from
+ *   `fpco-salt-formula`
+ *
+ */
+
 data "template_file" "init_snippet" {
   template = "${file("${path.module}/snippet.tpl")}"
 
