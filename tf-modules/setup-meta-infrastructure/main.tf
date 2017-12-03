d8ef8c6bd5adc7bb157f69c5c1493c6e6d9f103f
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:52:07 -0500

    setup-meta-infrastructure: doc fixup

--- tf-modules/setup-meta-infrastructure/main.tf
+++ tf-modules/setup-meta-infrastructure/main.tf
@@ -1,10 +1,11 @@
 /**
- *## MFA and IAM
+ * ## MFA and IAM
  *
  * This module provides a "boxed" set of IAM groups and policies suitable
  * for managing account access through IAM and leveraging MFA.
  *
  * NOTE: need to document each group, policy, and the resulting permissions
+ *
  */
 
 #----------------------------------------------------------------------
