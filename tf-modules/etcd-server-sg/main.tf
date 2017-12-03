2ee50464c55a36cc92ee06bff10ecd810bcb81a2
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:43:54 -0500

    etcd-server-sg: doc fixup

--- tf-modules/etcd-server-sg/main.tf
+++ tf-modules/etcd-server-sg/main.tf
@@ -1,5 +1,9 @@
-/*
- * ADD DOCS
+/**
+ * ## etcd Server Security Group Rules
+ *
+ * Attach an `aws_security_group_rule` to the specified security group. Allow
+ * TCP on ports `2379` and `2380`.
+ *
  */
 
 variable "security_group_id" {
