2faa9ea165dc0d55897c91c11072ebe7db85f22c
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:39:16 -0500

    aws-ipsec-vpn: doc fixup

--- tf-modules/aws-ipsec-vpn/main.tf
+++ tf-modules/aws-ipsec-vpn/main.tf
@@ -1,20 +1,43 @@
+/**
+ * ## AWS IPSEC VPN
+ *
+ * This module packages the various resources needed to setup an IPSEC VPN
+ * on AWS:
+ *
+ * * `aws_vpn_gateway`
+ * * `aws_customer_gateway`
+ * * `aws_vpn_connection`
+ * * `aws_vpn_connection_route`
+ *
+ * See the [`vpc-scenario-4` module](https://github.com/fpco/fpco-terraform-aws/tree/master/tf-modules/vpc-scenario-4)
+ * in this repo for an example that uses this module.
+ *
+ */
+
 variable "name" {
   description = "Used to name the various VPN resources"
+  type        = "string"
 }
+
 variable "vpc_id" {
   description = "ID of the VPC to associate the VPN with"
+  type        = "string"
 }
+
 variable "remote_device_ip" {
   description = "The public IP address of the remote (client) device"
+  type        = "string"
 }
+
 variable "static_routes" {
-  type        = "list"
   description = "The list of CIDR blocks to create static routes for"
+  type        = "list"
 }
+
 variable "extra_tags" {
-  type        = "map"
-  default     = {}
   description = "Extra tags to append to various AWS resources"
+  default     = {}
+  type        = "map"
 }
 
 resource "aws_vpn_gateway" "main" {
