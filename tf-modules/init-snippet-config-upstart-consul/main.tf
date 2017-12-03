044b99ebc54640c8a15335409072b2aa6a45837d
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:45:30 -0500

    init-snippet/config-upstart-consul: doc fixup

--- tf-modules/init-snippet-config-upstart-consul/main.tf
+++ tf-modules/init-snippet-config-upstart-consul/main.tf
@@ -1,3 +1,11 @@
+/**
+ * ## Init Snippet: Config Upstart, run Consul
+ *
+ * Generate an init snippet that will configure Upstart to run `consul` as a
+ * system service.
+ *
+ */
+
 # variables used by this snippet of init shellcode
 variable "upstart_config" {
   default     = "/etc/init/consul.conf"
