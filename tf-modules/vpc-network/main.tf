644fd7181f38be61562e83e2ade587ec7d97f39e
user <ketzacoatl@users.noreply.github.com>
2017-12-03 11:48:00 -0500

    vpc-network: doc fixup

--- tf-modules/vpc-network/main.tf
+++ tf-modules/vpc-network/main.tf
@@ -1,12 +1,17 @@
 /**
- *## VPC with Basic Network
+ * ## VPC with Basic Network
  *
- *This module will create:
+ * This module will create:
  *
- ** `aws_vpc`
- ** `aws_internet_gateway`
- ** `aws_route_table`
- ** `aws_main_route_table_association`
+ * * `aws_vpc`
+ * * `aws_internet_gateway`
+ * * `aws_route_table`
+ * * `aws_main_route_table_association`
+ *
+ * We recommend using the `vpc-scenario-*` modules instead of this one.
+ *
+ * **DEPRECATED: MIGHT BE REMOVED IN A FUTURE RELEASE**
+ *
  */
 resource "aws_vpc" "core" {
   cidr_block           = "${var.vpc_cidr_prefix}.0.0/16"
