df34bbafba089f37820af559d7082f6d34db8e24
user <ketzacoatl@users.noreply.github.com>
2017-12-04 20:06:28 -0500

    cross-account-assume-role-policy: doc fixup

--- tf-modules/cross-account-assume-role-policy/main.tf
+++ tf-modules/cross-account-assume-role-policy/main.tf
@@ -1,4 +1,4 @@
-/*
+/**
  * ## Cross-Account Assume Role Policy
  *
  * Creates an IAM policy that allows attached entities to assume given role(s)
@@ -7,6 +7,7 @@
  * Either `account_ids` and `role_name` can be provided, OR `account_id` and
  * `role_names`. Other combinations will not work correctly due to limitations
  * in Terraform.
+ *
  */
 
 variable "aws_cloud" {
