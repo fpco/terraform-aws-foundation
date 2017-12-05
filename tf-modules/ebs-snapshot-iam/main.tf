6b1925e6f2d3047eec409302cd674fb58463ce27
user <ketzacoatl@users.noreply.github.com>
2017-12-04 20:05:52 -0500

    ebs-snapshot-iam: doc fixup

--- tf-modules/ebs-snapshot-iam/main.tf
+++ tf-modules/ebs-snapshot-iam/main.tf
@@ -1,8 +1,29 @@
-/*
+/**
  * ## IAM for Managing EBS Snapshots
  *
- * This module creates an `aws_iam_user_policy` that provides access to
- * EBS volumes and managing their snapshots.
+ * This module creates an `aws_iam_user` and `aws_iam_user_policy` that provide
+ * access to EBS volumes and managing their snapshots, specifically:
+ *
+ * ```
+ * "Action": [
+ *   "ec2:CreateSnapshot",
+ *   "ec2:CreateTags",
+ *   "ec2:DeleteSnapshot",
+ *   "ec2:DescribeAvailabilityZones",
+ *   "ec2:DescribeSnapshots",
+ *   "ec2:DescribeTags",
+ *   "ec2:DescribeVolumeAttribute",
+ *   "ec2:DescribeVolumeStatus",
+ *   "ec2:DescribeVolumes"
+ * ],
+ * "Effect": "Allow",
+ * "Resource": [
+ *   "*"
+ * ]
+ * ```
+ * 
+ * NOTE: It would be nice to provide this as a role policy that could be attached
+ * to a role, more freely than locking into an `aws_iam_user`.
  *
  */
 
