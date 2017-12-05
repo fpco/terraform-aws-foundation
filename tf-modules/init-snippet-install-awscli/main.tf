f71a65897cc411798f2292b3f29faf02cadfeae3
user <ketzacoatl@users.noreply.github.com>
2017-12-05 13:30:09 -0500

    init-snippet-install-awscli: doc fixup

--- tf-modules/init-snippet-install-awscli/main.tf
+++ tf-modules/init-snippet-install-awscli/main.tf
@@ -1,7 +1,11 @@
 /**
  * ## Init Snippet: Install AWS CLI Tool
  *
- * Document.
+ * Generate an init snippet to install the AWS cli tool on boot. This will:
+ *
+ * * use apt to install `python-pip`
+ * * use `pip` to upgrade `pip`
+ * * us `pip` to install `awscli`
  *
  */
 
