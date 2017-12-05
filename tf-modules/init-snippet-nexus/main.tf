74ee0d3060b8d14fba1ad126ab330c189e50ad49
user <ketzacoatl@users.noreply.github.com>
2017-12-05 13:29:50 -0500

    init-snippet-nexus: doc fixup

--- tf-modules/init-snippet-nexus/main.tf
+++ tf-modules/init-snippet-nexus/main.tf
@@ -1,8 +1,13 @@
 /**
  * ## Init Snippet: Run Nexus (w/ Docker)
  *
- * Use Docker to run the Nexus package manager.
- * DOCUMENT.
+ * Generate an init snippet to use Docker to run the Nexus package manager:
+ *
+ * * use `apt-get` to install docker
+ * * create `/etc/rc.local` to create/chown the `data_path` and run the
+ *   docker image
+ *
+ * The `sonatype/nexus` docker image is run.
  *
  */
 
