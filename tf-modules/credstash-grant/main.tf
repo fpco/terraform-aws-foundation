5a3b0bd374f8c0ec459f2cbe80f3fe2d84611c8d
user <ketzacoatl@users.noreply.github.com>
2017-12-05 13:43:26 -0500

    credstash-grant: doc fixup

--- tf-modules/credstash-grant/main.tf
+++ tf-modules/credstash-grant/main.tf
@@ -4,9 +4,11 @@
  * This module will make it possible for anybody assuming the supplied IAM Role to read
  * and/or write secrets from/to credstash store.
  *
- * The cleanup actions `when = "destroy"` in this module require at least terraform 0.9.0.
- * See RFC: https://docs.google.com/document/d/15nEcV7fxskDgYrXoNMl6RYIo10PCiZGle7TP8xitrFE/edit#
- * and: https://github.com/hashicorp/terraform/issues/386 ## for documentation
+ * The cleanup actions `when = "destroy"` in this module require at least
+ * terraform 0.9.0.
+ * See [this RFC](https://docs.google.com/document/d/15nEcV7fxskDgYrXoNMl6RYIo10PCiZGle7TP8xitrFE/)
+ * and [this ticket](https://github.com/hashicorp/terraform/issues/386) for more
+ * details on that.
  *
  */
 
