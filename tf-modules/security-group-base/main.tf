24e9899c854e678c4278c79a3f033b216bd97252
user <ketzacoatl@users.noreply.github.com>
2017-12-04 20:05:40 -0500

    security-group-base: doc fixup

--- tf-modules/security-group-base/main.tf
+++ tf-modules/security-group-base/main.tf
@@ -1,7 +1,11 @@
-/*
+/**
  * ## Security Group Base
  *
- * Add DOCS
+ * Create the `aws_security_group` that we will then go attach a bunch of
+ * `aws_security_group_rule`s to.
+ *
+ * Note that, when setting `aws_security_group.name`, the AWS API will require
+ * removing and recreating the resource if the `name` is changed.
  *
  */
 
