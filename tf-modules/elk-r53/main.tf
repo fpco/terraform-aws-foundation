f45b84488f76402295a4fb242e200dadb65e7d2b
user <ketzacoatl@users.noreply.github.com>
2017-12-05 13:43:26 -0500

    elk-r53: doc fixup

--- tf-modules/elk-r53/main.tf
+++ tf-modules/elk-r53/main.tf
@@ -1,8 +1,8 @@
 /**
  * ## ELK Route53
  *
- * This module adds all domain names to the Hosted Zone,
- * with an A Alias to their respective LBs
+ * This module adds all domain names to the Hosted Zone, with an A Alias to
+ * their respective load balancers.
  *
 **/
 
