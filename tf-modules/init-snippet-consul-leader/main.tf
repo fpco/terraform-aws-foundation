9e0473a46f8828b299cc24aca4bbc9c20820c2eb
user <ketzacoatl@users.noreply.github.com>
2017-12-05 13:30:06 -0500

    init-snippet-consul-leader: doc fixup

--- tf-modules/init-snippet-consul-leader/main.tf
+++ tf-modules/init-snippet-consul-leader/main.tf
@@ -1,7 +1,12 @@
 /**
  * ## Init Snippet: Consul Leader
  *
- * Document.
+ * Configure and run the consul service as a leader, using the `consul.service`
+ * Salt formula from `fpco-salt-formula`.
+ *
+ * This will write out bootstrap pillar before applying the formula. This is
+ * based on the DNS method for discovering the leaders, and should be updated
+ * to leverage the AWS API for that discovery.
  *
  */
 
