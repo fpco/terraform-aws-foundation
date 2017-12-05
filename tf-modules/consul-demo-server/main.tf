e04468f216fac072fd4d81769d916070bdceebd4
user <ketzacoatl@users.noreply.github.com>
2017-12-05 13:44:30 -0500

    consul-demo-server: doc fixup

--- tf-modules/consul-demo-server/main.tf
+++ tf-modules/consul-demo-server/main.tf
@@ -4,6 +4,8 @@
  * Run an instance of consul in server mode, on a single-node auto-scaling group.
  * Use an EBS volume for data persistence.
  *
+ * **TODO: Add an example that shows how to use this module..**
+ *
  */
 
 module "consul-server" {
