51de9f38a1222c060f30184eb07caa6c203c299f
user <ketzacoatl@users.noreply.github.com>
2017-12-04 20:07:21 -0500

    consul-agent-generic-init: doc fixup

--- tf-modules/consul-agent-generic-init/main.tf
+++ tf-modules/consul-agent-generic-init/main.tf
@@ -1,43 +1,43 @@
 /**
- *## Generic Host Init for Consul Agents
- *
- *The purpose of this module is to provide the `user_data` to init a new Host that
- *will be joining a network or SOA cluster built on Consul.
- *
- *This module relies on the same assumptions as the other modules in this
- *repository (we're using saltstack for CM and to abstract away the details). Thus,
- *the `user_data` generated makes use of Saltstack, the platform's configuration
- *management formula and related abstractions.
- *
- *The goals of this module are:
- *
- ** make the code easier to read and maintain,
- ** make the ASG definitions more reliable and consistent,
- ** simplify the code that defines the environment as a whole
- *
- *NOTE: need to update for template_file data type
- *
- *### Example
- *
- *```
- *# provisioning for worker cluster
- *module "worker-init" {
- *    source = "../tf-modules/consul-agent-generic-init"
- *    datacenter = "${var.datacenter}"
- *    service = "worker"
- *    consul_secret_key = "${var.consul_secret_key}"
- *    consul_client_token = "${var.consul_master_token}"
- *    leader_dns = "${module.consul-leaders.leader_dns}"
- *    extra_pillar = "extra: pillar"
- *    extra_init = <<EOF
- *echo "customize this node's init.."
- *date
- *consul --version
- *salt-call --version
- *uname -a
- *EOF
- *}
- *```
+ * ## Generic Host Init for Consul Agents
+ * 
+ * The purpose of this module is to provide the `user_data` to init a new Host that
+ * will be joining a network or SOA cluster built on Consul.
+ * 
+ * This module relies on the same assumptions as the other modules in this
+ * repository (we're using saltstack for CM and to abstract away the details). Thus,
+ * the `user_data` generated makes use of Saltstack, the platform's configuration
+ * management formula and related abstractions.
+ * 
+ * The goals of this module are:
+ * 
+ * * make the code easier to read and maintain,
+ * * make the ASG definitions more reliable and consistent,
+ * * simplify the code that defines the environment as a whole
+ * 
+ * NOTE: need to update for template_file data type
+ * 
+ * ### Example
+ * 
+ * ```
+ * # provisioning for worker cluster
+ * module "worker-init" {
+ *     source = "../tf-modules/consul-agent-generic-init"
+ *     datacenter = "${var.datacenter}"
+ *     service = "worker"
+ *     consul_secret_key = "${var.consul_secret_key}"
+ *     consul_client_token = "${var.consul_master_token}"
+ *     leader_dns = "${module.consul-leaders.leader_dns}"
+ *     extra_pillar = "extra: pillar"
+ *     extra_init = <<EOF
+ * echo "customize this node's init.."
+ * date
+ * consul --version
+ * salt-call --version
+ * uname -a
+ * EOF
+ * }
+ * ```
  */
 
 # render init script for a cluster using our generic template
