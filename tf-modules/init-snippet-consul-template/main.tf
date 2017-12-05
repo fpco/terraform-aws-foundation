5f8c1c0b8e44fe6e06a3088b577923da99ea680f
user <ketzacoatl@users.noreply.github.com>
2017-12-05 13:30:00 -0500

    init-snippet-consul-template: doc fixup

--- tf-modules/init-snippet-consul-template/main.tf
+++ tf-modules/init-snippet-consul-template/main.tf
@@ -1,7 +1,9 @@
 /**
  * ## Init Snippet: Consul Template
  *
- * Document.
+ * Configure and run `consul-template` as a system service using the
+ * `consul.template-tool.service` formula from `fpco-salt-formula`. This will
+ * write out bootstrap pillar before applying the formula.
  *
  */
 
