930e6a8b26064cbd5302e8c44747ff03dcf57d59
user <ketzacoatl@users.noreply.github.com>
2017-12-05 13:26:56 -0500

    bind-server: doc fixup

--- tf-modules/bind-server/main.tf
+++ tf-modules/bind-server/main.tf
@@ -1,20 +1,19 @@
 /**
- * ## Simple DNS server(s) running bind9.
- * 
+ * ## Simple DNS server(s) running `bind9`
+ *
  * Supports setting up multiple servers in different AZs using the same
  * configuration.
- * 
+ *
  * Currently the intent is for setting up caching/forwarding
  * servers on private VPCs (as Route53 provides serving DNS zones well).
- * 
+ *
  * The `null_resource` provisioner will upload new options
  * and reload the configuration whenever it changes.
- * 
+ *
  * TODO: support uploading additional files, such as DNS zones
- * 
+ *
  * ### Example
- * 
- *     ```
+ *
  *     module "dns" {
  *       source  = "../../vendor/fpco/terraform-aws/tf-modules/bind-server"
  *       name = "dns"
@@ -25,11 +24,9 @@
  *       key_name = "${aws_key_pair.admin.id}"
  *       named_conf_options = "${file("${path.module}/files/dns/named.conf.options")}"
  *     }
- *     ```
- * 
+ *
  * And `files/dns/named.conf.options` would be something like this:
- * 
- *     ```
+ *
  *     options {
  *     	directory "/var/cache/bind";
  *     	forward only;
@@ -38,10 +35,10 @@
  *     	 	10.0.0.2;
  *     	};
  *     	dnssec-validation auto;
- * 
+ *
  *     	auth-nxdomain no;    # conform to RFC1035
  *     	listen-on-v6 { any; };
- * 
+ *
  *     	allow-query { any; };
  *     };
  *     # This is a private zone served by Route 53 that uses the Amazon DNS server
@@ -55,7 +52,10 @@
  *     	type forward;
  *     	forwarders { 10.10.1.50; 10.10.1.51; };
  *     };
- *     ```
+ *
+ * Note that the instance has auto-recovery enabled, so in case there is a
+ * hardware failure, the instance will be recreated on a new hardware node (but
+ * retaining the same instance ID and ip addresses).
  *
  */
 
