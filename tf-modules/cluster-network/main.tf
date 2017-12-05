7c6b02a2e9422fcd99efc705b11158e94c6281a2
user <ketzacoatl@users.noreply.github.com>
2017-12-05 13:43:26 -0500

    cluster-network: doc fixup

--- tf-modules/cluster-network/main.tf
+++ tf-modules/cluster-network/main.tf
@@ -1,11 +1,12 @@
 /**
  * ## cluster-network
  * 
+ * **DEPRECATED** - This module will be removed in a future release.
+ * 
  * This module creates subnets in each of two Availability Zones in the region
  * specified. Specifically, an `aws_subnet` and `aws_route_table_association`
  * for each as a pair, named `a` and `c`.
  * 
- *  **DEPRECATED**
  */
 
 # Subnets in each of two AZ in this region
