be58e0367b171cf8f70f411d093600606d6dc260
user <ketzacoatl@users.noreply.github.com>
2017-12-05 13:43:26 -0500

    cross-account-role: doc fixup

--- tf-modules/cross-account-role/main.tf
+++ tf-modules/cross-account-role/main.tf
@@ -1,7 +1,7 @@
 /**
  * ## Cross-Account Role
  *
- * Creates an IAM role that can be assumed by users in given accounts
+ * Creates an IAM role that can be assumed by users in given accounts.
  * It is up to the caller to attach desired policies to this role.
  *
  */
