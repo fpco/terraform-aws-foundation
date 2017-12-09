1950acb530a3ebaa427bb94bc2f4672c2bfa72ba
user <ketzacoatl@users.noreply.github.com>
2017-12-09 08:34:04 -0500

    tests/gitlab-asg: make the deployment more robust/complete
    add/improve support for the following:
    
    * load balancer (ELB) in front of gitlab
    * SSL on the ELB
    * create DNS records to go along with gitlab
    * sync with the updated init-snippet module

--- tests/gitlab-asg/main.tf
+++ tests/gitlab-asg/main.tf
@@ -29,7 +29,30 @@ variable "region" {
 }
 
 variable "ssh_pubkey" {
+  default     = "./id_rsa.pub"
   description = "The path to the SSH pub key to use"
+}
+
+variable "dns_zone_id" {
+  description = "The ID of the DNS zone on Route53, to create records in for gitlab"
+  type        = "string"
+}
+
+variable "ssl_arn" {
+  description = "The ARN of the SSL cert, see 'make upload-tls-certs'"
+  type        = "string"
+}
+
+variable "gitlab_name" {
+  description = "To generate the DNS record for gitlab, prefix the zone"
+  default     = "gitlab"
+  type        = "string"
+}
+
+variable "gitlab_registry_name" {
+  description = "To generate the DNS record for the docker registry, prefix the zone"
+  default     = "registry"
+  type        = "string"
 }
 
 provider "aws" {
@@ -48,6 +71,67 @@ resource "aws_key_pair" "main" {
   public_key = "${file(var.ssh_pubkey)}"
 }
 
+resource "aws_elb" "gitlab" {
+  name                  = "${var.name}"
+  subnets               = ["${module.vpc.public_subnet_ids[0]}"]
+  security_groups       = ["${aws_security_group.gitlab-elb.id}"]
+
+  listener {
+    instance_port       = 80
+    instance_protocol   = "http"
+    lb_port             = 443
+    lb_protocol         = "https"
+    ssl_certificate_id  = "${var.ssl_arn}"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "TCP:80"
+    interval            = 30
+  }
+
+  tags {
+    Name = "${var.name}"
+  }
+}
+
+resource "aws_security_group" "gitlab-elb" {
+  name        = "gitlab-elb"
+  vpc_id      = "${module.vpc.vpc_id}"
+  description = "Security group for the gitlab ELB"
+}
+
+module "elb-http-rule" {
+  source            = "../../tf-modules/single-port-sg"
+  port              = 80
+  description       = "Allow ingress for HTTP, port 80 (TCP), thru the ELB"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.gitlab-elb.id}"
+}
+
+module "elb-https-rule" {
+  source            = "../../tf-modules/single-port-sg"
+  port              = 443
+  description       = "Allow ingress for HTTPS, port 443 (TCP), thru the ELB"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.gitlab-elb.id}"
+}
+
+module "elb-gitlab-ssh-rule" {
+  source            = "../../tf-modules/single-port-sg"
+  port              = 8022
+  description       = "Allow ingress for Git over SSH, port 8022 (TCP), thru the ELB"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.gitlab-elb.id}"
+}
+
+module "elb-open-egress-rule" {
+  source            = "../../tf-modules/open-egress-sg"
+  security_group_id = "${aws_security_group.gitlab-elb.id}"
+}
+
 module "gitlab-asg" {
   source        = "../../tf-modules/single-node-asg"
   name_prefix   = "${var.name}"
@@ -59,6 +143,7 @@ module "gitlab-asg" {
   instance_type = "t2.medium"
   subnet_id     = "${module.vpc.public_subnet_ids[0]}"
 
+  load_balancers        = ["${aws_elb.gitlab.name}"]
   security_group_ids    = ["${aws_security_group.gitlab.id}"]
   data_volume_encrypted = false
 
@@ -89,7 +174,8 @@ module "init-install-ops" {
 }
 
 module "init-gitlab-docker" {
-  source = "../../tf-modules/init-snippet-gitlab-docker"
+  source        = "../../tf-modules/init-snippet-gitlab-docker"
+  gitlab_domain = "${data.aws_route53_zone.selected.name}"
 }
 
 module "vpc" {
@@ -141,6 +227,32 @@ module "open-egress-rule" {
   source            = "../../tf-modules/open-egress-sg"
   security_group_id = "${aws_security_group.gitlab.id}"
 }
+
+##################
+## DNS setup
+
+data "aws_route53_zone" "selected" {
+  zone_id = "${var.dns_zone_id}"
+}
+
+resource "aws_route53_record" "gitlab" {
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name    = "${var.gitlab_name}.${data.aws_route53_zone.selected.name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${aws_elb.gitlab.dns_name}"]
+}
+
+resource "aws_route53_record" "registry" {
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name    = "${var.gitlab_registry_name}.${data.aws_route53_zone.selected.name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${aws_elb.gitlab.dns_name}"]
+}
+
+##################
+## Outputs
 
 // region deployed to
 output "region" {
