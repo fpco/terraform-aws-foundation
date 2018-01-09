727ed1a794ca773f7431c159b6e49ec4b9812717
Ryan King <ryan.king@gosynesis.com>
2018-01-09 12:56:02 -0800

    Removed duplicate outputs

--- tf-modules/single-node-asg/outputs.tf
+++ tf-modules/single-node-asg/outputs.tf
@@ -17,13 +17,3 @@ output "asg_iam_role_arn" {
 output "asg_iam_role_name" {
   value = "${module.service-data.iam_role_name}"
 }
-
-// `arn` exported from the Service Data `aws_iam_profile`
-output "asg_iam_profile_arn" {
-  value = "${module.service-data.iam_profile_arn}"
-}
-
-//`arn` exported from the Service Data `aws_iam_role`
-output "asg_iam_role_arn" {
-  value = "${module.service-data.iam_role_arn}"
-}
