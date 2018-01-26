fc7a5b057dcfaf351111bc0cbeab3b46774db269
Alexey Kuleshevich <alexey@fpcomplete.com>
2018-01-26 22:07:55 +0300

    Updated credstash-setup to reflect changes in new version terrraform-0.11

--- tf-modules/credstash-setup/output.tf
+++ tf-modules/credstash-setup/output.tf
@@ -1,31 +1,31 @@
 //KMS Key ARN. It can later be used to store and retrieve secrets.
 output "kms_key_arn" {
-  value = "${aws_kms_key.credstash-key.arn}"
+  value = "${aws_kms_key.credstash-key.*.arn}"
 }
 
 //KMS Master key id.
 output "kms_key_id" {
-  value = "${aws_kms_key.credstash-key.key_id}"
+  value = "${aws_kms_key.credstash-key.*.key_id}"
 }
 
 //KMS Master key alias. It can later be used to store and retrieve secrets.
 output "kms_key_alias" {
-  value = "${aws_kms_alias.credstash-key.name}"
+  value = "${aws_kms_alias.credstash-key.*.name}"
 }
 
 //KMS Master key alias ARN.
 output "kms_key_alias_arn" {
-  value = "${aws_kms_alias.credstash-key.arn}"
+  value = "${aws_kms_alias.credstash-key.*.arn}"
 }
 
 //DynamoDB table ARN that can be used by credstash to store/retrieve secrets.
 output "db_table_arn" {
-  value = "${aws_dynamodb_table.credstash-db.arn}"
+  value = "${aws_dynamodb_table.credstash-db.*.arn}"
 }
 
 //DynamoDB table name that can be used by credstash to store/retrieve secrets.
 output "db_table_name" {
-  value = "${aws_dynamodb_table.credstash-db.id}"
+  value = "${aws_dynamodb_table.credstash-db.*.id}"
 }
 
 // Ubuntu bash script snippet for installing credstash and its dependencies
@@ -45,10 +45,10 @@ output "put_cmd" {
 
 // Secret Reader policy
 output "reader_policy_arn" {
-  value = "${aws_iam_policy.reader-policy.arn}"
+  value = "${aws_iam_policy.reader-policy.*.arn}"
 }
 
 // Secret Writer policy
 output "writer_policy_arn" {
-  value = "${aws_iam_policy.writer-policy.arn}"
+  value = "${aws_iam_policy.writer-policy.*.arn}"
 }
