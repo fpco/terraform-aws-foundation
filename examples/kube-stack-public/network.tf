e8c5f6a4c531b6a20dfe9c8cb97aa8da81866350
Yghor Kerscher <kerscher@outlook.com>
2018-03-12 15:06:25 +0100

    examples/kube-stack-public: reorganize resources

---
+++ examples/kube-stack-public/network.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+  source      = "../../modules/vpc-scenario-1"
+  region      = "${var.region}"
+  cidr        = "${var.vpc_cidr}"
+  name_prefix = "${var.name}"
+
+  #extra_tags           = "${var.extra_tags}"
+  public_subnet_cidrs = ["${var.public_subnet_cidrs}"]
+  azs                 = ["${slice(data.aws_availability_zones.available.names, 0, 2)}"]
+}
