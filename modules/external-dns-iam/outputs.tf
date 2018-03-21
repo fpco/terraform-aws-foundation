ace25c649923811f64b22106f8060aa99ddc15ff
Deni Bertovic <deni@denibertovic.com>
2018-03-21 13:39:09 +0100

    Prepend role_ to outputs

--- modules/external-dns-iam/outputs.tf
+++ modules/external-dns-iam/outputs.tf
@@ -1,9 +1,9 @@
-output "name" {
+output "role_name" {
   value       = "${aws_iam_role.dnscontroller.name}"
   description = "Name of the new role with route53 permissions."
 }
 
-output "arn" {
+output "role_arn" {
   value       = "${aws_iam_role.dnscontroller.arn}"
   description = "ARN of the new role with route53 permissions."
 }
