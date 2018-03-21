bbf7f096bb0340f45630e514e2f4a8103dd64cbd
Deni Bertovic <deni@denibertovic.com>
2018-03-21 13:39:09 +0100

    Prepend role_ to outputs

--- modules/cloudwatch-exporter-iam/outputs.tf
+++ modules/cloudwatch-exporter-iam/outputs.tf
@@ -1,9 +1,9 @@
-output "name" {
+output "role_name" {
   value       = "${aws_iam_role.cloudwatch_exporter.name}"
   description = "Name of the new role with cloudwatch read-only permissions."
 }
 
-output "arn" {
+output "role_arn" {
   value       = "${aws_iam_role.cloudwatch_exporter.arn}"
   description = "ARN of the new role with cloudwatch read-only permissions."
 }
