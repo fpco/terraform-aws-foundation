a7765354f4b721f2607e6edfc1aaa219c57ea067
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2018-05-20 10:42:32 -0400

    cloudtrail: add type definitions for all variables, #108

--- modules/cloudtrail/variables.tf
+++ modules/cloudtrail/variables.tf
@@ -1,27 +1,34 @@
 variable "name_prefix" {
   description = "name to be prepended to all resources"
+  type        = "string"
 }
 
 variable "aws_cloud" {
   description = "set to 'aws-us-gov' if using GovCloud, otherwise leave the default"
   default     = "aws"
+  type        = "string"
 }
 
 variable "kms_key_id" {
   description = "KMS key ARN for encryption of logs"
   default     = ""
+  type        = "string"
 }
 
 variable "include_global_service_events" {
   description = "boolean, if true logs IAM events as well as region-specific ones"
   default     = true
+  type        = "string"
 }
 
 variable "enable_logging" {
   description = "boolean, maps to `aws_cloudtrail`'s enable_logging parameter"
   default     = true
+  type        = "string"
 }
 
 variable "extra_tags" {
-  default = {}
+  description = "map of tags to append to AWS resources that support tagging"
+  default     = {}
+  type        = "map"
 }
