eaa8bd40cab401abcec87d128c376d072a8320b8
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2018-05-21 06:18:19 -0400

    credstash-setup: add type definitions for all variables, #108

--- modules/credstash-setup/variables.tf
+++ modules/credstash-setup/variables.tf
@@ -1,39 +1,47 @@
 variable "aws_cloud" {
   description = "set to 'aws-us-gov' if using GovCloud, otherwise leave the default"
   default     = "aws"
+  type        = "string"
 }
 
 variable "create_kms_key" {
   default     = true
   description = "Should the Master key be created"
+  type        = "string"
 }
 
 variable "kms_key_name" {
   default     = "credstash"
   description = "KMS Master Key Name."
+  type        = "string"
 }
 
 variable "enable_key_rotation" {
   default     = false
   description = "Specifies whether key rotation is enabled"
+  type        = "string"
 }
 
 variable "create_db_table" {
   default     = true
   description = "Should the DynamoDB table be created"
+  type        = "string"
 }
 
 variable "db_table_name" {
   default     = "credential-store"
   description = "Name of the DynamoDB table where credentials will be stored"
+  type        = "string"
 }
 
 variable "create_reader_policy" {
   default     = false
   description = "Should credstash Secret Reader IAM Policy be created."
+  type        = "string"
 }
 
 variable "create_writer_policy" {
   default     = false
   description = "Should credstash Secret Writer IAM Policy be created."
+  type        = "string"
 }
