a9c5ae6592e625c0ac9cdbd6bdf1e2b865f074d0
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2018-05-21 06:16:42 -0400

    cw-auto-recover-existing-ec2: add type definitions for all variables, #108

--- modules/cloudwatch-auto-recover-existing-ec2/variables.tf
+++ modules/cloudwatch-auto-recover-existing-ec2/variables.tf
@@ -1,30 +1,35 @@
 variable "aws_cloud" {
   description = "set to 'aws-us-gov' if using GovCloud, otherwise leave the default"
   default     = "aws"
+  type        = "string"
 }
 
 variable "name_prefix" {
   description = "Name to be prefixed to all resources"
   default     = ""
+  type        = "string"
 }
 
 variable "name_format" {
   description = "Naming scheme as a string, to use with the format() function."
   default     = "%s-auto-recover-%02d"
+  type        = "string"
 }
 
 variable "ec2_instance_ids" {
   description = "EC2 instance ids of VMs that should receive auto-recover capabilities."
-  type        = "list"
   default     = []
+  type        = "list"
 }
 
 variable "maximum_failure_duration" {
   description = "Maximum amount of system status checks failures period in seconds before recovery kicks in."
   default     = "60"
+  type        = "string"
 }
 
 variable "alarm_actions" {
   description = "list of alarm actions to append to the default (optional)"
   default     = []
+  type        = "list"
 }
