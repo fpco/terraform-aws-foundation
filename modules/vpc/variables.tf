487aec04a73d837123f71c5bf78e83a6a0c7c6fe
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2018-05-24 06:48:04 -0400

    vpc: add type definitions for all variables, #108

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,36 +1,44 @@
 variable "name_prefix" {
   description = "Name to prefix the VPC/DHCP resources with"
+  type        = "string"
 }
 
 variable "region" {
   description = "Region were VPC will be created"
+  type        = "string"
 }
 
 variable "cidr" {
   description = "CIDR range of VPC. eg: 172.16.0.0/16"
+  type        = "string"
 }
 
 variable "extra_tags" {
   description = "Extra tags that will be added to VPC and DHCP resources"
   default     = {}
+  type        = "map"
 }
 
 variable "enable_dns_hostnames" {
   default     = true
   description = "boolean, enable/disable VPC attribute, enable_dns_hostnames"
+  type        = "string"
 }
 
 variable "enable_dns_support" {
   default     = true
   description = "boolean, enable/disable VPC attribute, enable_dns_support"
+  type        = "string"
 }
 
 variable "dns_servers" {
   default     = ["AmazonProvidedDNS"]
   description = "list of DNS servers for the DHCP options resource"
+  type        = "list"
 }
 
 variable "ntp_servers" {
   default     = []
   description = "list of NTP servers for the DHCP options resource"
+  type        = "list"
 }
