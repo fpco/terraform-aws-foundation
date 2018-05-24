e25daae4dc7abb45f952c545195be69a5f8e73c7
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2018-05-24 06:48:21 -0400

    vpc-network: add type definitions for all variables, #108

--- modules/vpc-network/variables.tf
+++ modules/vpc-network/variables.tf
@@ -1,12 +1,15 @@
 variable "name" {
   description = "The name of this network"
+  type        = "string"
 }
 
 variable "vpc_cidr_prefix" {
   description = "The prefix to the VPC CIDR block, eg: 10.100"
+  type        = "string"
 }
 
 variable "enable_dns_hostnames" {
   default     = "true"
   description = "Boolean to set the `enable_dns_hostnames` flag for the VPC"
+  type        = "string"
 }
