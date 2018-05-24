a797d2d165dc765efc32fd27a5bde696a2010692
user <ketzacoatl@users.noreply.github.com>
2018-05-24 06:44:44 -0400

    kube-controller-sg: add type definitions for all variables, #108

--- modules/kube-controller-sg/variables.tf
+++ modules/kube-controller-sg/variables.tf
@@ -1,19 +1,23 @@
 variable "name_prefix" {
   description = "Prefix that will be added to names of all resources"
+  type        = "string"
 }
 
 variable "name_suffix" {
   description = "suffix to use when naming the various resources"
   default     = "kube-controller"
+  type        = "string"
 }
 
 variable "vpc_id" {
   description = "VPC id for the security group"
+  type        = "string"
 }
 
 variable "api_port" {
   description = "TCP port the kube controller API is listening on"
   default     = "6443"
+  type        = "string"
 }
 
 variable "cidr_blocks_api" {
