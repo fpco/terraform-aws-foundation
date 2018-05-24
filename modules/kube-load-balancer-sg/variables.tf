04024c1b2b60146837fff2d6e226694899e099c8
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2018-05-24 06:46:47 -0400

    kube-load-balancer-sg: add type definitions for all variables, #108

--- modules/kube-load-balancer-sg/variables.tf
+++ modules/kube-load-balancer-sg/variables.tf
@@ -1,19 +1,23 @@
 variable "name_prefix" {
   description = "Prefix that will be added to names of all resources"
+  type        = "string"
 }
 
 variable "name_suffix" {
   description = "suffix to include when naming the various resources"
   default     = "kube-load-balancer"
+  type        = "string"
 }
 
 variable "vpc_id" {
   description = "VPC id for the security group"
+  type        = "string"
 }
 
 variable "api_port" {
   description = "TCP port the load balancer should be configured to listen on"
   default     = "443"
+  type        = "string"
 }
 
 variable "cidr_blocks_api" {
