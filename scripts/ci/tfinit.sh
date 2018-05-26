7083325c78df9dccff27fd0303efd91b2204f2b4
Mike McGirr <mike@oatsmail.com>
2018-05-25 18:11:42 -0700

    Update scripts for gitlab-ci. This is to add linting of the s3-full-access-policy module example.

--- scripts/ci/tfinit.sh
+++ scripts/ci/tfinit.sh
@@ -1,10 +1,16 @@
-#/bin/bash
+#!/usr/bin/env bash
 
 set -o nounset
 set -o pipefail
 set -o errexit
 
 pushd "$(dirname $(basename "${0}"))/tests" > /dev/null
+
+terraform init 1> /dev/null
+
+popd > /dev/null
+
+pushd "$(dirname $(basename "${0}"))/examples/s3-full-access-policy" > /dev/null
 
 terraform init 1> /dev/null
 
--- scripts/ci/tflint.sh
+++ scripts/ci/tflint.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/usr/bin/env bash
 
 set -o nounset
 set -o pipefail
@@ -7,6 +7,13 @@ set -o errexit
 pushd "$(dirname "$(basename "${0}")")/tests" > /dev/null
 
 terraform init 1> /dev/null 2>&1 || true
-tflint --error-with-issues 
+tflint --error-with-issues
+
+popd > /dev/null
+
+pushd "$(dirname $(basename "${0}"))/examples/s3-full-access-policy" > /dev/null
+
+terraform init 1> /dev/null 2>&1 || true
+tflint --error-with-issues
 
 popd > /dev/null
