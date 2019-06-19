23cf983024782988033ad689be063866d1fedd62
Sibi <sibi@fpcomplete.com>
2019-06-19 17:25:15 +0530

    Demonstrate basic ASG integration with lifecycle hooks (#189)
    * Initial working http web app
    * Sample working web application with ASG and ALB
    * Monitor works
    * Add working version of asg lifecycle hooks
    * Cleanup and simplify
    * Add lifecycled example component
    * Simplify and fix the bug for Amazon AMI
    * Add elb sg to it
    * Commit working configuration
    * Add new variables
    * Remove lifecycle-hooks-asg
    * Update README.md
    * template isn't needed anymore
    * Update configuration, add some documentation
    * Remove s3 bucket logic
    * Download lifecycled binary from github release
    * Fix asg-lifecycle-hooks based on lifecycled module change
    * Remove cloudwatch
    * Add Testing section
    * Fix the options, remove cloudwatch
    * This works for unregistering - still need to fix cloud_config.yml
    * Fix script
    * Change to elb_name
    * Simplify the permission
    * Update README
    * Refactor modules
    * Move cloud init script to the examples section. Make lifecycled component less coupled.
    * Use new module asg-lifecycle and docuple logic
    * Add the cloud-config.yml configuration file

---
+++ examples/asg-lifecycle-hooks/variables.tf
@@ -0,0 +1,50 @@
+variable "extra_tags" {
+  description = "Extra tags that will be added to aws_subnet resources"
+  default     = {}
+}
+
+variable "lifecycle_name_prefix" {
+  description = "Prefix used for resource names."
+  default     = "lifecycled-eg"
+}
+
+variable "name_prefix" {
+  description = "Prefix used for resource names."
+  default     = "asg-lc"
+}
+
+variable "name" {
+  description = "name of the project, use as prefix to names of resources created"
+  default     = "test-lifecycle-project"
+}
+
+variable "region" {
+  description = "Region where the project will be deployed"
+  default     = "us-east-2"
+}
+
+variable "vpc_cidr" {
+  description = "Top-level CIDR for the whole VPC network space"
+  default     = "10.23.0.0/16"
+}
+
+variable "ssh_pubkey" {
+  description = "File path to SSH public key"
+  default     = "./id_rsa.pub"
+}
+
+variable "ssh_key" {
+  description = "File path to SSH public key"
+  default     = "./id_rsa"
+}
+
+variable "public_subnet_cidrs" {
+  default     = ["10.23.11.0/24", "10.23.12.0/24", "10.23.13.0/24"]
+  description = "A list of public subnet CIDRs to deploy inside the VPC"
+}
+
+variable "tags" {
+  description = "A map of tags (key-value pairs) passed to resources."
+  type        = "map"
+  default     = {}
+}
