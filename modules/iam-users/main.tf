4074e174e3856e0ffba826f4a97f91026629c4ab
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2019-07-12 16:38:20 -0400

    iam-users: add new module
    Creates a bunch of IAM users from a list of usernames

---
+++ modules/iam-users/main.tf
@@ -0,0 +1,20 @@
+variable "user_list" {
+  description = "List of strings, where each entry is the intended username for a user in IAM"
+  type        = list(string)
+}
+
+variable "path" {
+  default     = "/"
+  description = "string path in IAM, for the users created (all get the same path)"
+  type        = string
+}
+
+resource "aws_iam_user" "u" {
+  count = length(var.user_list)
+  name  = var.user_list[count.index]
+}
+
+output "users" {
+  description = "The list of IAM user objects that exist"
+  value       = zipmap(var.user_list, aws_iam_user.u.*)
+}
