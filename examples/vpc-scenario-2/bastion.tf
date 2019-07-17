755d81f861bd0b0b6e1bbeaf85d8eeabbaa42953
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2019-07-17 18:39:25 -0400

    examples/vpc-scenario-2: update to make it more useful
    * bump Ubuntu web instance to 16.04
    * use the simple-hostname init-snippet to include tests for more modules
    * update init to better use our modules
    * add a bastion instance
    * update security groups for the web/bastion so SSH tunneling through
      the bastion works as expected
    * add some outputs to make it easier
    * add new make targets in the Makefile, to lookup-ips, terminate
      instances, and poll for HTTP success - easier testing

---
+++ examples/vpc-scenario-2/bastion.tf
@@ -0,0 +1,52 @@
+resource "aws_instance" "bastion" {
+  ami                         = "${module.ubuntu-xenial-ami.id}"
+  associate_public_ip_address = "true"
+  instance_type               = "t2.nano"
+  key_name                    = "${aws_key_pair.main.key_name}"
+  subnet_id                   = "${module.vpc.public_subnet_ids[0]}"
+  vpc_security_group_ids      = ["${aws_security_group.bastion.id}"]
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "20"
+  }
+
+  lifecycle = {
+    ignore_changes = ["ami", "user_data"]
+  }
+
+  user_data = <<END_INIT
+#!/bin/bash
+echo "hello init"
+END_INIT
+}
+
+# Security group for the bastion instance
+resource "aws_security_group" "bastion" {
+  name   = "${var.name}-bastion"
+  vpc_id = "${module.vpc.vpc_id}"
+}
+
+module "bastion-ssh-rule" {
+  source            = "../../modules/ssh-sg"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.bastion.id}"
+}
+
+# open egress for bastion instance (outbound from the node)
+module "bastion-egress-rule" {
+  source            = "../../modules/open-egress-sg"
+  security_group_id = "${aws_security_group.bastion.id}"
+}
+
+# EIP for the bastion instance
+resource "aws_eip" "bastion" {
+  vpc   = "true"
+}
+
+# Attach the EIP to the bastion instance
+resource "aws_eip_association" "bastion" {
+  allocation_id = "${aws_eip.bastion.id}"
+  instance_id   = "${aws_instance.bastion.id}"
+}
+
