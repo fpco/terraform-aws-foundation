74a7251684f98a89276fe9ca44089e33bd5bdb3e
Magicloud <1886157+Magicloud@users.noreply.github.com>
2019-07-18 20:31:22 +0800

    New: module to get CentOS AMI by release.
    Provide more possiblities.
    
    AMI is filtered by name "^CentOS Linux ${var.release} x86_64 HVM EBS ENA" and "hvm" virt, "x86_64" arch.
    
    Example ami-centos-test is the unit test for module ami-centos.

---
+++ modules/ami-centos/main.tf
@@ -0,0 +1,33 @@
+variable "most_recent" {
+  default     = "true"
+  description = "boolean, maps to `most_recent` parameter for `aws_ami` data source"
+  type        = "string"
+}
+
+variable "release" {
+  description = "default centos release to target"
+  type        = "string"
+}
+
+data "aws_ami" "centos" {
+  most_recent = "${var.most_recent}"
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  owners = ["679593333241"]
+
+  name_regex = "^CentOS Linux ${var.release} x86_64 HVM EBS ENA"
+}
+
+output "id" {
+  value       = "${data.aws_ami.centos.id}"
+  description = "ID of the AMI"
+}
