89c12e4b0e5fb9b59001975960a86e7e3fb27444
Magicloud <1886157+Magicloud@users.noreply.github.com>
2019-10-03 23:26:20 +0800

    Bugfix: module/persistent-ebs
    - output **iam_policy_arn** definition was wrong.
    
      And since it is in attaching style, exposing policy ARN itself is non-sense.
    
    - output **iam_profile_policy_document** definition was wrong.
    
      Due to the attaching style change, original resource was gone. So directly expose the policy document.

--- modules/persistent-ebs/outputs.tf
+++ modules/persistent-ebs/outputs.tf
@@ -3,12 +3,7 @@ output "volume_id" {
   description = "`id` exported from the `aws_ebs_volume`"
 }
 
-output "iam_policy_arn" {
-  value       = aws_ebs_volume.main.id
-  description = "`id` exported from the `aws_ebs_volume`"
-}
-
 output "iam_profile_policy_document" {
-  value       = aws_iam_role_policy.attach_ebs.policy
+  value       = data.aws_iam_policy_document.attach_ebs_policy_doc.json
   description = "`policy` exported from the `aws_iam_role_policy`"
 }
