c612c4b77775a4bc408f2b6548ee4433c5def561
Magicloud <1886157+Magicloud@users.noreply.github.com>
2020-03-01 08:44:24 +0800

    Module/single-node-asg: Output attached data volume tag value "Name"
    The value can be used by DLM for tag matching.

--- modules/single-node-asg/outputs.tf
+++ modules/single-node-asg/outputs.tf
@@ -7,3 +7,8 @@ output "asg_iam_role_name" {
   value       = module.instance_profile.iam_role_name
   description = "`name` exported from the Service Data `aws_iam_role`"
 }
+
+output "data_volume_name_tag" {
+  value       = "${local.data_volume_name_prefix}-${local.az}"
+  description = "Name tag value for attached data volume"
+}
