06471e55d3b9261beb88a44fa97b26a4272d265b
Jose Duran <jose.daniel.duran.toro@gmail.com>
2020-05-11 23:41:45 -0500

    remove sg rule and exporting values

--- modules/ec2-connect-tunnel/outputs.tf
+++ modules/ec2-connect-tunnel/outputs.tf
@@ -2,3 +2,27 @@ output "public_ip" {
   value       = module.asg.eip_address
   description = "Public IP of the tunnel"
 }
+
+output "sg_id" {
+  value       = module.tunnel-sg.id
+  description = "Security group id of the tunnel"
+}
+
+output "asg_name" {
+  value       = module.asg.name
+  description = "`name` exported from the Server `aws_autoscaling_group`"
+}
+
+output "asg_iam_role_name" {
+  value       = module.asg.asg_iam_role_name
+  description = "`name` exported from the Service Data `aws_iam_role`"
+}
+
+output "data_volume_name_tag" {
+  value       = module.asg.data_volume_name_tag
+  description = "Name tag value for attached data volume."
+}
+
+output "eip_address" {
+  value = module.asg.eip_address
+}
--- modules/ec2-connect-tunnel/sg.tf
+++ modules/ec2-connect-tunnel/sg.tf
@@ -6,14 +6,6 @@ module "tunnel-sg" {
   extra_tags  = var.extra_tags
 }
 
-module "ssh-port-sg-rule" {
-  source            = "../single-port-sg"
-  security_group_id = module.tunnel-sg.id
-  cidr_blocks       = ["0.0.0.0/0"]
-  port              = 22
-  description       = "SSH from anywhere"
-}
-
 # security group rule to open egress (outbound from nodes)
 module "allow-open-egress" {
   source            = "../open-egress-sg"
