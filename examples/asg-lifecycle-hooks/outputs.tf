1a139383c20ac53f5ed408aa3da2867b0625b258
Sibi Prabakaran <sibi@psibi.in>
2020-07-07 19:54:32 +0530

    Update

--- examples/asg-lifecycle-hooks/outputs.tf
+++ examples/asg-lifecycle-hooks/outputs.tf
@@ -1,20 +1,4 @@
-output "elb_dns" {
-  value       = "http://${aws_elb.web.dns_name}"
-  description = "DNS name of the ELB"
+output "asg_id" {
+  value       = aws_autoscaling_group.cluster.id
+  description = "The autoscaling group id."
 }
-
-output "elb_name" {
-  value       = aws_elb.web.name
-  description = "ELB Name"
-}
-
-output "elb_instances" {
-  value       = aws_elb.web.instances
-  description = "ELB Instances"
-}
-
-output "elb_arn" {
-  value       = aws_elb.web.arn
-  description = "ELB Arn"
-}
-
